42f9e6470ac56e2949fbf95dfaa82425b5218cfe
brunofaefaion <bruno.fae.faion@gmail.com>
2020-04-21 11:22:01 -0300

    Project structure defined

---
+++ api/modules/database.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase'
+
+export default: {
+  createObject: (payload) => {
+    
+  },
+  deleteObject: (payload) => {
+
+  },
+  updateObject: (payload) => {
+
+  },
+  getObject: (payload) => {
+
+  },
+  queryObjects: (payload) => {
+
+  },
+  getAllObjects: (payload) => {
+
+  },
+  arrayOperations: (payload) => {
+
+  },
+}
