853b2864eaf3d0a07404c7a8503e788434f96344
brunofaefaion <bruno.fae.faion@gmail.com>
2020-04-21 11:15:00 -0300

    Fix firebase crash

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+}
+
+firebase.initializeApp(firebaseConfig)
+
+
 
 Vue.config.productionTip = false
 
