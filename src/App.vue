1ea48dedd03678a5db1ec2070c1af2faddfc6a2f
leoruas <leo.coelho.ruas@gmail.com>
2020-04-29 11:36:46 -0300

    turned toolbar into component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <template>
   <v-app>
-  <v-app-bar app>
-    <v-icon left>mdi-alpha-r-circle</v-icon>
-    <v-toolbar-title>Research Workflow</v-toolbar-title>
-
-    <v-spacer></v-spacer>
-
-    <v-btn text>
-      <v-icon left>mdi-lock</v-icon>
-      Sign-in</v-btn>
-  </v-app-bar>
-
- 
+   <Toolbar/>
+    
   <v-content>
     <v-container fluid>
       <router-view></router-view>
@@ -22,11 +12,15 @@
 </template>
 
 <script>
+import Toolbar from './components/atoms/Toolbar';
 
 export default {
   name: 'App',
   data: () => ({
     //
   }),
+  components: {
+    Toolbar
+  }
 };
 </script>
