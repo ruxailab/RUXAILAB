637ad812c53aad3091f2e06e7159cf195978c150
leoruas <leo.coelho.ruas@gmail.com>
2020-04-29 11:20:16 -0300

    updated toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <v-app>
   <v-app-bar app>
-    <!-- -->
+    <v-icon left>mdi-alpha-r-circle</v-icon>
+    <v-toolbar-title>Research Workflow</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn text>
+      <v-icon left>mdi-lock</v-icon>
+      Sign-in</v-btn>
   </v-app-bar>
 
  
