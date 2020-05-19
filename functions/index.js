67106192671889ec59569136585e4284b0dc7d93
Kevin Almeida <kevin.souza30@yahoo.com>
2020-05-19 11:20:10 -0300

    WIP - Added firebase function create user in data base

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
+admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.processSignUp = functions.auth.user().onCreate(async (user) =>{
+    try {
+        admin.firestore().collection('users').doc(user.uid).set({
+            email: user.email,
+            level: 0,
+            tokens:[]
+        })
+    } catch (err) {
+        console.error("Error to create user in database ",err)
+    }
+})
