b629d36403578945240c689518ce41c66ebf7d5a
Kevin Almeida <kevin.souza30@yahoo.com>
2020-05-21 12:25:19 -0300

    Added auto sign in

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 import database from '@/store/modules/database/database'
 import tests from '@/store/modules/database/tests'
 import auth from '@/store/modules/auth/auth'
-import users from '@/store/modules/auth/users'
+import users from '@/store/modules/database/users'
 
 Vue.use(Vuex)
 
--- src/store/modules/auth/auth.js
+++ src/store/modules/auth/auth.js
@@ -1,65 +1,83 @@
-import api from '@/api'
+import api from "@/api";
 
 export default {
-    state:{
-        user: null,
-        errorMessage: null
+  state: {
+    user: null,
+    errorMessage: null,
+  },
+  getters: {
+    user(state) {
+      return state.user;
     },
-    getters:{
-        user(state) {
-            return state.user;
-        },
-        errorMessage(state) {
-            return state.errorMessage;
-        }
+    errorMessage(state) {
+      return state.errorMessage;
     },
-    mutations:{
-        setUser(state, payload){
-            state.user = payload
-        },
-        setError(state, payload) {
-            state.errorMessage = payload;
-        }
+  },
+  mutations: {
+    setUser(state, payload) {
+      state.user = payload;
     },
-    actions:{
-        async signup({commit}, payload){
-            commit('setLoading', true);
-            try {
-                await api.auth.signUp(payload)
-                //await api.auth.logout()
-                console.log("User created :D! ihhaaa")
-                commit('setUser', payload);
-            } catch (err) {
-                console.error("Error when creating user :(", err)
-                commit('setError', err);
-            } finally { //Statements that are executed after the try statement completes. These statements execute regardless of whether an exception was thrown or caught.
-                commit('setLoading', false);
-            }
-        },
-        async signin({commit}, payload) {
-            commit('setLoading', true);
-            try {
-                var user = await api.auth.signIn(payload);
-                console.log("User Auth",user)
-                user = await api.database.getObject({collection: 'users', id: user.uid}); //ainda nao tem na database
-                user = Object.assign({uid: user.id}, user.data());   
-                console.log("User DB",user)  
-                commit('setUser', user);
-            } catch(err) {
-                console.error('Error signing in: ' + err);
-                commit('setError', err);
-            } finally { //Statements that are executed after the try statement completes. These statements execute regardless of whether an exception was thrown or caught.
-                commit('setLoading', false);
-            }
-        },
-        async logout({ commit }) {
-            try {
-              await api.auth.singOut();
-              console.log('Signed Out');
-              commit('setUser', null);
-            } catch (err) {
-              console.error('Error logging out.',err);
-            }
+    setError(state, payload) {
+      state.errorMessage = payload;
+    },
+  },
+  actions: {
+    async signup({ commit }, payload) {
+      commit("setLoading", true);
+      try {
+        await api.auth.signUp(payload);
+        //await api.auth.logout()
+        console.log("User created :D! ihhaaa");
+        commit("setUser", payload);
+      } catch (err) {
+        console.error("Error when creating user :(", err);
+        commit("setError", err);
+      } finally {
+        //Statements that are executed after the try statement completes. These statements execute regardless of whether an exception was thrown or caught.
+        commit("setLoading", false);
+      }
+    },
+    async signin({ commit }, payload) {
+      commit("setLoading", true);
+      try {
+        var user = await api.auth.signIn(payload);
+        user = await api.database.getObject({
+          collection: "users",
+          id: user.uid,
+        });
+        user = Object.assign({ uid: user.id }, user.data());
+        commit("setUser", user);
+      } catch (err) {
+        console.error("Error signing in: " + err);
+        commit("setError", err);
+      } finally {
+        //Statements that are executed after the try statement completes. These statements execute regardless of whether an exception was thrown or caught.
+        commit("setLoading", false);
+      }
+    },
+    async logout({ commit }) {
+      try {
+        await api.auth.singOut();
+        console.log("Signed Out");
+        commit("setUser", null);
+      } catch (err) {
+        console.error("Error logging out.", err);
+      }
+    },
+    async autoSignIn({ commit }) {
+      try {
+        var user = await api.auth.getCurrentUser();
+        if (user) {
+          user = await api.database.getObject({
+            collection: "users",
+            id: user.uid,
+          });
+          user = Object.assign({ uid: user.id }, user.data());
+          commit("setUser", user);
         }
-    }
-}
+      } catch (err) {
+        console.error("Error auto signing in ", err);
+      }
+    },
+  },
+};
