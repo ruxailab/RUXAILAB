d7a0ac3fa352fd409cf052bc5e554ae2bc1d0547
Kevin Almeida <kevin.souza30@yahoo.com>
2020-05-21 14:02:29 -0300

    Added access' control

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Public from "@/router/modules/public.js"
-import Admin from "@/router/modules/admin.js"
-import SuperAdmin from "@/router/modules/superAdmin.js"
-import {autoSignIn} from "@/router/tools.js"
+import Public from "@/router/modules/public.js";
+import Admin from "@/router/modules/admin.js";
+import SuperAdmin from "@/router/modules/superAdmin.js";
+//import { autoSignIn } from "@/router/tools.js";
+import store from "@/store/index.js"
 
 Vue.use(VueRouter);
 
-const routes = [
-  ...Public,
-  ...Admin,
-  ...SuperAdmin
-];
+const routes = [...Public, ...Admin, ...SuperAdmin];
 
 const router = new VueRouter({
   mode: "history",
@@ -19,15 +16,24 @@ const router = new VueRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
-  const user = autoSignIn()
-  console.log(user)
+router.beforeResolve(async (to, from, next) => {
+  const { authorize } = to.meta;
+  //const signin = autoSignIn();
+  if (authorize.length > 0 && to.path !== "/signin") {
+    //await signin
+    const user = store.state.auth.user
+    console.log(user)
+    
+    if(!user){  
+      return next('/signin')
+    }
+    if(!authorize.includes(user.accessLevel)){
+      console.error("Numpodi")
+    }
+    next()
+  }
+  console.log("haaaa")
   next()
-  to
-  from
-  
-  
- 
-})
+});
 
 export default router;
