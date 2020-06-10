8bc4c9d17d6bdfac60e5b900b8a7782b48cc443d
Karine Pistili <karine.pistili@gmail.com>
2020-06-10 11:14:01 -0300

    Fixed localeChanger style

--- src/components/atoms/LocaleChanger.vue
+++ src/components/atoms/LocaleChanger.vue
@@ -1,15 +1,20 @@
 <template>
-  <div>
+<v-col cols="12" lg="2" md="2" sm="3" xs="2">
     <v-select
-      class="pt-6"
-      append-icon="mdi-translate"
+      class="pt-7"
+      prepend-inner-icon="mdi-translate"
       :items="languages"
       item-text="label"
       item-value="value"
       :label="$t('language')"
       v-model="lang"
+      solo
+      dense
+      flat
+      light
+      background-color="grey lighten-4"
     ></v-select>
-  </div>
+</v-col>
 </template>
 
 <script>
