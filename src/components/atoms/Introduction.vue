4959bad246e0b8e5dc403b3b2da40f750955c55e
leoruas <leo.coelho.ruas@gmail.com>
2020-06-15 14:51:47 -0300

    get started button now sends to /signup

--- src/components/atoms/Introduction.vue
+++ src/components/atoms/Introduction.vue
@@ -13,7 +13,7 @@
                 style="width: 80%"
                 align="justify"
               >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>
-              <v-btn color="white" outlined rounded class="mb-2">Get started</v-btn>
+              <v-btn color="white" outlined rounded class="mb-2" @click="goTo('/signup')">Get started</v-btn>
             </v-col>
           </v-row>
         </v-container>
@@ -89,3 +89,16 @@ section {
   position: relative;
 }
 </style>
+
+<script>
+export default {
+    data: () => ({
+
+    }),
+    methods: {
+        goTo(path) {
+            this.$router.push(path)
+        }
+    }
+}
+</script>
