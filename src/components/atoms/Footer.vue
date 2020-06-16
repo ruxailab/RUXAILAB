68d8e799875286b1e02f84c7dc91c45bdc18e2b7
leoruas <leo.coelho.ruas@gmail.com>
2020-06-16 11:38:26 -0300

    added footer to landing page view

---
+++ src/components/atoms/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-footer dark padless>
+    <v-card flat tile class="indigo lighten-1 white--text text-center">
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+          @click="sendTo(icon.linkTo)"
+        >
+          <v-icon size="24px">{{ icon.icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text
+        class="white--text pt-0"
+      >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} —
+        <strong>Research Workflow</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: [
+      {icon: "mdi-facebook", linkTo: 'https://www.facebook.com'},
+      {icon: "mdi-twitter", linkTo: 'https://www.twitter.com'},
+      { icon: "mdi-instagram", linkTo: "https://www.instagram.com/lgfacens/" }
+    ]
+  }),
+  methods: {
+    sendTo(link) {
+      window.open(link);
+    }
+  }
+};
+</script>
--- src/components/molecules/LandingAbout.vue
+++ src/components/molecules/LandingAbout.vue
@@ -1,29 +1,34 @@
 <template>
-  <v-container fluid class="background-blob">
-    <AboutTop />
-    <AboutBottom />   
-  </v-container>
+  <div>
+    <v-container fluid class="background-blob">
+      <AboutTop />
+      
+      <AboutBottom />
+    </v-container>
+
+    <Footer />
+  </div>
 </template>
 
 <style scoped>
 .background-blob {
-  background-image: url('https://i.ibb.co/C7F8Vmq/blob.png');
-  /* 'https://i.ibb.co/zZh2fwN/blob.png' */
-  /* url(https://assets-global.website-files.com/5af97a9c84ec1bc79d81b5f4/5af9951ea9b84ec337b92251_Oval2.svg); */
+  background-image: url("https://i.ibb.co/C7F8Vmq/blob.png");
   background-size: contain;
   background-position: center;
 }
 </style>
 
 <script>
-import AboutTop from '@/components/atoms/AboutTop';
-import AboutBottom from '@/components/atoms/AboutBottom';
+import AboutTop from "@/components/atoms/AboutTop";
+import AboutBottom from "@/components/atoms/AboutBottom";
+import Footer from "@/components/atoms/Footer";
 
 export default {
-    data: () => ({}),
-    components: {
-      AboutTop,
-      AboutBottom
-    }
-}
+  data: () => ({}),
+  components: {
+    AboutTop,
+    AboutBottom,
+    Footer
+  }
+};
 </script>
