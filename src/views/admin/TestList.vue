45e1955a2028265921a251a05439282d9b7be8c0
leoruas <leo.coelho.ruas@gmail.com>
2020-06-19 14:08:40 -0300

    changed TestList View

--- src/views/admin/TestList.vue
+++ src/views/admin/TestList.vue
@@ -1,38 +1,44 @@
 <template>
-  <v-container>
-    <v-row justify="center">
+  <v-container style="display:contents">
+    <v-row justify="center" class="fill-height background-orange background-img">
       <v-col cols="10">
         <v-text-field label="Search" prepend-inner-icon="mdi-magnify" outlined></v-text-field>
-      </v-col>
-      <v-col cols="10">
-        <v-tabs  centered>
-          <v-tab>My Tests</v-tab>
-          <v-tab>Tests I colaborate with</v-tab>
-          <v-tab>My Answers</v-tab>
-
-          <v-tab-item class="grid">
-            <v-row>
-              <v-col v-for="test in user.myTests" :key="test.id">
-                <CardTest :test="test"></CardTest>
-              </v-col>
-            </v-row>
-          </v-tab-item>
-          <v-tab-item class="grid">
-            <v-row>
-              <v-col v-for="test in user.myCoops" :key="test.id">
-                <CardTest :test="test"></CardTest>
-              </v-col>
-            </v-row>
-          </v-tab-item>
-          <v-tab-item class="grid">
-            <v-row>
-              <v-col v-for="test in user.myAnswers" :key="test.id">
-                <CardTest :test="test"></CardTest>
-              </v-col>
-            </v-row>
-          </v-tab-item>
+        <v-tabs centered background-color="transparent" color="grey darken-2">
+          <v-tab @click="index = 0">My Tests</v-tab>
+          <v-tab @click="index = 1">Tests I colaborate with</v-tab>
+          <v-tab @click="index = 2">My Answers</v-tab>
         </v-tabs>
+
+        <!-- My Tests -->
+        <!-- Coloquei varias vezes so pra ver como fica com scroll -->
+        <v-row justify="center" v-if="index == 0" class="grid">
+          <v-row v-for="n in 20" :key="n">
+            <v-col v-for="test in user.myTests" :key="test.id">
+              <CardTest :item="test"></CardTest>
+            </v-col>
+          </v-row>
+
+        </v-row>
+
+        <!-- Tests I Colaborate With -->
+        <v-row justify="center" v-if="index == 1" class="grid">
+          <v-col v-for="test in user.myCoops" :key="test.id">
+            <CardTest :item="test"></CardTest>
+          </v-col>
+          <v-col v-for="test in user.myCoops" :key="test.id">
+            <CardTest :item="test"></CardTest>
+          </v-col>
+        </v-row>
+
+        <!-- My Answers -->
+        <v-row justify="center" v-if="index == 2" class="grid">
+          <v-col v-for="test in user.myAnswers" :key="test.id">
+            <CardTest :item="test"></CardTest>
+          </v-col>
+        </v-row>
       </v-col>
+
+      <v-card></v-card>
     </v-row>
 
     <v-btn large dark fab fixed bottom right @click="createTest()">
@@ -52,6 +58,7 @@ export default {
     x: 0,
     y: 0,
     test: {},
+    index: 0,
     headers: [
       {
         text: "Title",
@@ -102,10 +109,35 @@ export default {
   margin: 10px;
 }
 .grid {
-  margin-top: 100px;
+  margin-top: 60px;
 }
 .img {
   opacity: 0.6;
   transform: rotate(20deg);
+}
+.background-orange {
+  background-color: #f9a826;
+  height: 100%;
+}
+.background-img {
+  position: relative;
+  z-index: 1;
+}
+.background-img:before {
+  content: "";
+  position: fixed;
+  z-index: -1;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  opacity: 0.5;
+  background-image: url(../../assets/toDoTest.svg);
+  background-size: 700px;
+  background-position-x: right;
+  background-position-y: bottom;
+  margin-right: 30px;
+  /* background-position: 150% 800%; */
+  background-repeat: no-repeat;
 }
 </style>
