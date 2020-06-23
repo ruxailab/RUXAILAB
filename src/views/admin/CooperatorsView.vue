6490239f95c201b82e4c606574996e57ddada0b4
Kevin Almeida <kevin.souza30@yahoo.com>
2020-06-23 14:31:56 -0300

    Added Backgound

--- src/views/admin/CooperatorsView.vue
+++ src/views/admin/CooperatorsView.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="background"></div>
+  <v-container style="display:contents">
+    <v-row class="fill-height background">
+      <v-col>
+        <v-card shaped class="card">
+          <v-card-title>Haaaaaa</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style scoped>
 .background {
   position: absolute;
-  left: -323px;
-  top: -725px;
+
   background: url(../../assets/BackgroundShape.png),
     url(../../assets/BackgroundShapeBottom.png);
-  background-position: top left, bottom right;
+  background-position: -525px -450px, 0px 0px;
   background-repeat: no-repeat;
-  background-origin: border-box;
   background-size: cover;
 }
-
 </style>
