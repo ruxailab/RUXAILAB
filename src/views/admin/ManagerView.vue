d26c395dae32dbad0a3cbf5f4079080ea12661ef
Kevin Almeida <kevin.souza30@yahoo.com>
2020-06-23 13:22:34 -0300

    Cleared code

--- src/views/admin/ManagerView.vue
+++ src/views/admin/ManagerView.vue
@@ -1,24 +1,5 @@
 <template>
   <div class="fill-height background">
-    <!-- <v-tabs v-model="tab" centered grow>
-      <v-tab @click="setTest()">Test</v-tab>
-      <v-tab @click="setReport()">Reports</v-tab>
-      <v-tab @click="setAnswer()">Answer</v-tab>
-    </v-tabs>
-
-    <v-tabs-items v-model="tab">
-      <v-tab-item>
-        <v-row justify="center" align="center">
-          <h1>{{test.title}}</h1>
-        </v-row>
-      </v-tab-item>
-      <v-tab-item>
-        <router-view />
-      </v-tab-item>
-      <v-tab-item>
-        <router-view />
-      </v-tab-item>
-    </v-tabs-items>-->
     <v-container>
       <v-row class="fill-height" justify="center" align="center" dense>
         <v-col cols="4" v-for="(card,index) in cards" :key="index">
@@ -37,47 +18,8 @@ import CardManager from "@/components/atoms/CardManager";
 export default {
   props: ["id"],
   data: () => ({
-    items: [],
-    drawer: false,
-    group: null,
-    test: null,
-    tab: null,
     cards: []
   }),
-  methods: {
-    setReport() {
-      if (this.items.length > 1) this.items.pop();
-      this.items.push({
-        text: "Report",
-        disabled: true,
-        href: `/reportview/${this.test.reports}`
-      });
-      this.$router.push("/reportview/" + this.test.reports);
-    },
-    setAnswer() {
-      if (this.items.length > 1) this.items.pop();
-      this.items.push({
-        text: "Answer",
-        disabled: true,
-        href: `/answerview/${this.test.answers}`
-      });
-      this.$router.push("/answerview/" + this.test.answers);
-    },
-    setTest() {
-      if (this.items.length > 1) this.items.pop();
-      this.items.push({
-        text: "Test",
-        disabled: true,
-        href: `/managerView/${this.id}`
-      });
-      this.$router.push(`/managerView/${this.id}`);
-    }
-  },
-  watch: {
-    group() {
-      this.drawer = false;
-    }
-  },
 
   created() {
     this.test = Object.assign(
@@ -85,19 +27,29 @@ export default {
       this.$store.state.auth.user.myTests.find(test => test.id == this.id)
     );
 
-    this.items.push({
-      text: "Test",
-      disabled: false,
-      href: `/managerView/${this.id}`
-    });
-
     this.cards = [
-      { title:'Preview', url: "Preview.svg", path: `/testview/${this.test.id}` },
-      { title:'Reports',url: "Reports.svg", path: `/reportview/${this.test.reports}` },
-      { title:'Answers', url: "Answers.svg", path: `/answerview/${this.test.answers}` },
-      { title:'Cooperators', url: "Coops.svg", path: `/coopsview/${this.test.id}` },
-      { title:'Edit',url: "Edit.svg", path: `/edittest/${this.test.id}` }
-    ]
+      {
+        title: "Preview",
+        url: "Preview.svg",
+        path: `/testview/${this.test.id}`
+      },
+      {
+        title: "Reports",
+        url: "Reports.svg",
+        path: `/reportview/${this.test.reports}`
+      },
+      {
+        title: "Answers",
+        url: "Answers.svg",
+        path: `/answerview/${this.test.answers}`
+      },
+      {
+        title: "Cooperators",
+        url: "Coops.svg",
+        path: `/coopsview/${this.test.id}`
+      },
+      { title: "Edit", url: "Edit.svg", path: `/edittest/${this.test.id}` }
+    ];
   },
   components: {
     CardManager
