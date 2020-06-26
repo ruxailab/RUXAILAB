12060a70c0a4ba2291faaac1151a580337be9ef0
Kevin Almeida <kevin.souza30@yahoo.com>
2020-06-26 12:56:37 -0300

    WIP - Layout manager view

--- src/views/admin/ManagerView.vue
+++ src/views/admin/ManagerView.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="fill-height background">
+  <v-container class="pa-0 ma-0" fluid>
+    <v-row class="background" dense>
+      <v-navigation-drawer
+        class="nav"
+        v-model="drawer"
+        :mini-variant.sync="mini"
+        permanent
+        color="#3F3D56"
+      >
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-title>John Leider</v-list-item-title>
+
+          <v-btn icon @click.stop="mini = !mini">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list dense>
+          <v-list-item v-for="item in items" :key="item.title" link>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-col class="pa-0 ma-0">
+        <div class="background-top"></div>
+      </v-col>
+    </v-row>
+  </v-container>
+  <!-- <div class="fill-height background">
     <v-container>
       <v-row class="fill-height" justify="center" align="center" dense>
         <v-col cols="4" v-for="(card,index) in cards" :key="index">
@@ -9,16 +49,22 @@
         </v-col>
       </v-row>
     </v-container>
-  </div>
+  </div>-->
 </template>
 
 <script>
-import CardManager from "@/components/atoms/CardManager";
+// import CardManager from "@/components/atoms/CardManager";
 
 export default {
   props: ["id"],
   data: () => ({
-    cards: []
+    drawer: true,
+    items: [
+      { title: "Home", icon: "mdi-home-city" },
+      { title: "My Account", icon: "mdi-account" },
+      { title: "Users", icon: "mdi-account-group-outline" }
+    ],
+    mini: true
   }),
 
   created() {
@@ -52,23 +98,25 @@ export default {
     ];
   },
   components: {
-    CardManager
+    // CardManager
   }
 };
 </script>
 
 <style>
+.background-top {
+  background-color: #fca326;
+  height: 45%;
+  width: 100%;
+}
 .background {
-  position: absolute;
-  left: 0%;
-  right: 0%;
-  top: 0%;
-  bottom: 0%;
-
-  background: url(../../assets/BackgroundShape.png), #dfdfdf;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-origin: border-box;
-  background-size: cover;
+  background-color: #e8eaf2;
+  height: 100vh;
+}
+.nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  overflow-y: none;
 }
 </style>
