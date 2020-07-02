6867666a728cd1aaef7df6e58ef263f665aa0372
Kevin Almeida <kevin.souza30@yahoo.com>
2020-07-02 14:42:40 -0300

    WIP - added card create

--- src/components/atoms/CardNewTest.vue
+++ src/components/atoms/CardNewTest.vue
@@ -1,20 +1,49 @@
 <template>
-<v-hover v-slot:default="{ hover }">
-  <v-card :elevation="hover ? 24 : 10"  shaped class="card" @click="createTest()">
-    <v-card-text>
-      <v-row class="fill-height" justify="center" align="center">
-        <v-icon color="#f9a826" size="150">mdi-plus-circle-outline</v-icon>
-      </v-row>
-      <v-row class="fill-height" justify="center" align="center">
-        <p>New Test</p>
-      </v-row>
-    </v-card-text>
-  </v-card>
-</v-hover>
+  <div>
+    <v-hover v-slot:default="{ hover }">
+      <v-card :elevation="hover ? 24 : 10" shaped class="card" @click="dialog = true">
+        <v-card-text>
+          <v-row class="fill-height" justify="center" align="center">
+            <v-icon color="#f9a826" size="150">mdi-plus-circle-outline</v-icon>
+          </v-row>
+          <v-row class="fill-height" justify="center" align="center">
+            <p>New Test</p>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-hover>
+    <v-dialog v-model="dialog" max-width="80%">
+      <v-card color="#e8eaf2">
+        <v-container>
+          <p class="titleView ma-2 pa-2">Create Test</p>
+          <v-divider></v-divider>
+          <FormTestDescription :test="test" ref="form1" />
+          <v-card-actions class="ma-0 pa-2">
+            <v-spacer></v-spacer>
+            <v-btn color="black" text @click="dialog = false">Cancel</v-btn>
+            <v-btn color="#F9A826" @click="dialog = false">Create</v-btn>
+          </v-card-actions>
+        </v-container>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
+import FormTestDescription from "@/components/atoms/FormTestDescription";
+
 export default {
+  components: {
+    FormTestDescription
+  },
+  data: () => ({
+    dialog: false,
+    test: {
+      title: "",
+      description: "",
+      type: ""
+    }
+  }),
   methods: {
     createTest() {
       this.$router.push("/createtest");
@@ -24,6 +53,16 @@ export default {
 </script>
 
 <style>
+.titleView {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 60px;
+  line-height: 70px;
+  display: flex;
+  align-items: center;
+  color: #000000;
+}
 .card {
   max-width: 330px;
   height: 200px;
