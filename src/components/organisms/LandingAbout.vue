8176543c6f432704653b8c0bb6f5741b523e1f6e
leoruas <leo.coelho.ruas@gmail.com>
2020-07-06 12:18:10 -0300

    LandingAbout component is now responsive

--- src/components/organisms/LandingAbout.vue
+++ src/components/organisms/LandingAbout.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-container fluid>
-    <v-row justify="space-around" align="center" class="my-10">
+  <v-container fluid class="background-blob-desktop">
+    <v-row justify="space-around" align="center" class="my-10 background-blob-mobile1">
       <h4
         class="display-1 text-center mb-3 mx-1 hidden-md-and-up"
       >Quickly get feedback on your projects</h4>
@@ -27,8 +27,8 @@
       </v-col>
     </v-row>
 
-    <v-row justify="space-around" align="center" class="my-10">
-      <v-col cols="12" sm="5">
+    <v-row justify="space-around" align="center" class="my-10 background-blob-mobile2">
+      <v-col cols="12" md="5">
         <h4 class="display-1 text-left mb-3 hidden-sm-and-down">Start your own UX LAB</h4>
         <h4 class="display-1 text-center mb-3 hidden-md-and-up">Start your own UX LAB</h4>
 
@@ -50,7 +50,7 @@
         </v-row>
       </v-col>
 
-      <v-col cols="12" sm="5" class="hidden-sm-and-down">
+      <v-col cols="12" md="5" class="hidden-sm-and-down">
         <v-img src="@/assets/aboutBottom.png"></v-img>
       </v-col>
     </v-row>
@@ -58,15 +58,32 @@
 </template>
 
 <style scoped>
-.background-blob {
+.background-blob-desktop {
   background-image: url("https://i.ibb.co/C7F8Vmq/blob.png");
   background-size: contain;
   background-position: center;
 }
+.background-blob-mobile1 {
+  background-image: none;
+}
+.background-blob-mobile2 {
+  background-image: none;
+}
 
-/* @media screen and (max-width: 960px) {
-  .background-blob {
-    background-color: olive;
+@media screen and (max-width: 960px) {
+  .background-blob-desktop {
+    background-image: none;
   }
-} */
+  .background-blob-mobile1 {
+  background-image: url("https://i.ibb.co/C7F8Vmq/blob.png");
+  background-size: contain;
+  background-position: center;
+  }
+  .background-blob-mobile2 {
+  background-image: url("https://i.ibb.co/hgZWz0Z/blob2.png");
+  background-size: contain;
+  background-position: center;
+  
+  }
+}
 </style>
