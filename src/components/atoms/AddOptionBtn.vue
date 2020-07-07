54a6719b699745cc8d886382188a3abc85166a26
leoruas <leo.coelho.ruas@gmail.com>
2020-07-07 11:28:27 -0300

    Added options tab to EditTest.vue

---
+++ src/components/atoms/AddOptionBtn.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <v-btn
+      rounded
+      color="#f9a826"
+      class="white--text"
+      small
+      @click="$emit('dialog', true)"
+    >Add a new Option</v-btn>
+
+    <v-dialog width="500" v-model="dialog" persistent>
+      <v-card class="dataCard">
+        <p class="subtitleView ma-3 pt-3 mb-0 pa-2">Add a new Option</p>
+        <v-divider></v-divider>
+        <v-row justify="center" class="ma-0">
+          <v-col cols="11">
+            <v-form ref="form">
+              <v-row justify="center" align="center">
+                <v-col cols="9">
+                  <v-text-field v-model="option.text" label="Text" :rules="textRequired"></v-text-field>
+                </v-col>
+
+                <v-col cols="2">
+                  <v-text-field
+                    v-model.number="option.value"
+                    label="Value"
+                    :disabled="!hasValue"
+                    type="number"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
+              <v-row justify="center">
+                <v-checkbox v-model="hasValue" label="Has Value"></v-checkbox>
+              </v-row>
+            </v-form>
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            @click="$emit('dialog', false), resetVal()"
+            small
+            text
+            color="red lighten-1 white--text"
+          >Cancel</v-btn>
+
+          <v-btn @click="validate()" small color="#f9a826" class="white--text">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["option", "dialog"],
+  data: () => ({
+    id: 0,
+    hasValue: true,
+    textRequired: [v => !!v || "Text is required"]
+  }),
+  methods: {
+    validate() {
+      if (this.$refs.form.validate()) {
+        if (this.hasValue && !this.option.value) {
+          alert("Please enter a value for this option.");
+        } else {
+          if(!this.hasValue) {
+            this.option.value = null
+          }
+
+          this.$emit("dialog", false);
+          this.$emit("addOption");
+          this.resetVal();
+        }
+      }
+    },
+    resetVal() {
+      this.$refs.form.resetValidation();
+    }
+  },
+  watch: {
+    dialog() {
+      if(!this.dialog) {
+        this.hasValue = true;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.subtitleView {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 200;
+  font-size: 18.1818px;
+  align-items: flex-end;
+  color: #000000;
+  margin-bottom: 4px;
+  padding-bottom: 2px;
+}
+
+.dataCard {
+  background: #f5f7ff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+}
+</style>
---
+++ src/components/molecules/OptionsTable.vue
@@ -0,0 +1,137 @@
+<template>
+  <v-row class="ma-0 pa-0">
+    <v-col class="pt-0">
+      <v-data-table
+        height="420px"
+        class="dataCard"
+        :headers="headers"
+        :items="options"
+        :items-per-page="5"
+      >
+        <template v-slot:top>
+          <v-row class>
+            <v-col class="ml-2 mb-1 pa-4 pb-0">
+              <p class="subtitleView">Current Options</p>
+            </v-col>
+            <v-col class="mr-2 mb-1 pb-0 pa-4">
+              <v-row justify="end" class="ma-0 pa-0">
+                <AddOptionBtn
+                  :option="option"
+                  :dialog="dialog"
+                  @addOption="updateOptions"
+                  @dialog="changeDialog"
+                />
+              </v-row>
+            </v-col>
+          </v-row>
+          <v-divider class="mb-4"></v-divider>
+        </template>
+
+        <template v-slot:item.actions="{ item }">
+          <!-- table actions -->
+          <v-row justify="end" class="pr-1">
+            <v-btn icon small class="mr-2" @click="editItem(item)">
+              <v-icon small>mdi-pencil</v-icon>
+            </v-btn>
+            <v-btn icon small @click="deleteItem(item)">
+              <v-icon small>mdi-delete</v-icon>
+            </v-btn>
+          </v-row>
+        </template>
+      </v-data-table>
+    </v-col>
+  </v-row>
+</template>
+
+
+<script>
+import AddOptionBtn from "../atoms/AddOptionBtn";
+
+export default {
+  props: ["options"],
+  data: () => ({
+    headers: [
+      {
+        text: "Text",
+        align: "start",
+        value: "text"
+      },
+      { text: "Edit/Delete", value: "actions", align: "end", sortable: false },
+    ],
+    option: {
+      text: '',
+      value: null
+    },
+    dialog: false,
+    editIndex: -1
+  }),
+  methods: {
+    updateOptions() {
+      if (this.editIndex == -1) {
+        this.options.push(this.option);
+      } else {
+        Object.assign(this.options[this.editIndex], this.option);
+        this.editIndex = -1;
+      }
+      
+      this.option = {
+        text: '',
+        value: null
+      }
+    },
+    changeDialog(payload) {
+      this.dialog = payload;
+    },
+    deleteItem(item) {
+      this.options.splice(this.options.indexOf(item), 1);
+    },
+    editItem(item) {
+      this.editIndex = this.options.indexOf(item);
+      this.option.text = this.options[this.editIndex].text;
+      this.option.value = this.options[this.editIndex].value;
+      
+      this.dialog = true;
+    }
+  },
+  watch: {
+    dialog() {
+      if(!this.dialog) {
+        this.option = {
+          text: '',
+          value: null
+        }
+      }
+    },
+    options() {
+      console.log('options', this.options);
+      this.$emit("change");
+    }
+  },
+  components: {
+    AddOptionBtn
+  }
+};
+</script>
+
+
+<style scoped>
+.subtitleView {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 200;
+  font-size: 18.1818px;
+  align-items: flex-end;
+  color: #000000;
+  margin-bottom: 4px;
+  padding-bottom: 2px;
+}
+
+.dataCard {
+  background: #f5f7ff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+  margin: 10px;
+  padding-bottom: 10px;
+  min-height: 550px;
+}
+</style>
