01c2e11b4cc550ca1bd0ca7b0115418ccce41f91
leoruas <leo.coelho.ruas@gmail.com>
2020-07-14 11:57:07 -0300

    remade page not found

--- src/views/public/PageNotFoundView.vue
+++ src/views/public/PageNotFoundView.vue
@@ -1,13 +1,34 @@
 <template>
-  <h1>Error: 404 Page Not Found</h1>
+  <div class="background">
+    <v-row justify="center">
+      <v-col cols="12" md="5">
+        <v-img src="@/assets/pageNotFound.svg"></v-img>
+        <div class="text-center" style="font-size:50px; color: grey">Page Not Found</div>
+        <div
+          class="text-center"
+          style="font-size:15px; color: grey"
+        >We weren't able to find the page you were looking for.</div>
+        <v-row justify="center" class="mt-4">
+          <v-btn style="color: #f9a826" outlined rounded @click="sendHome()">Back Home</v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  methods: {
+    sendHome() {
+      this.$router.push("/testslist");
+    }
+  }
+};
 </script>
 
-<style>
-
+<style scoped>
+.background {
+  background-color: #f5f7ff;
+  height: 100vh;
+}
 </style>
