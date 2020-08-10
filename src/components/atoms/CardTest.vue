f9cb801171b5ec722544a18544a3abc66ae880dd
leoruas <leo.coelho.ruas@gmail.com>
2020-08-10 13:30:08 -0300

    changed menu to automatic redirection based on accesss level

--- src/components/atoms/CardTest.vue
+++ src/components/atoms/CardTest.vue
@@ -1,102 +1,54 @@
 <template>
   <div>
-    <!--
-    <Snackbar />
-    <Dialog :dialog="dialog" :text="dialogText">
-      <v-card-title slot="title" class="headline error white--text" primary-title>Are you sure you want to delete this test?</v-card-title>
-
-      <div slot="actions">
-        <v-btn class="grey lighten-3" text @click="dialog = false">Cancel</v-btn>
-        <v-btn
-          class="red white--text ml-1"
-          :loading="loading"
-          text
-          @click="deleteTest(item), loading = true"
-        >Delete</v-btn>
-      </div>
-    </Dialog>-->
     <v-hover v-slot:default="{ hover }">
-      <v-menu absolute offset-y style="max-width: 600px">
-        <template v-slot:activator="{ on, attrs }">
-          <v-card :elevation="hover ? 24 : 10" shaped class="card" v-bind="attrs" v-on="on">
-            <v-container>
-              <v-col cols="12" align-self="start">
-                <h4>{{item.title}}</h4>
+
+    <v-card :elevation="hover ? 24 : 10" shaped class="card" @click="redirect(item)">
+
+      <v-container>
+        <v-col cols="12" align-self="start">
+          <h4>{{item.title}}</h4>
+        </v-col>
+
+        <!-- Desktop/Tablet -->
+        <v-row align="center">
+          <div class="hidden-sm-and-down">
+            <v-row class="bottomStart">
+              <v-col>
+                <v-icon x-large>mdi-account-circle</v-icon>
+                <v-btn color="grey darken-3 white--text" disabled rounded :ripple="false">{{item.type}}</v-btn>
+                <div class="text-center caption">Created  {{item.date || '~no date~'}}</div>
+
               </v-col>
+            </v-row>
+          </div>
+        </v-row>
 
-              <!-- Desktop/Tablet -->
-              <v-row align="center">
-                <div class="hidden-sm-and-down">
-                  <v-row class="bottomStart">
-                    <v-col>
-                      <v-icon x-large>mdi-account-circle</v-icon>
-                      <v-btn
-                        color="grey darken-3 white--text"
-                        disabled
-                        rounded
-                        :ripple="false"
-                      >{{item.type}}</v-btn>
-                      <div class="text-center caption">Created {{item.date}}</div>
-                    </v-col>
-                  </v-row>
-                </div>
-              </v-row>
+        <!-- Mobile -->
+        <v-row justify="center">
+          <div class="hidden-md-and-up bottomMobile">
+            <v-row class="ma-1" justify="space-between">
+              <v-icon x-large>mdi-account-circle</v-icon>
+              <v-btn color="grey darken-3 white--text" rounded :ripple="false">{{item.type}}</v-btn>
+            </v-row>
 
-              <!-- Mobile -->
-              <v-row justify="center">
-                <div class="hidden-md-and-up bottomMobile">
-                  <v-row class="ma-1" justify="space-between">
-                    <v-icon x-large>mdi-account-circle</v-icon>
-                    <v-btn color="grey darken-3 white--text" rounded :ripple="false">{{item.type}}</v-btn>
-                  </v-row>
+            <v-row class="mt-2" justify="center">
+              <div class="text-center caption">{{new Date().toDateString()}}</div>
+            </v-row>
+          </div>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-hover>
 
-                  <v-row class="mt-2" justify="center">
-                    <div class="text-center caption">{{new Date().toDateString()}}</div>
-                  </v-row>
-                </div>
-              </v-row>
-            </v-container>
-          </v-card>
-        </template>
-
-        <v-list dense>
-          <v-list-item v-if="accessLevel < 2" @click="openManager(item)">
-            <v-list-item-icon>
-              <v-icon>mdi-cog</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>Manager</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-
-          <v-list-item @click="openTest(item)">
-            <v-list-item-icon>
-              <v-icon v-if="accessLevel <= 1">mdi-file-eye</v-icon>
-              <v-icon v-else>mdi-file-document-edit</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>{{accessLevel == 2 ? 'Open Test' : 'Preview Test'}}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          
-        </v-list>
-      </v-menu>
-    </v-hover>
   </div>
 </template>
 
+
 <script>
-//
-// import Snackbar from "@/components/atoms/Snackbar";
-// import Dialog from "@/components/atoms/Dialog";
+
 
 export default {
   props: ["item", "accessLevel"],
-  //
-  // components: {
-  //   Snackbar,
-  //   Dialog,
-  // },
   data: () => ({
     menu: false,
     dialog: false,
@@ -176,6 +128,10 @@ export default {
         path: "/managerview/" + test.id,
       });
     },
+    redirect(test) {
+      if(this.accessLevel <= 1) this.openManager(test);
+      else this.openTest(test);
+    }
   },
   computed: {
     user() {
