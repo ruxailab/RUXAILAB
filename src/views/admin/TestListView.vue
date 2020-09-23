498aacb770ea5aa53d2f98125f11a8ec5e942184
leoruas <leo.coelho.ruas@gmail.com>
2020-09-23 09:47:07 -0300

    clean TestListView

--- src/views/admin/TestListView.vue
+++ src/views/admin/TestListView.vue
@@ -25,6 +25,7 @@
     <div>
       <v-row justify="center" class="fill-height">
         <v-col cols="10">
+          <!-- Mobile search button -->
           <v-row align="center" v-if="!searching">
             <span class="titleText ml-3">Tests</span>
             <v-spacer></v-spacer>
@@ -95,48 +96,35 @@
 
 
 <script>
-// import CardTest from "@/components/atoms/CardTest";
-// import NewTestBtn from "@/components/atoms/NewTestBtn";
 import Snackbar from "@/components/atoms/Snackbar";
 import List from "@/components/atoms/ListTests";
 
 export default {
+  components: {
+    Snackbar,
+    List
+  },
   data: () => ({
-    showMenu: false,
-    label: "My Tests",
-    test: {},
     search: "",
     index: 0,
     searching: false,
-    headers: [
-      {
-        text: "Title",
-        align: "start",
-        value: "title",
-      },
-      { text: "Id", value: "id", align: "center" },
-      { text: "Type", value: "type", align: "center" },
-      { text: "Edit", value: "edit", align: "center", sortable: false },
-      { text: "Delete", value: "delete", align: "center", sortable: false },
-      { text: "More", value: "more", align: "center", sortable: false },
-    ],
-    items: [
-      { title: "Open Test" },
-      { title: "Open Answers" },
-      { title: "Open Manager" },
-    ],
     buttonItems: [
       { text: "My Tests", value: 0 },
       { text: "Tests I colaborate with", value: 1 },
       { text: "My Answers", value: 2 },
     ],
   }),
+  methods: {
+    pushCreate() {
+      this.$router.push("/createtest");
+    },
+    goTo(test) {
+        this.$router.push((test.accessLevel <= 1 ? "/managerview/" : "/testview/") + test.id)
+    },
+  },
   computed: {
     user() {
       return this.$store.state.auth.user;
-    },
-    loading() {
-      return this.$store.state.tests.loading;
     },
     filteredMyTests() {
       return this.user.myTests.filter((test) => {
@@ -153,95 +141,13 @@ export default {
         return test.title.toLowerCase().includes(this.search.toLowerCase());
       });
     },
-  },
-  watch: {
-    index() {
-      if (this.index == 0) {
-        this.label = "My Tests";
-      } else if (this.index == 1) {
-        this.label = "Tests I colaborate with";
-      } else {
-        this.label = "My Answers";
-      }
-    },
-  },
-  components: {
-    // CardTest,
-    // NewTestBtn,
-    Snackbar,
-    List,
-  },
-  methods: {
-    pushCreate() {
-      this.$router.push("/createtest");
-    },
-    goTo(test) {
-        this.$router.push((test.accessLevel <= 1 ? "/managerview/" : "/testview/") + test.id)
-    },
   }
 };
 </script>
 
 <style scoped>
-.button {
-  margin: 10px;
-}
-.grid {
-  margin-top: 30px;
-}
-.img {
-  opacity: 0.6;
-  transform: rotate(20deg);
-}
-.background-orange {
-  background-color: #f9a826;
-  height: 100%;
-}
-.background-img {
-  position: relative;
-  z-index: 1;
-}
-.background-img:before {
-  content: "";
-  position: absolute;
-  z-index: -1;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-image: url(../../assets/toDoTest.svg);
-
-  /* background-position: 150% 800%; */
-  background-repeat: no-repeat;
-
-  background-size: 300px auto;
-  background-position: right 0px bottom 0px;
-  transition: opacity 0.15s cubic-bezier(0.4, 0, 0.2, 1);
-}
 .titleText {
   font-size: 40px;
   font-weight: 300;
-}
-
-@media screen and (max-width: 960px) {
-  /*sm */
-  .background-img:before {
-    content: "";
-    position: absolute;
-    z-index: -1;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-image: url(../../assets/toDoTest.svg);
-    opacity: 70%;
-
-    /* background-position: 150% 800%; */
-    background-repeat: no-repeat;
-
-    background-size: 300px auto;
-    background-position: right 0px bottom 0px;
-    transition: opacity 0.15s cubic-bezier(0.4, 0, 0.2, 1);
-  }
 }
 </style>
