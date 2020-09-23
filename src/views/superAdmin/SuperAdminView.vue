822c8bf2384e1c970a3fd6f14bee664554b7feaa
leoruas <leo.coelho.ruas@gmail.com>
2020-09-23 11:41:56 -0300

    removed Dialog component from SuperAdminView

--- src/views/superAdmin/SuperAdminView.vue
+++ src/views/superAdmin/SuperAdminView.vue
@@ -1,31 +1,48 @@
 <template>
   <v-content>
     <Snackbar></Snackbar>
-    <Dialog :dialog="dialogDel" :text="dialogText">
-      <v-card-title
-        slot="title"
-        class="headline error white--text"
-        primary-title
-      >Are you sure you want to delete this test?</v-card-title>
 
-      <div slot="actions">
-        <v-btn class="grey lighten-3" text @click="dialogDel = false, userClicked = null">Cancel</v-btn>
-        <v-btn
-          class="red white--text ml-1"
-          text
-          @click="deleteUser(userClicked)"
-        >Delete</v-btn>
-      </div>
-    </Dialog>
-    
+    <!-- Delete Alert Dialog -->
+    <v-dialog v-model="dialogDel" width="600" persistent>
+      <v-card>
+        <v-card-title
+          class="headline error white--text"
+          primary-title
+        >Are you sure you want to delete this user?</v-card-title>
+
+        <v-card-text>{{ dialogText }}</v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn class="grey lighten-3" text @click="dialogDel = false, userClicked = null">Cancel</v-btn>
+          <v-btn class="red white--text ml-1" text @click="deleteUser(userClicked)">Delete</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
     <v-row align="center" justify="center">
       <v-col cols="10">
-        <v-data-table :search="search" :headers="headers" :items="users" class="elevation-1" :loading="loading">
+        <v-data-table
+          :search="search"
+          :headers="headers"
+          :items="users"
+          class="elevation-1"
+          :loading="loading"
+        >
           <template v-slot:top>
             <v-toolbar flat color="white">
               <v-toolbar-title>Users</v-toolbar-title>
             </v-toolbar>
-            <v-text-field outlined prepend-inner-icon="mdi-magnify" v-model="search" class="mx-3" dense label="Search"></v-text-field>
+            <v-text-field
+              outlined
+              prepend-inner-icon="mdi-magnify"
+              v-model="search"
+              class="mx-3"
+              dense
+              label="Search"
+            ></v-text-field>
           </template>
           <template v-slot:item.accessLevel="{ item }">
             <div>{{level(item.accessLevel)}}</div>
@@ -76,45 +93,43 @@
 </template>
 
 <script>
-import Dialog from "@/components/atoms/Dialog";
 import Snackbar from "@/components/atoms/Snackbar";
 
 export default {
   components: {
-    Dialog,
-    Snackbar
-  },  
+    Snackbar,
+  },
   data: () => ({
     dialog: false,
     dialogDel: false,
     userClicked: null,
-    search: '',
+    search: "",
     headers: [
       {
         text: "Id",
         align: "start",
-        value: "id"
+        value: "id",
       },
       { text: "E-mail", value: "email", align: "center" },
       { text: "Access Level", value: "accessLevel", align: "center" },
-      { text: "Actions", value: "actions", align: "end" }
+      { text: "Actions", value: "actions", align: "end" },
     ],
     editedIndex: -1,
     editedUser: {
       uid: "",
       email: "",
-      accessLevel: 0
+      accessLevel: 0,
     },
     defaultUser: {
       uid: "",
       email: "",
-      accessLevel: 0
+      accessLevel: 0,
     },
     accessLevels: [
       { text: "Super Admin", level: 0 },
       { text: "Admin", level: 1 },
-      { text: "User", level: 2 }
-    ]
+      { text: "User", level: 2 },
+    ],
   }),
   methods: {
     editUser(item) {
@@ -135,15 +150,15 @@ export default {
       let payload = {
         uid: user.id,
         customClaims: {
-          accessLevel: user.accessLevel
-        }
+          accessLevel: user.accessLevel,
+        },
       };
       this.$store.dispatch("updateLevel", { data: payload });
       this.close();
     },
     level(lv) {
       let text;
-      this.accessLevels.forEach(item => {
+      this.accessLevels.forEach((item) => {
         if (item.level == lv) {
           text = item.text;
         }
@@ -151,21 +166,29 @@ export default {
       return text;
     },
     deleteUser(user) {
-      this.dialogDel = false;      
-      this.$store.dispatch("deleteAuth", user).then(() => {
-        this.$store.dispatch("deleteUser", user).then(() => {
-          this.$store.commit("setSuccess", "Successfully deleted user " + this.userClicked.email);
-          this.userClicked = null
-        }).catch(err => {
+      this.dialogDel = false;
+      this.$store
+        .dispatch("deleteAuth", user)
+        .then(() => {
+          this.$store
+            .dispatch("deleteUser", user)
+            .then(() => {
+              this.$store.commit(
+                "setSuccess",
+                "Successfully deleted user " + this.userClicked.email
+              );
+              this.userClicked = null;
+            })
+            .catch((err) => {
+              console.error(err);
+              this.commit("setError", "Error deleting user");
+            });
+        })
+        .catch((err) => {
           console.error(err);
           this.commit("setError", "Error deleting user");
-        }) 
-        
-      }).catch(err => {
-        console.error(err);
-        this.commit("setError", "Error deleting user");
-      });
-    }
+        });
+    },
   },
   computed: {
     users() {
@@ -175,17 +198,21 @@ export default {
       return this.$store.state.tests.loading;
     },
     dialogText() {
-      return 'Are you sure you want to delete the user ' + (this.userClicked !== null ? this.userClicked.email : '') + "? This action can't be undone.";
-    }
+      return (
+        "Are you sure you want to delete the user " +
+        (this.userClicked !== null ? this.userClicked.email : "") +
+        "? This action can't be undone."
+      );
+    },
   },
   watch: {
     dialog(val) {
       val || this.close();
-    }
+    },
   },
   created() {
     if (!this.$store.state.users.users) this.$store.dispatch("getUsers", {});
-  }
+  },
 };
 </script>
 
