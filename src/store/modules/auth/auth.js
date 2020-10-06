524c578a65e27215ab28e9ea8957db0bf7206394
Kevin de Souza Almeida <kevin.souza30@yahoo.com>
2020-10-06 14:19:57 -0300

    Added comments  the auth

--- src/store/modules/auth/auth.js
+++ src/store/modules/auth/auth.js
@@ -15,6 +15,15 @@ export default {
     }
   },
   actions: {
+    /**
+     * This action sign up a user in platform
+     * 
+     * @action signup 
+     * @param {object} payload - Data to create a new user 
+     * @param {string} payload.email - the user email
+     * @param {string} payload.password - the user password 
+     * @returns {void}
+     */
     async signup({ commit }, payload) {
       commit("setLoading", true);
       try {
@@ -33,6 +42,15 @@ export default {
         commit("setLoading", false);
       }
     },
+    /**
+     * This action sign in the user the platform
+     * 
+     * @action signin=setUser
+     * @param {object} payload - Data to create a new user 
+     * @param {string} payload.email - the user email
+     * @param {string} payload.password - the user password 
+     * @returns {void}
+    */
     async signin({ commit }, payload) {
       commit("setLoading", true);
       try {
@@ -53,6 +71,12 @@ export default {
         commit("setLoading", false);
       }
     },
+    /**
+     * This action log out user of platform
+     * 
+     *  @action signin=[setUser=null]
+     *  @returns {void}
+     */
     async logout({ commit }) {
       try {
         await api.auth.singOut();
@@ -64,6 +88,13 @@ export default {
         commit("setLoading", false);
       }
     },
+    /**
+     * This action reconnect user automatically to platform when 
+     * reload or come in the page
+     *
+     *  @action signin=setUser
+     *  @returns {void}
+     */
     async autoSignIn({ commit }) {
       try {
         var user = await api.auth.getCurrentUser();
@@ -89,7 +120,22 @@ export default {
     setUser({ commit }, user) {
       commit("setUser", user);
     },
+    /**
+     * This action delete the user's authentication
+     * 
+     * @action deleteAuth
+     * @param {object} user - user's data
+     * @param {number} user.accessLevel - user acess permition 
+     * @param {string} user.collection -  local in database 
+     * @param {string} user.email - user's email
+     * @param {string} user.id -user's indentification
+     * @param {object[]} user.myAnswers - test list that user is repling
+     * @param {object[]} user.myCoops- test list that user is cooperator
+     * @param {object[]} user.myTests - user's test list 
+     * @param {object[]} user.notifications - notificatinons recived 
+     */
     async deleteAuth({ dispatch }, user) {
+      console.log(user)
       // await api.auth.deleteUserAuth(user);
       dispatch("callFunction", Object.assign({}, {
         function: 'deleteAuth',
