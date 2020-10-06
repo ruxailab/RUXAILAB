798a76fcb0b16aaff2d4194a670db82c73d2e3cd
leoruas <leo.coelho.ruas@gmail.com>
2020-10-06 11:59:21 -0300

    refactored cooperators store module with setLoading and setError

--- src/store/modules/database/cooperators.js
+++ src/store/modules/database/cooperators.js
@@ -14,87 +14,97 @@ export default {
     }
   },
   actions: {
-    createCooperators({ dispatch }, payload) {
+    createCooperators({ dispatch, commit }, payload) {
+      commit("setLoading", true);
       payload = Object.assign(payload, { collection: "cooperators" });
 
       let docId = dispatch("createObject", payload)
         .then((doc) => {
           return doc.id;
         })
-        .catch((err) => {
-          console.error("Error ", err);
-        });
+        .catch((err) => commit("setError", "Error in createCooperators." + err));
 
       return docId;
     },
     async getCooperators({ dispatch, commit }, payload) {
-      payload = Object.assign(payload, { collection: "cooperators" });
       commit("setLoading", true)
-      let coop = await dispatch("getObject", payload);
+      payload = Object.assign(payload, { collection: "cooperators" });
+
+      let coop = await dispatch("getObject", payload)
+        .catch((err) => commit("setError", "Error in getCooperators." + err));
+
       commit("setCooperators", coop);
-      commit("setLoading", false)
     },
-    pushCooperator({ dispatch }, payload) {
+    pushCooperator({ dispatch, commit }, payload) {
+      commit("setLoading", true);
       payload = Object.assign(payload, {
         collection: "cooperators",
         param: "cooperators",
       });
 
-      dispatch("pushObject", payload).catch((err) => {
-        console.error("Error pushing", err);
-      });
+      dispatch("pushObject", payload)
+        .catch((err) => commit("setError", "Error in pushCooperator." + err));
     },
-    updateCooperator({ dispatch }, payload) {
+    updateCooperator({ dispatch, commit }, payload) {
+      commit("setLoading", true);
       payload = Object.assign(payload, {
         collection: "cooperators",
         field: "cooperators",
       });
+
       if (!payload.identifier)
-        Object.assign(payload, { identifier: "id", })
-      dispatch("updateArrayElement", payload).catch((err) => {
-        console.error("Error updating element", err);
-      });
+        Object.assign(payload, { identifier: "id", });
+
+      dispatch("updateArrayElement", payload)
+        .catch((err) => commit("setError", "Error in updateCooperator." + err));
     },
-    updateCooperatorObject({ dispatch }, payload) {
+    updateCooperatorObject({ dispatch, commit }, payload) {
+      commit("setLoading", true);
       payload = Object.assign(payload, {
         collection: "cooperators",
         field: "cooperators",
       });
+
       if (!payload.identifier)
-        Object.assign(payload, { identifier: "id", })
-      dispatch("updateArrayElementObject", payload).catch((err) => {
-        console.error("Error updating element", err);
-      });
+        Object.assign(payload, { identifier: "id", });
+
+      dispatch("updateArrayElementObject", payload)
+        .catch((err) => commit("setError", "Error in updateCooperatorObject." + err));
     },
-    removeCooperator({ dispatch }, payload) {
+    removeCooperator({ dispatch, commit }, payload) {
+      commit("setLoading", true);
       payload = Object.assign(payload, {
         collection: "cooperators",
         param: "cooperators",
       });
-      dispatch("removeObject", payload).catch((err) => {
-        console.error("Error ", err);
-      });
+
+      dispatch("removeObject", payload)
+        .catch((err) => commit("setError", "Error in removeCooperator." + err));
     },
-    deleteCooperators({ dispatch }, payload) {
+    deleteCooperators({ dispatch, commit }, payload) {
+      commit("setLoading", true);
       payload = Object.assign(payload, { collection: "cooperators" });
 
-      dispatch("deleteObject", payload).catch((err) => {
-        console.error("Error ", err);
-      });
+      dispatch("deleteObject", payload)
+        .catch((err) => commit("setError", "Error in deleteCooperators." + err));
     },
-    updateTestCooperators({ dispatch }, payload) {
+    updateTestCooperators({ dispatch, commit }, payload) {
+      commit("setLoading", true);
       payload = Object.assign(payload, { collection: "cooperators" });
-      dispatch("updateObject", payload).catch(() => {
-        console.error("Error to update");
-      });
+
+      dispatch("updateObject", payload)
+        .catch((err) => commit("setError", "Error in updateTestCooperators." + err));
     },
-    sendEmailInvitation({ dispatch }, payload) {
-      let link = `${payload.domain}/${payload.path}/${payload.testId}/${payload.token}`
-      Object.assign(payload, { link: link })
+    sendEmailInvitation({ dispatch, commit }, payload) {
+      commit("setLoading", true);
+      let link = `${payload.domain}/${payload.path}/${payload.testId}/${payload.token}`;
+      Object.assign(payload, { link: link });
+
       dispatch("callFunction", Object.assign({}, {
         function: 'sendEmail', data: Object.assign(payload, { template: template.getTemplate(payload) }
         )
-      }));
+      }))
+        .catch((err) => commit("setError", "Error in sendEmailInvitation." + err));
     }
   },
 };
