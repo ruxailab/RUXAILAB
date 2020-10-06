64119af3e195c5b022079bb85daed0e20bb23a66
leoruas <leo.coelho.ruas@gmail.com>
2020-10-06 11:15:51 -0300

    refactored tests store module with setLoading and setError

--- src/store/modules/database/tests.js
+++ src/store/modules/database/tests.js
@@ -60,27 +60,28 @@ export default {
     }
   },
   actions: {
-    createTest({ dispatch }, payload) {
+    createTest({ dispatch, commit }, payload) {
+      commit("setLoading", true);
       payload = Object.assign(payload, { collection: "test" });
+
       let docRef = dispatch("createObject", payload)
         .then((doc) => {
           return doc.id;
         })
-        .catch(() => {
-          console.error("Error to create test!");
-        });
+        .catch((err) => commit("setError", "Error in createTest." + err));
       return docRef;
     },
     async getTests({ commit, dispatch }, payload) {
-      payload = Object.assign(payload, { collection: "test" });
       commit("setLoading", true);
-      var tests = await dispatch("getAllObjects", payload);
+      payload = Object.assign(payload, { collection: "test" });
+      var tests = await dispatch("getAllObjects", payload)
+        .catch((err) => commit("setError", "Error in getTests." + err));
       commit("setTests", tests);
-      commit("setLoading", false);
     },
-    async deleteTest({ dispatch }, payload) {
-      // await dispatch("getTest", { id: payload.id });
-      await dispatch("getCooperators", { id: payload.cooperators });
+    async deleteTest({ dispatch, commit }, payload) {
+      commit("setLoading", true);
+      await dispatch("getCooperators", { id: payload.cooperators })
+        .catch((err) => commit("setError", "Error in deleteTest." + err));
 
       let coops = this.state.cooperators.cooperators;
 
@@ -88,94 +89,100 @@ export default {
       payload = Object.assign(payload, { collection: "test" }); //Delete test from Tests' Collection
       dispatch("deleteObject", payload)
         .then(() => {
-          dispatch("deleteReport", { id: payload.reports });
-          dispatch("deleteAnswers", { id: payload.answers });
+          dispatch("deleteReport", { id: payload.reports })
+            .catch((err) => commit("setError", "Error in deleteTest." + err));
+
+          dispatch("deleteAnswers", { id: payload.answers })
+            .catch((err) => commit("setError", "Error in deleteTest." + err));
 
           coops.cooperators.forEach(coop => {
-            if (coop.accessLevel.value <= 1) dispatch("removeMyCoops", {
-              docId: coop.id,
-              element: {
-                id: payload.id,
-                title: payload.title,
-                type: payload.type,
-              },
-            });
-            else dispatch("removeMyAnswers", {
-              docId: coop.id,
-              element: {
-                id: payload.id,
-                title: payload.title,
-                type: payload.type,
-              },
-            });
+            if (coop.accessLevel.value <= 1)
+              dispatch("removeMyCoops", {
+                docId: coop.id,
+                element: {
+                  id: payload.id,
+                  title: payload.title,
+                  type: payload.type,
+                },
+              })
+                .catch((err) => commit("setError", "Error in deleteTest." + err));
+            else
+              dispatch("removeMyAnswers", {
+                docId: coop.id,
+                element: {
+                  id: payload.id,
+                  title: payload.title,
+                  type: payload.type,
+                },
+              })
+                .catch((err) => commit("setError", "Error in deleteTest." + err));
           })
 
-          dispatch("deleteCooperators", { id: payload.cooperators });
+          dispatch("deleteCooperators", { id: payload.cooperators })
+            .catch((err) => commit("setError", "Error in deleteTest." + err));
         })
-
-      //delete user from test
+        .catch((err) => commit("setError", "Error in deleteTest." + err));
     },
     async getTest({ commit, dispatch }, payload) {
       commit("setLoading", true);
       payload = Object.assign(payload, { collection: "test" });
-      var test = await dispatch("getObject", payload);
+
+      var test = await dispatch("getObject", payload)
+        .catch((err) => commit("setError", "Error in getTest." + err));
+
       commit("setTest", test);
-      commit("setLoading", false);
     },
-    updateTest({ dispatch }, payload) {
+    updateTest({ dispatch, commit }, payload) {
+      commit("setLoading", true);
       payload = Object.assign(payload, { collection: "test" });
+
       dispatch("updateObject", payload)
-        .catch(() => {
-          console.error("Error to update");
-        });
+        .catch((err) => commit("setError", "Error in updateTest." + err));
     },
-    pushTestAnswer({ dispatch }, payload) {
+    pushTestAnswer({ dispatch, commit }, payload) {
+      commit("setLoading", true);
       payload = Object.assign(payload, { collection: "test" });
+
       dispatch("pushObject", payload)
-        .catch((err) => {
-          console.error("Error to push answer " + err);
-        });
+        .catch((err) => commit("setError", "Error in pushTestAnswer." + err));
     },
-    async pushCoop({ dispatch }, payload) {
+    async pushCoop({ dispatch, commit }, payload) {
+      commit("setLoading", true);
       payload = Object.assign(payload, { collection: "test", param: "coop" });
+
       dispatch("pushObject", payload)
-        .catch((err) => {
-          console.error("Error to push coops ", err);
-        });
+        .catch((err) => commit("setError", "Error in pushCoop." + err));
     },
-    setReportID({ dispatch }, payload) {
+    setReportID({ dispatch, commit }, payload) {
+      commit("setLoading", true);
       payload = Object.assign(payload, {
         collection: "test",
         param: "reports"
-      })
-      dispatch("setParamInObject", payload).
-        catch((err) => {
-          console.error("Error set report id ", err)
-        })
+      });
+
+      dispatch("setParamInObject", payload)
+        .catch((err) => commit("setError", "Error in setReportID." + err));
     },
-    setAnswerID({ dispatch }, payload) {
+    setAnswerID({ dispatch, commit }, payload) {
+      commit("setLoading", true);
       payload = Object.assign(payload, { collection: "test", param: "answers" });
 
       dispatch("setParamInObject", payload)
-        .catch((err) => {
-          console.error("Error ", err);
-        })
+        .catch((err) => commit("setError", "Error in setAnswerID." + err));
     },
-    setCooperatorsID({ dispatch }, payload) {
+    setCooperatorsID({ dispatch, commit }, payload) {
+      commit("setLoading", true);
       payload = Object.assign(payload, { collection: "test", param: "cooperators" });
 
       dispatch("setParamInObject", payload)
-        .catch((err) => {
-          console.error("Error ", err);
-        })
+        .catch((err) => commit("setError", "Error in setCooperatorsID." + err));
     },
-    setUpToDate({ dispatch }, payload) {
+    setUpToDate({ dispatch, commit }, payload) {
+      commit("setLoading", true);
       payload = Object.assign(payload, { collection: "test", param: "template.upToDate" });
 
       dispatch("setParamInObject", payload)
-        .catch((err) => {
-          console.error("Error ", err);
-        })
+        .catch((err) => commit("setError", "Error in setUpToDate." + err));
     }
   },
 };
