710d883dae6e9c6c2769d7462efd65fa09f2db6f
leoruas <leo.coelho.ruas@gmail.com>
2020-10-06 12:03:37 -0300

    refactored templates store module with setLoading and setError

--- src/store/modules/database/templates.js
+++ src/store/modules/database/templates.js
@@ -21,37 +21,49 @@ export default {
         }
     },
     actions: {
-        createTemplate({ dispatch }, payload) {
-            payload = Object.assign(payload, { collection: 'templates' })
+        createTemplate({ dispatch, commit }, payload) {
+            commit("setLoading", true);
+            payload = Object.assign(payload, { collection: 'templates' });
+
             let docRef = dispatch('createObject', payload)
                 .then((doc) => {
                     return doc.id;
                 })
-                .catch((err) => {
-                    console.error("Error to create template", err)
-                })
+                .catch((err) => commit("setError", "Error in createTemplate." + err));
+
             return docRef;
         },
         async getTemplates({ commit, dispatch }) {
-            let payload = Object.assign({}, { collection: 'templates' })
+            commit("setLoading", true);
+            let payload = Object.assign({}, { collection: 'templates' });
+
             let templates = await dispatch("getAllObjects", payload)
+                .catch((err) => commit("setError", "Error in getTemplates." + err));
+
             commit('setTemplates', templates)
         },
         async getTemplate({ commit, dispatch }, payload) {
-            payload = Object.assign(payload, { collection: 'templates' })
+            commit("setLoading", true);
+            payload = Object.assign(payload, { collection: 'templates' });
+
             let template = await dispatch("getObject", payload)
-            commit('setTemplate', template)
+                .catch((err) => commit("setError", "Error in getTemplate." + err));
+
+            commit('setTemplate', template);
         },
-        updateTemplate({ dispatch }, payload) {
+        updateTemplate({ dispatch, commit }, payload) {
+            commit("setLoading", true);
             payload = Object.assign(payload, { collection: "templates" });
+
             dispatch("updateObject", payload)
-                .catch(() => {
-                    console.error("Error to update");
-                });
+                .catch((err) => commit("setError", "Error in updateTemplate." + err));
         },
-        deteleTemplate({ dispatch }, payload) {
+        deteleTemplate({ dispatch, commit }, payload) {
+            commit("setLoading", true);
             payload = Object.assign(payload, { collection: "templates" });
+
             dispatch("deleteObject", payload)
+                .catch((err) => commit("setError", "Error in deleteTemplate." + err));
         }
     }
 }
