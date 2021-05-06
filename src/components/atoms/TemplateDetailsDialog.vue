93b79f1a11fe93ad86df0e80f48ad83a665837c9
leoruas <leo.coelho.ruas@gmail.com>
2021-05-06 10:32:15 -0400

    created TemplateDetailsDialog component

---
+++ src/components/atoms/TemplateDetailsDialog.vue
@@ -0,0 +1,222 @@
+<template>
+  <v-dialog v-model="dialog" width="800px">
+    <v-card min-height="400px" class="list-scroll">
+      <v-col class="mb-1 pa-4 pb-1">
+        <p class="subtitleView">
+          Current information:
+          <v-btn
+            style="position: absolute; right: 4px; top: 8px"
+            small
+            icon
+            @click="closeDialog()"
+            class="ma-1"
+          >
+            <v-icon color="error">mdi-close</v-icon>
+          </v-btn>
+        </p>
+      </v-col>
+      <v-divider></v-divider>
+      <v-row class="ma-0 pa-0">
+        <v-col cols="10">
+          <v-treeview
+            v-model="tree"
+            :open="open"
+            activatable
+            item-key="id"
+            open-on-click
+            dense
+            :items="items"
+          >
+            <template v-slot:prepend="{ item, open }">
+              <v-icon v-if="!item.icon">{{
+                open ? "mdi-folder-open" : "mdi-folder"
+              }}</v-icon>
+              <v-icon v-else>{{ icons[item.icon] }}</v-icon>
+            </template>
+          </v-treeview>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    dialog: {
+      type: Boolean,
+      required: true,
+      default: () => false,
+    },
+    template: {
+      type: Object,
+      required: true,
+      default: () => {
+        return {};
+      },
+    },
+  },
+  data: () => ({
+    tree: [],
+    open: [],
+    icons: {
+      question: "mdi-timeline-help",
+      reponse: "mdi-timeline-check",
+      description: "mdi-timeline-text",
+      item: "mdi-timeline",
+    },
+  }),
+  methods: {
+    closeDialog() {
+      this.$emit("close");
+      this.open = [];
+    },
+  },
+  computed: {
+    items() {
+      let items = [];
+
+      if (this.template) {
+        let template = this.template.body;
+        if (template.type == "Heuristics") {
+          let id = 0;
+          let heuristics = template.heuristics;
+          let options = template.options;
+          if (heuristics) {
+            items.push({
+              id: id++,
+              name: "Heuristics",
+              children: heuristics.map((h) => {
+                return {
+                  id: id++,
+                  name: h.title,
+                  children: h.questions.map((q) => {
+                    return {
+                      id: id++,
+                      name: q.title,
+                      children: q.descriptions.map((d) => {
+                        return {
+                          id: id++,
+                          name: d.title,
+                          icon: "description",
+                        };
+                      }),
+                      icon: "question",
+                    };
+                  }),
+                };
+              }),
+            });
+          }
+          if (options) {
+            items.push({
+              id: id++,
+              name: "Options",
+              children: options.map((op) => {
+                return {
+                  id: id++,
+                  name: op.text,
+                  children: [
+                    { id: id++, name: `value: ${op.value}`, icon: "reponse" },
+                  ],
+                };
+              }),
+            });
+          }
+        } else if (template.type == "User") {
+          let id = 0;
+          let tasks = template.tasks;
+          let preTest = template.preTest;
+          let postTest = template.postTest;
+          if (tasks) {
+            items.push({
+              id: id++,
+              name: "Tasks",
+              children: tasks.map((task) => {
+                return {
+                  id: id++,
+                  name: task.name,
+                  children: Object.entries(task).map((item) => {
+                    return {
+                      id: id++,
+                      name: `${item[0]}: ${item[1]}`,
+                      icon: "item",
+                    };
+                  }),
+                  icon: "question",
+                };
+              }),
+            });
+          }
+          if (preTest) {
+            items.push({
+              id: id++,
+              name: "Pre Test",
+              children: Object.entries(preTest).map((item) => {
+                return {
+                  id: id++,
+                  name: `${item[0]}: ${item[1]}`,
+                  icon: "item",
+                };
+              }),
+            });
+          }
+          if (postTest) {
+            items.push({
+              id: id++,
+              name: "Post Test",
+              children: Object.entries(postTest).map((item) => {
+                return {
+                  id: id++,
+                  name: `${item[0]}: ${item[1]}`,
+                  icon: "item",
+                };
+              }),
+            });
+          }
+        }
+      }
+
+      return items;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.subtitleView {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 200;
+  font-size: 18.1818px;
+  line-height: 21px;
+  align-items: flex-end;
+  color: #000000;
+  margin-bottom: 0px;
+  padding-bottom: 0px;
+}
+
+.list-scroll {
+  height: 508px;
+  overflow: auto;
+}
+/* Nav bar list scroll bar */
+/* width */
+.list-scroll::-webkit-scrollbar {
+  width: 7px;
+}
+/* Track */
+.list-scroll::-webkit-scrollbar-track {
+  background: none;
+}
+/* Handle */
+.list-scroll::-webkit-scrollbar-thumb {
+  background: #ffcd86;
+  border-radius: 4px;
+}
+/* Handle on hover */
+.list-scroll::-webkit-scrollbar-thumb:hover {
+  background: #fca326;
+  /* background: #515069; */
+}
+</style>
--- src/views/admin/TemplateView.vue
+++ src/views/admin/TemplateView.vue
@@ -57,47 +57,12 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
-
-    <!-- Detailed Info Dialog -->
-    <v-dialog v-model="dialogDetail" width="800px">
-      <v-card min-height="400px" class="list-scroll">
-        <v-col class="mb-1 pa-4 pb-1">
-          <p class="subtitleView">
-            Current informations
-            <v-btn
-              style="position: absolute; right: 4px; top: 8px"
-              small
-              icon
-              @click="(dialogDetail = false), (open = [])"
-              class="ma-1"
-            >
-              <v-icon color="error">mdi-close</v-icon>
-            </v-btn>
-          </p>
-        </v-col>
-        <v-divider></v-divider>
-        <v-row class="ma-0 pa-0">
-          <v-col cols="10">
-            <v-treeview
-              v-model="tree"
-              :open="open"
-              activatable
-              item-key="id"
-              open-on-click
-              dense
-              :items="items"
-            >
-              <template v-slot:prepend="{ item, open }">
-                <v-icon v-if="!item.icon">{{
-                  open ? "mdi-folder-open" : "mdi-folder"
-                }}</v-icon>
-                <v-icon v-else>{{ icons[item.icon] }}</v-icon>
-              </template>
-            </v-treeview>
-          </v-col>
-        </v-row>
-      </v-card>
-    </v-dialog>
+    
+    <TemplateDetails
+      :dialog="dialogDetails"
+      :template="template"
+      @close="dialogDetails = false"
+    />
 
     <ShowInfo title="Template">
       <v-alert
@@ -158,7 +123,7 @@
                   color="#F9A826"
                 ></v-checkbox>
                 <v-row class="mx-1">
-                  <v-btn outlined @click="dialogDetail = true"
+                  <v-btn outlined @click="dialogDetails = true"
                     >Detailed information</v-btn
                   >
                   <v-spacer></v-spacer>
@@ -222,26 +187,20 @@
 <script>
 import ShowInfo from "@/components/organisms/ShowInfo";
 import SnackBar from "@/components/atoms/Snackbar";
+import TemplateDetails from "@/components/atoms/TemplateDetailsDialog";
 
 export default {
   props: ["id"],
   components: {
     ShowInfo,
     SnackBar,
+    TemplateDetails,
   },
   data: () => ({
-    tree: [],
-    open: [],
-    icons: {
-      question: "mdi-timeline-help",
-      reponse: "mdi-timeline-check",
-      description: "mdi-timeline-text",
-      item: "mdi-timeline",
-    },
     change: false,
     dialogDel: false,
     dialogAlert: false,
-    dialogDetail: false,
+    dialogDetails: false,
     loading: false,
     object: null,
     template: null,
@@ -300,7 +259,7 @@ export default {
               docId: this.template.header.author.id,
               element: Object.assign(this.template.header, {
                 type: this.template.body.type,
-                id: this.id
+                id: this.id,
               }),
             })
             .then(() =>
@@ -453,112 +412,6 @@ export default {
 
       return `Are you sure you want to delete this template? This action can't be undone`; //in case object isnt loaded
     },
-    items() {
-      let items = [];
-
-      if (this.template) {
-        let template = this.template.body;
-        if (template.type == "Heuristics") {
-          let id = 0;
-          let heuristics = template.heuristics;
-          let options = template.options;
-          if (heuristics) {
-            items.push({
-              id: id++,
-              name: "Heuristics",
-              children: heuristics.map((h) => {
-                return {
-                  id: id++,
-                  name: h.title,
-                  children: h.questions.map((q) => {
-                    return {
-                      id: id++,
-                      name: q.title,
-                      children: q.descriptions.map((d) => {
-                        return {
-                          id: id++,
-                          name: d.title,
-                          icon: "description",
-                        };
-                      }),
-                      icon: "question",
-                    };
-                  }),
-                };
-              }),
-            });
-          }
-          if (options) {
-            items.push({
-              id: id++,
-              name: "Options",
-              children: options.map((op) => {
-                return {
-                  id: id++,
-                  name: op.text,
-                  children: [
-                    { id: id++, name: `value: ${op.value}`, icon: "reponse" },
-                  ],
-                };
-              }),
-            });
-          }
-        } else if (template.type == "User") {
-          let id = 0;
-          let tasks = template.tasks;
-          let preTest = template.preTest;
-          let postTest = template.postTest;
-          if (tasks) {
-            items.push({
-              id: id++,
-              name: "Tasks",
-              children: tasks.map((task) => {
-                return {
-                  id: id++,
-                  name: task.name,
-                  children: Object.entries(task).map((item) => {
-                    return {
-                      id: id++,
-                      name: `${item[0]}: ${item[1]}`,
-                      icon: "item",
-                    };
-                  }),
-                  icon: "question",
-                };
-              }),
-            });
-          }
-          if (preTest) {
-            items.push({
-              id: id++,
-              name: "Pre Test",
-              children: Object.entries(preTest).map((item) => {
-                return {
-                  id: id++,
-                  name: `${item[0]}: ${item[1]}`,
-                  icon: "item",
-                };
-              }),
-            });
-          }
-          if (postTest) {
-            items.push({
-              id: id++,
-              name: "Post Test",
-              children: Object.entries(postTest).map((item) => {
-                return {
-                  id: id++,
-                  name: `${item[0]}: ${item[1]}`,
-                  icon: "item",
-                };
-              }),
-            });
-          }
-        }
-      }
-
-      return items;
-    },
   },
   watch: {
     test: async function () {
@@ -572,20 +425,12 @@ export default {
       }
     },
   },
-  created() {
-    if (!this.$store.getters.template) {
-      this.$store.dispatch("getTemplate", { id: this.id });
-    } else if (this.$store.getters.template.id !== this.id) {
-      this.$store.dispatch("getTemplate", { id: this.id });
-    }
+  async created() {
+    await this.$store.dispatch("getTemplate", { id: this.id });
 
-    if (!this.$store.test && this.id !== null && this.id !== undefined) {
-      this.$store.dispatch("getTest", {
-        id: this.$store.getters.user.myTests.find((test) => {
-          if ("template" in test) return test.template.id == this.id;
-        }).id,
-      });
-    }
+    await this.$store.dispatch("getTest", {
+      id: this.template.testId,
+    });
   },
   beforeRouteLeave(to, from, next) {
     if (this.change) {
