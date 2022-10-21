36e82f00e8e5b142da034c2145dfb2d7a2ae636a
TalesAugusto1 <tales.a.s.furlan@gmail.com>
2022-10-21 13:44:03 -0300

    controllers base

---
+++ src/controllers/AuthController.js
@@ -0,0 +1,16 @@
+// imports
+
+/* 
+import {AuthController} from '././AuthController'
+
+let authController = new AuthController()
+*/
+
+
+
+export class AuthController {
+  // attributes/properties
+  // methods
+  // constructor
+  constructor() {}
+}
---
+++ src/controllers/TemplateController.js
@@ -0,0 +1,14 @@
+// imports
+
+/* 
+import {TemplateController} from '././TemplateController'
+
+let templateController = new TemplateController()
+*/
+
+export class TemplateController {
+  // attributes/properties
+  // methods
+  // constructor
+  constructor() {}
+}
