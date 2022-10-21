19bb162a32f4ddae4b58710244ec173f3d6ba5f2
TalesAugusto1 <tales.a.s.furlan@gmail.com>
2022-10-21 13:44:03 -0300

    controllers base

--- src/controllers/TestController.js
+++ src/controllers/TestController.js
@@ -1,8 +1,18 @@
 //controller is an action class, like add, edit, clean etc etc.
 
+// imports
+
+/* 
+import {TestController} from '././TestController'
+
+let testController = new TestController()
+*/
+
 export class TestController {
   // attributes/properties
   // methods
   // constructor
   constructor() {}
 }
+
+/* "_" before attibutes and mehtods turn them into PRIVATE*/
---
+++ src/controllers/UserController.js
@@ -0,0 +1,14 @@
+// imports
+
+/* 
+import {UserController} from '././UserController'
+
+let userController = new UserController()
+*/
+
+export class UserController {
+  // attributes/properties
+  // methods
+  // constructor
+  constructor() {}
+}
