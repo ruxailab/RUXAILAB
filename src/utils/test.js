05e3c285d8e2e17f37761d7813589ea60f828cc8
Camila Jardim <camila.dfj@hotmail.com>
2022-10-24 13:51:25 -0300

    begin of enums implements

---
+++ src/utils/test.js
@@ -0,0 +1,19 @@
+export const testTypes = 
+    {
+        0: 'heuristics',
+        1: 'user'
+    };
+
+export const systemAccessLevels = 
+    {
+        0: 'superAdmin',
+        1: 'admin',
+        2: 'user'
+    };
+
+export const testAccessLevels = 
+    {
+        0: 'administrator',
+        1: 'guest',
+        2: 'evaluator'
+    };
