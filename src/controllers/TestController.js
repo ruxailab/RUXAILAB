dc3903f19d8762b0af3c6e97f0d747d596da2716
TalesAugusto1 <tales.a.s.furlan@gmail.com>
2022-10-24 14:17:15 -0300

    Test controllers get done

--- src/controllers/TestController.js
+++ src/controllers/TestController.js
@@ -11,36 +11,54 @@ let testController = new TestController()
 import api from "@/api/index";
 import database from "../api/modules/database";
 
-// import Test from "../models/Test";
-// import TestAdmin from "../models/TestAdmin";
-// import TestStructure from "../models/TestStructure";
-// import TestStructureOptions from "../models/TestStructureOptions";
-// import TestTempleteDoc from "../models/TestTemplateDoc";
+import Test from "../models/Test";
+import TestAdmin from "../models/TestAdmin";
+import TestStructure from "../models/TestStructure";
+import TestStructureOptions from "../models/TestStructureOptions";
+import TestTempleteDoc from "../models/TestTemplateDoc";
 
 export default class TestController {
-  async TestAdmin(Object) {
-    return super.create(api, Object);
-  }
-
-  // // attributes/properties
-  // // methods
-  // // constructor
-
+  //
   async createNewTest(data) {
     return database.createObject(api, data);
   }
-
-  async;
-
-  // createObject: async (payload) => {
-  //   const db = firebase.firestore();
-  //   var collectionRef = db.collection(payload.collection);
-  //   return collectionRef.add(payload.data);
-  // },
+  //
+  async deleteTest(data) {
+    return database.deleteObject(api, data);
+  }
+  //
+  async updateTest(data) {
+    return database.updateObject(api, data);
+  }
+  //Get data from "Test"
+  async getTest() {
+    const answer = await database.getObject(api);
+    return new Map(answer.map((obj) => [obj.id, new Test(obj)]));
+  }
+  //Get data from "TestAdmin"
+  async getTestAdmin() {
+    const answer = await database.getObject(api);
+    return new Map(answer.map((obj) => [obj.id, new TestAdmin(obj)]));
+  }
+  //Get data from "TestStructure"
+  async getTestStructure() {
+    const answer = await database.getObject(api);
+    return new Map(answer.map((obj) => [obj.id, new TestStructure(obj)]));
+  }
+  //Get data from "TestStructureOptions"
+  async getTestStructureOptions() {
+    const answer = await database.getObject(api);
+    return new Map(
+      answer.map((obj) => [obj.id, new TestStructureOptions(obj)])
+    );
+  }
+  //Get data from "TestTempleteDoc"
+  async getTestTempleteDoc() {
+    const answer = await database.getObject(api);
+    return new Map(answer.map((obj) => [obj.id, new TestTempleteDoc(obj)]));
+  }
 
   constructor() {}
-
-  async;
 }
 
 /* "_" before attibutes and mehtods turn them into PRIVATE*/
