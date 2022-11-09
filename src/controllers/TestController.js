dfb2d2998cee613274f8a1fae4d0e08cf01623da
TalesAugusto1 <tales.a.s.furlan@gmail.com>
2022-11-09 15:28:31 -0300

    csv import button+page+upload button empty

--- src/controllers/TestController.js
+++ src/controllers/TestController.js
@@ -1,8 +1,93 @@
-//controller is an action class, like add, edit, clean etc etc.
+/* 
+import {TestController} from '././TestController'
 
-export class TestController {
-  // attributes/properties
-  // methods
-  // constructor
-  constructor() {}
+let testController = new TestController()
+*/
+
+// imports
+
+import api from "@/api/index";
+import database from "../api/modules/database";
+
+import Test from "../models/Test";
+import TestAdmin from "../models/TestAdmin";
+import TestStructure from "../models/TestStructure";
+import TestStructureOptions from "../models/TestStructureOptions";
+import TestTemplateDoc from "../models/TestTemplateDoc";
+
+export default class TestController {
+  //
+  async createNewTest(data) {
+    return database.createObject(api, data);
+  }
+  //
+  async deleteTest(data) {
+    return database.deleteObject(api, data);
+  }
+  //
+  async updateTest(data) {
+    return database.updateObject(api, data);
+  }
+
+  //------------------GET OBJECTS------------------
+  //GetObject of Test
+  async getObjectTest(id) {
+    const answer = await database.getObject(api + "/" + id);
+    return new Test(answer);
+  }
+
+  //GetObject of TestAdmin
+  async getObjectTestAdmin(id) {
+    const answer = await database.getObject(api + "/" + id);
+    return new TestAdmin(answer);
+  }
+
+  //GetObject of TestStructure
+  async getObjectTestStructure(id) {
+    const answer = await database.getObject(api + "/" + id);
+    return new TestStructure(answer);
+  }
+
+  //GetObject of TestStructureOptions
+  async getObjectTestStructureOptions(id) {
+    const answer = await database.getObject(api + "/" + id);
+    return new TestStructureOptions(answer);
+  }
+
+  //GetObject of TestTemplateDoc
+  async getObjectTestTemplateDoc(id) {
+    const answer = await database.getObject(api + "/" + id);
+    return new TestTemplateDoc(answer);
+  }
+
+  //----------------GET ALL OBJECTS----------------
+  //GetAll data from "Test"
+  async getAllTest() {
+    const answer = await database.getAllObject(api);
+    return new Map(answer.map((obj) => [obj.id, new Test(obj)]));
+  }
+  //GetAll data from "TestAdmin"
+  async getAllTestAdmin() {
+    const answer = await database.getAllObject(api);
+    return new Map(answer.map((obj) => [obj.id, new TestAdmin(obj)]));
+  }
+  //GetAll data from "TestStructure"
+  async getAllTestStructure() {
+    const answer = await database.getAllObject(api);
+    return new Map(answer.map((obj) => [obj.id, new TestStructure(obj)]));
+  }
+  //GetAll data from "TestStructureOptions"
+  async getAllTestStructureOptions() {
+    const answer = await database.getAllObject(api);
+    return new Map(
+      answer.map((obj) => [obj.id, new TestStructureOptions(obj)])
+    );
+  }
+  //GetAll data from "TestTemplateDoc"
+  async getAllTestTemplateDoc() {
+    const answer = await database.getAllObject(api);
+    return new Map(answer.map((obj) => [obj.id, new TestTemplateDoc(obj)]));
+  }
 }
+
+/* "_" before attibutes and mehtods turn them into PRIVATE*/
