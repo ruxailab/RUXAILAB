3340102ae32d6e432682840cb55e0906a2216085
tamitavares <tamiris.lemes18@gmail.com>
2022-12-09 20:19:00 -0300

    Fix bugs and test TestController

--- src/controllers/TestController.js
+++ src/controllers/TestController.js
@@ -1,13 +1,29 @@
 // imports
 
-//import api from "@/api/index";
-//import database from "@/api/modules/database";
-
 import Test from "@/models/Test";
 import TestAdmin from "@/models/TestAdmin";
 import TestStructure from "@/models/TestStructure";
 import TestStructureOptions from "@/models/TestStructureOptions";
 import TestTemplateDoc from "@/models/TestTemplateDoc";
+
+// functions
+
+function toTest(){
+  return new Test()
+}
+function toTestAdmin(){
+  return new TestAdmin()
+}
+function toTestStructure(){
+  return new TestStructure()
+}
+function toTestStructureOptions(){
+  return new TestStructureOptions()
+}
+function toTestTemplateDoc(){
+  return new TestTemplateDoc()
+}
+
 
 import Controller from '@/controllers/BaseController'
 
@@ -37,148 +53,69 @@ export default class TestController extends Controller{
   //------------------GET OBJECTS  - ID------------------
 
   //GetObject of Test
-  async getObjectTest(path, parameter, id) {
-    return super.read(path, parameter, id).then((res)=> {
-      return new Test(res);
+  async getObjectTest(path, parameter, condition){
+    return super.read(path, parameter, condition, toTest)  //consulta onde (parameter)id == (condition)"legal"
+  }
+
+  //GetObject of TestAdmin
+  async getObjectTestAdmin(path, parameter, condition){
+    return super.read(path, parameter, condition, toTestAdmin)  //consulta onde (parameter)id == (condition)"legal"
+  }
+
+  //GetObject of TestStructure
+  async getObjectTestStructure(path, parameter, condition){
+    return super.read(path, parameter, condition, toTestStructure)  //consulta onde (parameter)id == (condition)"legal"
+  }
+
+  //GetObject of TestStructureOptions
+  async getObjectTestStructureOptions(path, parameter, condition){
+    return super.read(path, parameter, condition, toTestStructureOptions)  //consulta onde (parameter)id == (condition)"legal"
+  }
+
+  //GetObject of TestTemplateDoc
+  async getObjectTestTemplateDoc(path, parameter, condition){
+    return super.read(path, parameter, condition, toTestTemplateDoc)  //consulta onde (parameter)id == (condition)"legal"
+  }
+
+
+  // //----------------GET ALL OBJECTS----------------
+
+  //GetObject of Test
+  async getAllObjectTest(path){
+    return super.readAll(path, toTest).then((res)=> {
+      return res
     })
   }
 
   //GetObject of TestAdmin
-  async getObjectTestAdmin(path, parameter, id) {
-    return super.read(path, parameter, id).then((res)=> {
-      return new TestAdmin(res);
+  async getAllObjectTestAdmin(path){
+    return super.readAll(path, toTestAdmin).then((res)=> {
+      return res
     })
   }
 
   //GetObject of TestStructure
-  async getObjectTestStructure(path, parameter, id) {
-    return super.read(path, parameter, id).then((res)=> {
-      return new TestStructure(res);
+  async getAllObjectTestStructure(path){
+    return super.readAll(path, toTestStructure).then((res)=> {
+      return res
     })
   }
 
   //GetObject of TestStructureOptions
-  async getObjectTestStructureOptions(path, parameter, id) {
-    return super.read(path, parameter, id).then((res)=> {
-      return new TestStructureOptions(res);
+  async getAllObjectTestStructureOptions(path){
+    return super.readAll(path, toTestStructureOptions).then((res)=> {
+      return res
     })
   }
 
   //GetObject of TestTemplateDoc
-  async getObjectTestTemplateDoc(path, parameter, id) {
-    return super.read(path, parameter, id).then((res)=> {
-      return new TestTemplateDoc(res);
+  async getAllObjectTestTemplateDoc(path){
+    return super.readAll(path, toTestTemplateDoc).then((res)=> {
+      return res
     })
   }
 
-
-  //----------------GET ALL OBJECTS----------------
-
-  //GetAll data from "Test"
-  async getAllTest(path, parameter, id) {
-    return super.read(path, parameter, id).then((res)=> {
-      return new Map(res.map((obj) => [obj.id, new Test(obj)]));
-  })}
-
-  //GetAll data from "TestAdmin"
-  async getAllTestAdmin(path, parameter, id) {
-    return super.read(path, parameter, id).then((res)=> {
-      return new Map(res.map((obj) => [obj.id, new TestAdmin(obj)]));
-  })}
-
-  //GetAll data from "TestStructure"
-  async getAllTestStructure(path, parameter, id) {
-    return super.read(path, parameter, id).then((res)=> {
-      return new Map(res.map((obj) => [obj.id, new TestStructure(obj)]));
-  })}
-
-  //GetAll data from "TestStructureOptions"
-  async getAllTestStructureOptions(path, parameter, id) {
-    return super.read(path, parameter, id).then((res)=> {
-      return new Map(res.map((obj) => [obj.id, new TestStructureOptions(obj)]));
-  })}
-
-  //GetAll data from "TestTemplateDoc"
-  async getAllTestTemplateDoc(path, parameter, id) {
-    return super.read(path, parameter, id).then((res)=> {
-      return new Map(res.map((obj) => [obj.id, new TestTemplateDoc(obj)]));
-  })}
-
-
 }
-
-
-//   async createNewTest(data) {
-//     return database.createObject(api, data);
-//   }
-
-//   async deleteTest(data) {
-//     return database.deleteObject(api, data);
-//   }
   
-//   async updateTest(data) {
-//     return database.updateObject(api, data);
-//   }
-
-//   //------------------GET OBJECTS------------------
-//   //GetObject of Test
-//   async getObjectTest(id) {
-//     const answer = await database.getObject(api + "/" + id);
-//     return new Test(answer);
-//   }
-
-//   //GetObject of TestAdmin
-//   async getObjectTestAdmin(id) {
-//     const answer = await database.getObject(api + "/" + id);
-//     return new TestAdmin(answer);
-//   }
-
-//   //GetObject of TestStructure
-//   async getObjectTestStructure(id) {
-//     const answer = await database.getObject(api + "/" + id);
-//     return new TestStructure(answer);
-//   }
-
-//   //GetObject of TestStructureOptions
-//   async getObjectTestStructureOptions(id) {
-//     const answer = await database.getObject(api + "/" + id);
-//     return new TestStructureOptions(answer);
-//   }
-
-//   //GetObject of TestTemplateDoc
-//   async getObjectTestTemplateDoc(id) {
-//     const answer = await database.getObject(api + "/" + id);
-//     return new TestTemplateDoc(answer);
-//   }
-
-//   //----------------GET ALL OBJECTS----------------
-//   //GetAll data from "Test"
-//   async getAllTest() {
-//     const answer = await database.getAllObject(api);
-//     return new Map(answer.map((obj) => [obj.id, new Test(obj)]));
-//   }
-//   //GetAll data from "TestAdmin"
-//   async getAllTestAdmin() {
-//     const answer = await database.getAllObject(api);
-//     return new Map(answer.map((obj) => [obj.id, new TestAdmin(obj)]));
-//   }
-//   //GetAll data from "TestStructure"
-//   async getAllTestStructure() {
-//     const answer = await database.getAllObject(api);
-//     return new Map(answer.map((obj) => [obj.id, new TestStructure(obj)]));
-//   }
-//   //GetAll data from "TestStructureOptions"
-//   async getAllTestStructureOptions() {
-//     const answer = await database.getAllObject(api);
-//     return new Map(
-//       answer.map((obj) => [obj.id, new TestStructureOptions(obj)])
-//     );
-//   }
-//   //GetAll data from "TestTemplateDoc"
-//   async getAllTestTemplateDoc() {
-//     const answer = await database.getAllObject(api);
-//     return new Map(answer.map((obj) => [obj.id, new TestTemplateDoc(obj)]));
-//   }
-//}
 
 // "_" before attibutes and mehtods turn them into PRIVATE
