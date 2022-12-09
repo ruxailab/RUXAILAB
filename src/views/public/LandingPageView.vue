d88d1a5cf91235f616a820958a179f0e6f1832a9
tamitavares <tamiris.lemes18@gmail.com>
2022-12-09 20:19:00 -0300

    Fix bugs and test TestController

--- src/views/public/LandingPageView.vue
+++ src/views/public/LandingPageView.vue
@@ -14,18 +14,45 @@ import Footer from "@/components/organisms/Footer";
 import Snackbar from "@/components/atoms/Snackbar";
 import TestController from "../../controllers/TestController";
 
+//test to createNewTest
+// const docDataTest = {
+//     stringExample: "Hello world!",
+//     booleanExample: true,
+//     numberExample: 3.14159265,
+//     arrayExample: [5, true, "hello"],
+//     nullExample: null,
+//     objectExample: {
+//         a: 5,
+//         b: {
+//             nested: "foo"
+//         }
+//     }
+// };
+
+
+
 export default {
   data: () => ({}),
   components: {
     Intro,
     About,
     Footer,
-    Snackbar
+    Snackbar,
+    
+    
+    
   },
 
+  //teste dos controllers
+  
   async created() {
-    console.log("TESTEEEEE")
-    await new TestController().getObjectTest("0pZxsNhN2O3drsdCuA33", "udv4Qp66ncTgsL1T17nbeFajZh02","udv4Qp66ncTgsL1T17nbeFajZh02").then((res)=> {console.log(res)})
+    console.log("TEST CONTROLLER")
+    //await new TestController().createNewTest("test", "TesteTata", docDataTest).then((res)=> {console.log(res)})
+    console.log("GET ALL")
+    await new TestController().getAllObjectTest('test').then((res)=> {console.log(res)})
+    console.log("GET OBJECT")
+    await new TestController().getObjectTest('test', "answers", "n0MWTYrj8BAqJJwNdfxl").then((res)=> {console.log(res)})
   },
+
 };
 </script>
