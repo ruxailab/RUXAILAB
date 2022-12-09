d1402e4f2f88e2c1f1b2a9c87f8804399eb5d8d7
tamitavares <tamiris.lemes18@gmail.com>
2022-12-09 20:19:00 -0300

    Fix bugs and test TestController

--- src/controllers/BaseController.js
+++ src/controllers/BaseController.js
@@ -1,22 +1,46 @@
 import { db } from "@/firebase";
-import { doc, setDoc, updateDoc, query, where, getDocs, deleteDoc, collection } from "firebase/firestore";
+import { doc, setDoc, updateDoc, query, where, getDocs, deleteDoc, collection} from "firebase/firestore";
 
 export default class Controller {
   constructor() {}
+
+  // path - collection
+  // data - document to insert (object)
 
   async create(path, document, data) {
     await setDoc(doc(db, path, document), data);
   } 
 
-  async read(path, parameter, condition) {
-    const q = query(collection(db, path), where(parameter, "==", condition));
+  //model to define in Controller
+
+  async read(path, parameter, condition, model) {
+    const q = query(collection(db, path), where(parameter, '==', condition));  
+    const res = new Array();
     const querySnapshot = await getDocs(q);
-    const res = [];
-    querySnapshot.forEach((doc) => {
-      res.push(doc.data())
-    });
-    return res;
-  }
+      querySnapshot.forEach(doc => {
+        res.push(doc.data())
+      })
+      // res.forEach(element => {
+      //   console.log(element)
+      // })
+      console.log(res)
+      return res.map(model)
+    }
+
+  async readAll(path, model) {
+    const q = query(collection(db, path));  
+    const res = new Array();
+    const querySnapshot = await getDocs(q);
+      querySnapshot.forEach(doc => {
+        res.push(doc.data())
+      })
+      // res.forEach(element => {
+      //   console.log(element)
+      // })
+      console.log(res)
+      return res.map(model)
+    }
+ 
 
   async update(path, document, payload) {
     const uploadFile = doc(db, path, document);
