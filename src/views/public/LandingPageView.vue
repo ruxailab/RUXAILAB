4ee130f45d5f5348e3849945697e9639daab29c5
tamitavares <tamiris.lemes18@gmail.com>
2022-12-14 11:04:51 -0300

    Correct User Controller

--- src/views/public/LandingPageView.vue
+++ src/views/public/LandingPageView.vue
@@ -12,10 +12,7 @@ import Intro from "@/components/atoms/Introduction";
 import About from "@/components/organisms/LandingAbout";
 import Footer from "@/components/organisms/Footer";
 import Snackbar from "@/components/atoms/Snackbar";
-import TestController from "../../controllers/TestController";
-// import AuthController from "../../controllers/AuthController";
-// import Controller from "@/controllers/BaseController";
-
+import UserController from "../../controllers/UserController";
 
 export default {
   data: () => ({}),
@@ -27,20 +24,11 @@ export default {
   
   },
 
-  //tests of controllers
-  
-  async created() {
-  // console.log("USER CONTROLLER")
-  // await new UserController().getAllObjectUser().then((res)=> {console.log(res)})
-    console.log("GET ALL")
-    await new TestController().getAllObjectTest().then((res)=> {console.log(res)})
-    console.log("GET OBJECT")
-    await new TestController().getObjectTest("answers", "n0MWTYrj8BAqJJwNdfxl").then((res)=> {console.log(res)})
-  //   console.log("AUTH TEST")
-  //   await new AuthController().authGetCurrentUser().then((res)=> {console.log(res)})
-  //   const res = await new Controller().read("users", "email", "tamiris.tavares1801@gmail.com")
-  //   console.log("LANDING RESPONSE ==>>", res)
- },
+  async created(){
+    console.log("LANDING PAGE RESPONSE ---> ")
+    await new UserController().readUser("tales.furlan@gmail.com").then((res)=> {console.log(res)})
+  }
+
 }
 
 </script>
