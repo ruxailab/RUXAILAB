46375ad605e64ab6856331ff1bfa7b47d2e5001a
TalesAugusto1 <tales.a.s.furlan@gmail.com>
2023-01-03 14:13:00 -0300

    tests

--- src/controllers/BaseController.js
+++ src/controllers/BaseController.js
@@ -1,30 +1,41 @@
-import { db } from "@/firebase"
-import { doc, setDoc, updateDoc, query, where, getDocs, deleteDoc, collection } from "firebase/firestore";
+import { db } from "@/firebase";
+import {
+    doc,
+    setDoc,
+    updateDoc,
+    query,
+    where,
+    getDocs,
+    deleteDoc,
+    collection,
+} from "firebase/firestore";
 
 export default class Controller {
-  constructor() { }
+    constructor() {}
 
-  async create(path, document, data) {
-    await setDoc(doc(db, path, document), data);
-  } 
+    async create(path, document, data) {
+        await setDoc(doc(db, path, document), data);
+    }
 
-  async read(path, parameter, condition) {
-    const q = query(collection(db, path), where(parameter, "==", condition));
-    const querySnapshot = await getDocs(q);
-    const res = [];
-    querySnapshot.forEach((doc) => {
-      res.push(doc.data())
-    });
-    console.log("CONTROLLER RESPONSE ====>>>>", res)
-    return res;
-  }
+    async read(path, parameter, condition) {
+        const q = query(
+            collection(db, path),
+            where(parameter, "==", condition)
+        );
+        const querySnapshot = await getDocs(q);
+        const res = [];
+        querySnapshot.forEach((doc) => {
+            res.push(doc.data());
+        });
+        return res;
+    }
 
-  async update(path, document, payload) {
-    const uploadFile = doc(db, path, document);
-    await updateDoc(uploadFile, payload);
-  }
+    async update(path, document, payload) {
+        const uploadFile = doc(db, path, document);
+        await updateDoc(uploadFile, payload);
+    }
 
-  async delete(path, document) {
-    await deleteDoc(doc(db, path, document));
-  }
+    async delete(path, document) {
+        await deleteDoc(doc(db, path, document));
+    }
 }
