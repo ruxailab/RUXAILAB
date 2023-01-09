ae0cf1f4ac2592dd1f63dfb7c7dc97ea7b40a0c2
TalesAugusto1 <tales.a.s.furlan@gmail.com>
2023-01-09 13:37:27 -0300

    big bug of login page fixed, now it need the path to the other pages

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -24,13 +24,14 @@ export default class AuthController {
     //SignIn
 
     async authSingIn(email, password) {
-        await signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
+        return signInWithEmailAndPassword(auth, email, password).then(
+            (userCredential) => {
                 const user = userCredential.user;
                 console.log("User SingIn: " + user);
                 return user;
-            })
-            .catch(console.log("Error in SingIn"));
+            }
+        );
+        //           .catch(console.log("Error in SingIn"));
     }
 
     //Get Current User
