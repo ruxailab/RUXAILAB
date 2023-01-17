543af901ba6fb2ccef3b4092fbb8bb228463ee76
tamitavares <tamiris.lemes18@gmail.com>
2023-01-17 00:51:53 -0300

    Fix bugs to SignUp

--- src/controllers/BaseController.js
+++ src/controllers/BaseController.js
@@ -12,51 +12,6 @@ import {
 } from "firebase/firestore";
 
 export default class Controller {
-<<<<<<< HEAD
-  constructor() {}
-
-  // path - collection
-  // data - document to insert (object)
-
-  async create(path, document, data) {
-    await setDoc(doc(db, path, document), data);
-  } 
-
-  //model to define in Controller
-
-  async read(path, parameter, condition) {
-    const q = query(collection(db, path), where(parameter, "==", condition));
-    const querySnapshot = await getDocs(q);
-    const res = [];
-    querySnapshot.forEach((doc) => {
-      res.push(doc.data())
-    });
-    //console.log("CONTROLLER RESPONSE ====>>>>", res)
-    return res
-  }
-
-  async readAll(path) {
-    const q = query(collection(db, path));
-    const querySnapshot = await getDocs(q);
-    const res = [];
-    querySnapshot.forEach((doc) => {
-      res.push(doc.data())
-    });
-    //console.log("CONTROLLER RESPONSE ====>>>>", res)
-    return res
-  }
- 
-
-  async update(path, document, payload) {
-    const uploadFile = doc(db, path, document);
-    await updateDoc(uploadFile, payload);
-  }
-
-  async delete(path, document) {
-    await deleteDoc(doc(db, path, document));
-  }
-}
-=======
     constructor() {}
 
     // path - collection
@@ -115,4 +70,3 @@ export default class Controller {
         await deleteDoc(doc(db, path, document));
     }
 }
->>>>>>> c1de5b2bfdc6cb2f39692e90853fdabedcc4061a
