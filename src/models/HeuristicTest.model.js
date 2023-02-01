9bb462989e667bbf39511e460e54fbba116ba85f
TalesAugusto1 <tales.a.s.furlan@gmail.com>
2023-02-01 16:17:52 -0300

    hours working

--- src/models/HeuristicTest.model.js
+++ src/models/HeuristicTest.model.js
@@ -4,22 +4,30 @@ import Test from "./Test";
  * @param {Object[]} heuristics  - An array of HeuristicQuestion value.
  */
 
-export default class Heuristic extends Test{
-  constructor({
-    heuristics,
-    testTitle,
-    testDescription
-  } = {}
-  ) {
-    super({testTitle, testDescription})
-    this.heuristics = heuristics ?? null;
-    this.testType='heuristic' 
-  }
- 
-  toFirestore(){
-    return  Object.assign(super.toFirestore(), {heuristics: this.heuristics, testType: this.testType})
-  }
-  static toHeuristicTest(data) {
-      return new Heuristic(data)
-  }
+export default class Heuristic extends Test {
+    constructor({ heuristics, testTitle, testDescription } = {}) {
+        super({ testTitle, testDescription });
+        this.heuristics = heuristics ?? null;
+        this.testType = "heuristic";
+        let date = new Date();
+        let current_date =
+            date.getFullYear() +
+            "-" +
+            (date.getMonth() + 1) +
+            "-" +
+            date.getDate();
+        let current_time =
+            date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+        this.creationDate = current_date + " - " + current_time;
+    }
+
+    toFirestore() {
+        return Object.assign(super.toFirestore(), {
+            heuristics: this.heuristics,
+            testType: this.testType,
+        });
+    }
+    static toHeuristicTest(data) {
+        return new Heuristic(data);
+    }
 }
