907198e492bd84e9686dc84e76e4cfc44547ed22
TalesAugusto1 <tales.a.s.furlan@gmail.com>
2023-02-01 13:14:01 -0300

    all pu console.logs removed, navbar in progress

--- src/store/modules/newStores/Database.js
+++ src/store/modules/newStores/Database.js
@@ -42,7 +42,7 @@ export default {
         },
         async getObject({ commit }, payload) {
             try {
-                console.log("puta puta");
+
                 console.log(payload);
                 let x = await new Controller().read(
                     payload.collection,
