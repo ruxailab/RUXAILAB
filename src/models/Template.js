efcb873681e2844c2d8303037b53b1ab5ae2e4a7
marc <marcgc21@gmail.com>
2023-02-07 16:06:40 -0300

    chore: templates

--- src/models/Template.js
+++ src/models/Template.js
@@ -6,13 +6,52 @@
 
 export default class Template{
     constructor({
-        body, header
-    } = {}
+        id,
+        creationDate,
+        description,
+        title, 
+        type,
+        version, 
+        authorEmail,
+        authorDocId,
+        isPublic,
+        answersSheet,
+        testStructure,
+        testOptions,
+        testId,
+        } = {}
     ) {
-        this.body = body;
-        this.header = header;
+        this.id = id;
+        this.creationDate = creationDate;
+        this.description = description;
+        this.title = title;
+        this.type = type;
+        this.version = version;
+        this.authorEmail = authorEmail;
+        this.authorDocId = authorDocId;
+        this.isPublic = isPublic;
+        this.answersSheet = answersSheet;
+        this.testStructure= testStructure;
+        this.testOptions = testOptions;
+        this.testId = testId
     }
     static toTemplate(data) {
         return new Template(data)
+    }
+    toFirestore() {
+        return {
+           creationDate: this.creationDate,
+           description:  this.description,
+            title:  this.title,
+           type: this.type,
+           version: this.version,
+           authorEmail: this.authorEmail,
+           authorDocId:   this.authorDocId,
+           isPublic: this.isPublic,
+           answersSheet: this.answersSheet,
+           testStructure: this.testStructure,
+           testOptions:  this.testOptions,
+           testId:  this.testId,
+        };
     }
 }
