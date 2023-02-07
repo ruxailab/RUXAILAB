1a75e4f745628d7d87d31272a6d84f506520b76b
marc <marcgc21@gmail.com>
2023-02-07 13:43:42 -0300

    chore: leave alert store system

--- src/store/index.js
+++ src/store/index.js
@@ -16,6 +16,9 @@ export default new Vuex.Store({
     state:{
         loading:false,
         error:null,
+        dialogLeave:false,
+        localChanges:false,
+        pathTo:null,
     },
     mutations: {
         setLoading(state, payload) {
@@ -24,6 +27,24 @@ export default new Vuex.Store({
         setError(state, payload) {
           state.error = payload
         },
+        SET_DIALOG_LEAVE(state,payload){
+          console.log('aqui')
+          state.dialogLeave = payload
+        },
+        SET_LOCAL_CHANGES(state,payload){
+          state.localChanges = payload
+        },
+        SET_PATH_TO(state,payload){
+          state.pathTo = payload
+        }
+    },
+    getters:{
+      getDialogLeaveStatus(state){
+        return state.dialogLeave
+      },
+      localChanges(state){
+        return state.localChanges
+      }
     },
     modules: {
         Auth,
