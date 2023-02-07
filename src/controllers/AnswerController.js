a30f9031681a41d1e11f26a3282252462b2bef33
karine <karine.pistili@gmail.com>
2023-02-07 18:24:29 -0300

    fixed change role of cooperator

--- src/controllers/AnswerController.js
+++ src/controllers/AnswerController.js
@@ -1,7 +1,10 @@
 // import Answer from "@/models/Answer"
 
 import Controller from "@/controllers/BaseController";
+import UserController from "./UserController";
 const COLLECTION = "answers";
+
+const userController = new UserController()
 
 export default class AnswerController extends Controller {
     constructor() {
@@ -10,5 +13,12 @@ export default class AnswerController extends Controller {
 
     async createAnswer(payload) {
         return await super.create(COLLECTION, payload.toFirestore());
+    }
+
+    async updateUserAnswer(payload) {
+        const userToUpdate = await userController.getById(payload.cooperatorId)
+        const index = userToUpdate.myAnswers.findIndex((a) => a.testDocId === payload.testDocId)
+        userToUpdate.myAnswers[index] = Object.assign(userToUpdate.myAnswers[index], payload.data)
+        return userController.update(userToUpdate.id, userToUpdate.toFirestore())
     }
 }
