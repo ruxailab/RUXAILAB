25e7c045706f6b63831fd6554fee107f861688c9
karine <karine.pistili@gmail.com>
2023-02-22 11:15:23 -0300

    feat(templates): implemented create template from a test with new structure

--- src/models/TemplateHeader.js
+++ src/models/TemplateHeader.js
@@ -1,16 +1,18 @@
-     /**
-     * Create a TemplateHeader.
-     * @param {Object} templateAuthor - The TemplateAuthor value.
-     * @param {string} creationDate - The creationDate value.
-     * @param {string} updateDate - The updateDate value.
-     * @param {string} templateDescription - The templateDescription value.
-     * @param {boolean} isTemplatePublic - The isTemplatePublic value.
-     * @param {string} templateTitle - The templateTitle value.
-     * @param {string} templateVersion - The templateVersion value.
-     * @param {string} templateType - The templateType value.
-     */
+/**
+* Create a TemplateHeader.
+* @param {Object} templateAuthor - The TemplateAuthor value.
+* @param {string} creationDate - The creationDate value.
+* @param {string} updateDate - The updateDate value.
+* @param {string} templateDescription - The templateDescription value.
+* @param {boolean} isTemplatePublic - The isTemplatePublic value.
+* @param {string} templateTitle - The templateTitle value.
+* @param {string} templateVersion - The templateVersion value.
+* @param {string} templateType - The templateType value.
+*/
 
-export default class TemplateHeader{
+import TemplateAuthor from "./TemplateAuthor";
+
+export default class TemplateHeader {
     constructor({
         templateAuthor, creationDate, updateDate, templateDescription, isTemplatePublic, templateTitle, templateVersion, templateType
     } = {}
@@ -19,12 +21,27 @@ export default class TemplateHeader{
         this.creationDate = creationDate;
         this.updateDate = updateDate;
         this.templateDescription = templateDescription;
-        this.isTemplatePublic = isTemplatePublic;
+        this.isTemplatePublic = isTemplatePublic ?? false;
         this.templateTitle = templateTitle;
         this.templateVersion = templateVersion;
         this.templateType = templateType;
     }
     static toTemplateHeader(data) {
-        return new TemplateHeader(data)
+        return new TemplateHeader({
+            ...data,
+            templateAuthor: TemplateAuthor.toTemplateAuthor(data.templateAuthor)
+        })
+    }
+    toFirestore() {
+        return {
+            templateAuthor: this.templateAuthor.toFirestore(),
+            creationDate: this.creationDate,
+            updateDate: this.updateDate,
+            templateDescription: this.templateDescription,
+            isTemplatePublic: this.isTemplatePublic,
+            templateTitle: this.templateTitle,
+            templateVersion: this.templateVersion,
+            templateType: this.templateType,
+        }
     }
 }
