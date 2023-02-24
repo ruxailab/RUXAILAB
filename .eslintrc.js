68b0f3490b80be70b627f79fd8c8316006e9894d
Michell Algarra <michell.algarra@gmail.com>
2023-02-24 11:07:12 -0300

    build: improved code formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off',
+    semi: ['warning', 'all'],
+    'max-len': 'off',
+    'linebreak-style': 'off',
+    camelcase: [
+      'error',
+      { properties: 'never', ignoreDestructuring: true, ignoreImports: true },
+    ],
+    'arrow-parens': ['warn', 'always'],
+    'vue/multiline-html-element-content-newline': 'off',
+  },
 }
