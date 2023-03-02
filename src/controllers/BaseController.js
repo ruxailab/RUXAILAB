be186451658ce95a5b552ea89e83b70828900a58
Michell Algarra <michell.algarra@gmail.com>
2023-03-02 12:04:54 -0300

    fix: fixed the analytics rendering

--- src/controllers/BaseController.js
+++ src/controllers/BaseController.js
@@ -1,63 +1,123 @@
-import { db } from "@/firebase";
+/* eslint-disable max-len */
+
+import { db } from '@/firebase'
 import {
-    doc,
-    updateDoc,
-    getDoc,
-    addDoc,
-    query,
-    where,
-    getDocs,
-    deleteDoc,
-    collection,
-} from "firebase/firestore";
+  doc,
+  updateDoc,
+  getDoc,
+  addDoc,
+  query,
+  where,
+  getDocs,
+  deleteDoc,
+  collection,
+  orderBy,
+  limit,
+} from 'firebase/firestore'
+
+/**
+ * @typedef {Object} OrderBy
+ * @property {string} field The field to be sorted.
+ * @property {'asc'|'desc'} mode The sorting mode.
+ */
+
+/**
+ * @typedef {'!='|'<'|'<='|'=='|'>'|'>='|'array-contains'|'array-contains-any'|'in'|'not-in'} Operator
+ */
+
+/**
+ * @typedef {Object} Where
+ * @property {string} field The field name to be used in the filtering.
+ * @property {Operator} operator The operator to be used to compare the field with the value.
+ * @property {any} value The value to be used on the comparison.
+ */
+
+/**
+ * @typedef {Object} Query
+ * @property {number} limit The amount of items to get.
+ * @property {Where[]} where The filters to be applied.
+ * @property {OrderBy[]} orderBy A list with all sorting fields.
+ */
 
 export default class Controller {
-    constructor() { }
+  // path - collection
+  // data - document to insert (object)
+  async create(col, payload) {
+    return addDoc(collection(db, col), payload)
+  }
 
-    // path - collection
-    // data - document to insert (object)
-    async create(col, payload) {
-        return addDoc(collection(db, col), payload);
-    }
+  async readOne(col, docId) {
+    const ref = doc(db, `${col}/${docId}`)
 
-    async readOne(col, docId) {
-        const ref = doc(db, `${col}/${docId}`);
-        return getDoc(ref);
-    }
+    return getDoc(ref)
+  }
 
-    async query(col, params) {
-        const q = query(collection(db, col), where(params.field, params.condition, params.value))
-        return getDocs(q);
-    }
+  async query(col, params) {
+    const q = query(
+      collection(db, col),
+      where(params.field, params.condition, params.value),
+    )
 
-    /**
-     * Gets a document data by its id and collection.
-     *
-     * @param {string[]} path the document path to be accessed.
-     * @throws a Firebase exception.
-     */
-    async getById(path) {
-        // return getDoc(doc(db, ...path));
-        const docRef = doc(db, path[0], path[1]);
-        return getDoc(docRef);
-    }
+    return getDocs(q)
+  }
 
-    async readAll(path) {
-        const q = query(collection(db, path));
-        const querySnapshot = await getDocs(q);
-        const res = [];
-        querySnapshot.forEach((doc) => {
-            res.push(Object.assign(doc.data(), { id: doc.id }));
-        });
-        return res;
-    }
+  /**
+   * Makes a query to get data from the database.
+   *
+   * @param {string} col the collection name.
+   * @param {Query} constraints the query to be used.
+   * @returns {Promise<any[]>} the found entities.
+   */
+  async queryCustom(col, constraints = {}) {
+    const { orderBy: ob = [], where: wh = [], limit: lm = 0 } = constraints
 
-    async update(col, docId, payload) {
-        const ref = doc(db, `${col}/${docId}`)
-        return updateDoc(ref, payload)
-    }
+    const q = query(
+      collection(col),
+      ...ob.map((o) => orderBy(o.field, o.mode)),
+      ...wh.map((w) => where(w.field, w.operator, w.value)),
+      limit(lm),
+    )
 
-    async delete(col, docId) {
-        return deleteDoc(doc(db, col, docId));
-    }
+    const snap = await getDocs(q)
+
+    return snap.docs.map((document) => ({
+      ...document.data(),
+      id: document.id,
+    }))
+  }
+
+  /**
+   * Gets a document data by its id and collection.
+   *
+   * @param {string[]} path the document path to be accessed.
+   * @throws a Firebase exception.
+   */
+  async getById(path) {
+    // return getDoc(doc(db, ...path));
+    const docRef = doc(db, path[0], path[1])
+    return getDoc(docRef)
+  }
+
+  async readAll(path) {
+    const q = query(collection(db, path))
+    const querySnapshot = await getDocs(q)
+
+    const res = []
+
+    querySnapshot.forEach((document) => {
+      res.push(Object.assign(document.data(), { id: document.id }))
+    })
+
+    return res
+  }
+
+  async update(col, docId, payload) {
+    const ref = doc(db, `${col}/${docId}`)
+
+    return updateDoc(ref, payload)
+  }
+
+  async delete(col, docId) {
+    return deleteDoc(doc(db, col, docId))
+  }
 }
