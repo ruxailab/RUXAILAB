27e8ab8bad64adb06535ee71308ff879c8ff3050
tales furlan <tales.furlan@gmail.com>
2023-03-06 13:20:35 -0300

    begin of import images with description

---
+++ src/components/atoms/ImportImage.vue
@@ -0,0 +1,23 @@
+<template>
+<div><input type="file" name="my-image" id="image" accept="image/gif, image/jpeg, image/png" /></div>
+</template>
+
+<style>
+
+</style>
+<script>
+export default {
+  data: () => ({
+  }),
+  methods: {
+   
+  },
+  computed: {
+    test() {
+      return this.$store.state.Tests.Test;
+    },
+  
+  }
+};
+</script>
+
