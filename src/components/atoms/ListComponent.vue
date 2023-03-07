7d6da658120e63c3aabaea245a47df903eb34c8d
tamitavares <tamiris.lemes18@gmail.com>
2023-03-07 13:35:32 -0300

    Save tasks in Firebase

--- src/components/atoms/ListComponent.vue
+++ src/components/atoms/ListComponent.vue
@@ -1,19 +1,18 @@
 <template>
   <div>
-    <v-list class="py-0" v-if="items">
+    <v-list v-if="items" class="py-0">
       <div v-for="(item, n) in items" :key="n">
-        <v-list-item @click="emitClick(item)" :ripple="false" v-if="item">
+        <v-list-item v-if="item" :ripple="false" @click="emitClick(item)">
           <!-- Avatar -->
           <v-list-item-avatar tile style="border-radius: 5px" size="40">
             <v-avatar tile :color="generateColor()" style="color: #545454">
               <span
                 v-if="type === 'myTemplates' || type === 'publicTemplates'"
-                >{{ item.header.templateTitle[0].toUpperCase() }}</span
-              >
+              >{{ item.header.templateTitle[0].toUpperCase() }}</span>
               <span v-else>{{
                 item.testTitle[0].toUpperCase()
-              }}</span></v-avatar
-            >
+              }}</span>
+            </v-avatar>
           </v-list-item-avatar>
 
           <v-list-item-content>
@@ -22,16 +21,20 @@
               v-if="type === 'myTemplates' || type === 'publicTemplates'"
             >
               {{ item.header.templateTitle }}
-              <v-chip label outlined style="color: grey" small class="ml-1">{{
-                item.header.templateType
-              }}</v-chip>
+              <v-chip label outlined style="color: grey" small class="ml-1">
+                {{
+                  item.header.templateType
+                }}
+              </v-chip>
             </v-list-item-title>
             <v-list-item-title v-else>
               {{ item.testTitle }}
               <!-- Tales -->
-              <v-chip label outlined style="color: grey" small class="ml-1">{{
-                item.testType
-              }}</v-chip>
+              <v-chip label outlined style="color: grey" small class="ml-1">
+                {{
+                  item.testType
+                }}
+              </v-chip>
             </v-list-item-title>
 
             <!-- Subtitle -->
@@ -44,36 +47,40 @@
                 item.testAdmin
                   ? item.testAdmin.email
                   : item.header
-                  ? item.header.templateAuthor.userEmail
-                  : ""
+                    ? item.header.templateAuthor.userEmail
+                    : ""
               }}</strong>
             </v-list-item-subtitle>
             <div
               class="hidden-sm-and-down"
               style="position: absolute; right: 25%"
             >
-              <v-tooltip top v-if="type === 'myTests'">
+              <v-tooltip v-if="type === 'myTests'" top>
                 <template v-slot:activator="{ on, attrs }">
                   <v-row
                     class="ma-0 pa-0"
                     v-bind="attrs"
-                    v-on="on"
                     align="center"
+                    v-on="on"
                   >
                     {{
                       item.numberColaborators >= 0
                         ? item.numberColaborators
                         : "-"
                     }}
-                    <v-icon class="ml-1">mdi-account-multiple</v-icon>
+                    <v-icon class="ml-1">
+                      mdi-account-multiple
+                    </v-icon>
                   </v-row>
                 </template>
                 <span>Cooperators</span>
               </v-tooltip>
-              <v-tooltip top v-else-if="type === 'sharedWithMe'">
+              <v-tooltip v-else-if="type === 'sharedWithMe'" top>
                 <template v-slot:activator="{ on, attrs }">
                   <v-row v-bind="attrs" v-on="on">
-                    <div class="caption">{{ item.progress }}%</div>
+                    <div class="caption">
+                      {{ item.progress }}%
+                    </div>
 
                     <v-progress-circular
                       rotate="-90"
@@ -81,7 +88,7 @@
                       color="grey darken-1"
                       :size="20"
                       class="ml-1"
-                    ></v-progress-circular>
+                    />
                   </v-row>
                 </template>
                 <span>Progress</span>
@@ -99,30 +106,31 @@
                 outlined
                 class="my-1"
                 :color="getAccessLevelColor(item.accessLevel)"
-                >{{ getAccessLevelText(item.accessLevel) }}</v-chip
               >
+                {{ getAccessLevelText(item.accessLevel) }}
+              </v-chip>
             </v-list-item-action-text>
-            <v-list-item-action-text v-if="item.updateDate"
-              >Last Updated on
-              {{ getFormattedDate(item.updateDate) }}</v-list-item-action-text
-            >
+            <v-list-item-action-text v-if="item.updateDate">
+              Last Updated on
+              {{ getFormattedDate(item.updateDate) }}
+            </v-list-item-action-text>
             <v-list-item-action-text
               v-if="type === 'myTemplates' || type === 'publicTemplates'"
             >
-              <v-chip outlined small class="ml-1" label
-                >Version: {{ item.header.templateVersion }}</v-chip
-              >
+              <v-chip outlined small class="ml-1" label>
+                Version: {{ item.header.templateVersion }}
+              </v-chip>
             </v-list-item-action-text>
           </v-list-item-action>
         </v-list-item>
-        <v-divider v-if="n !== items.length - 1"></v-divider>
+        <v-divider v-if="n !== items.length - 1" />
       </div>
 
       <v-row
+        v-if="items.length <= 0"
         justify="center"
         align="center"
         class="ma-0 mt-2 pa-0"
-        v-if="items.length <= 0"
       >
         <span
           v-if="
@@ -130,11 +138,8 @@
               type === 'publicTests' ||
               type === 'sharedWithMe'
           "
-          >No tests found</span
-        >
-        <span v-else-if="type === 'myTemplates' || type === 'publicTemplates'"
-          >No templates found</span
-        >
+        >No tests found</span>
+        <span v-else-if="type === 'myTemplates' || type === 'publicTemplates'">No templates found</span>
       </v-row>
     </v-list>
     <!-- <v-row v-if="hasPagination" justify="center" class="mt-5">
@@ -155,7 +160,7 @@ export default {
       type: Array,
       required: true,
       default: function() {
-        return [];
+        return []
       },
     },
     type: {
@@ -174,53 +179,53 @@ export default {
     },
   },
   data: () => ({}),
+  beforeUpdate() {
+    const availableTypes = [
+      "myTests",
+      "publicTests",
+      "sharedWithMe",
+      "myTemplates",
+      "publicTemplates",
+    ]
+
+    if (!availableTypes.includes(this.type)) {
+      console.error(this.type + " type in ListTests.vue is not valid.")
+    }
+  },
   methods: {
     getFormattedDate(date) {
-      const d = new Date(date);
-      return d.toLocaleString();
+      const d = new Date(date)
+      return d.toLocaleString()
     },
     getAccessLevelText(accessLevel) {
       return accessLevel === 0
         ? "Administrator"
         : accessLevel === 1
         ? "Evaluator"
-        : "Guest";
+        : "Guest"
     },
     getAccessLevelColor(accessLevel) {
       return accessLevel === 0
         ? "primary"
         : accessLevel === 1
         ? "secondary"
-        : "warning";
+        : "warning"
     },
     generateColor() {
-      let hue = Math.floor(Math.random() * 360);
-      let color = "hsl(" + hue + ", 80%, 80%)";
+      const hue = Math.floor(Math.random() * 360)
+      const color = "hsl(" + hue + ", 80%, 80%)"
 
-      return color;
+      return color
     },
     emitClick(item) {
-      this.$emit("clicked", item);
+      this.$emit("clicked", item)
     },
     emitNextPage() {
-      this.$emit("nextPage");
+      this.$emit("nextPage")
     },
     emitPreviousPage() {
-      this.$emit("previousPage");
+      this.$emit("previousPage")
     },
   },
-  beforeUpdate() {
-    let availableTypes = [
-      "myTests",
-      "publicTests",
-      "sharedWithMe",
-      "myTemplates",
-      "publicTemplates",
-    ];
-
-    if (!availableTypes.includes(this.type)) {
-      console.error(this.type + " type in ListTests.vue is not valid.");
-    }
-  },
-};
+}
 </script>
