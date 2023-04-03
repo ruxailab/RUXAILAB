038039b4c4d5c5702cfb9fa19f2aebccd4e279ae
tales furlan <tales.furlan@gmail.com>
2023-04-03 12:02:16 -0300

    before implement the steps

--- src/views/admin/FinalReportView.vue
+++ src/views/admin/FinalReportView.vue
@@ -51,6 +51,56 @@
       @click="textIsDone = !textIsDone"
       >Next step</v-btn
     >
+
+    <v-stepper v-model="e1">
+      <v-stepper-header>
+        <v-stepper-step :complete="e1 > 1" step="1"
+          >FINAL REPORT</v-stepper-step
+        >
+
+        <v-divider></v-divider>
+
+        <v-stepper-step :complete="e1 > 2" step="2"
+          >SELECTION BOX</v-stepper-step
+        >
+
+        <v-divider></v-divider>
+
+        <v-stepper-step step="3">PREVIEW</v-stepper-step>
+      </v-stepper-header>
+
+      <v-stepper-items>
+        <v-stepper-content step="1">
+          <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+
+          <v-btn color="primary" @click="e1 = 2">
+            Continue
+          </v-btn>
+
+          <v-btn flat>Cancel</v-btn>
+        </v-stepper-content>
+
+        <v-stepper-content step="2">
+          <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+
+          <v-btn color="primary" @click="e1 = 3">
+            Continue
+          </v-btn>
+
+          <v-btn flat>Cancel</v-btn>
+        </v-stepper-content>
+
+        <v-stepper-content step="3">
+          <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+
+          <v-btn color="primary" @click="e1 = 1">
+            Continue
+          </v-btn>
+
+          <v-btn flat>Cancel</v-btn>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
   </div>
 </template>
 
