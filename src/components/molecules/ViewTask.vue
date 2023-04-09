e68c9b9d1d63efc7d064f8ae0b2cd232edeba2ab
tamitavares <tamiris.lemes18@gmail.com>
2023-04-08 23:34:18 -0300

    Create a Timer in ViewTask

--- src/components/molecules/ViewTask.vue
+++ src/components/molecules/ViewTask.vue
@@ -17,14 +17,34 @@
         </v-row>
         <v-spacer />
         <!-- Criar link para hasPost aqui com v-if -->
-        <v-row justify="center">
-          <v-btn v-if="item.hasTimer === true" color="success">
-            <v-icon left>
-              mdi-timer
-            </v-icon>Timer
-          </v-btn>
-        </v-row>
+        <!-------------- Timer -------------->
+        <v-card>
+          <v-row justify="center">
+            <v-col>
+              <v-card-title v-if="item.hasTimer === true" color="success">
+                <v-icon left>
+                  mdi-timer
+                </v-icon>Timer
+              </v-card-title>
+            </v-col>
+            <v-col>
+              <v-row justify="center">
+                {{ prettyTime }}
+              </v-row>
+              <v-btn v-if="!isRunning" @click="start">
+                Start
+              </v-btn>
+              <v-btn v-if="isRunning" @click="stop">
+                Stop
+              </v-btn>
+              <v-btn @click="reset">
+                Reset
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card>
         <v-spacer />
+        <!----------------------------------->
         <br>
         <v-spacer />
         <v-row justify="center">
@@ -84,9 +104,7 @@
             />
           </v-col>
         </v-row>
-      </v-col>
-    </v-row>
-    <!-- <v-row v-else class="fill-height" align="center" justify="center">
+        <!-- <v-row v-else class="fill-height" align="center" justify="center">
       <iframe
         :src="item.postTest"
         width="100%"
@@ -96,6 +114,8 @@
         marginwidth="0"
       >Carregando…</iframe>
     </v-row> -->
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -115,11 +135,53 @@ export default {
     //   default: false,
     // },
   },
+  data: () => ({
+    isRunning: false,
+    minutes: 0,
+    seconds: 0,
+    time: 0,
+    timer: null,
+  }),
+  computed: {
+		prettyTime () {
+			 const time = this.time / 60
+			 const minutes = parseInt(time)
+			 const seconds = Math.round((time - minutes) * 60)
+			 return minutes+":"+seconds
+		}
+	},
+
   methods: {
     updated() {
       this.$emit("updatedAnswer", this.item)
     },
+    start() {
+      this.isRunning = true
+      if (!this.timer) {
+        this.timer = setInterval(() => {
+          if (this.time < 60) {
+            this.time++
+          } else {
+            alert("Timeout reached!")
+            this.stop()
+          }
+        }, 1000)
+      }
+    },
+    stop() {
+      this.isRunning = false
+      clearInterval(this.timer)
+      this.timer = null
+    },
+    reset() {
+      this.stop()
+      this.time = 0
+      this.seconds = 0
+      this.minutes = 0
+    },
+
   },
+
 }
 </script>
 
