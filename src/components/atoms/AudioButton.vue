e678e8055a925de14aa24f0f612d8787004e6631
tamitavares <tamiris.lemes18@gmail.com>
2023-04-18 17:37:27 -0300

    Update Audio Record

--- src/components/atoms/AudioButton.vue
+++ src/components/atoms/AudioButton.vue
@@ -7,21 +7,19 @@
         </v-icon>Audio Record
       </v-card-title>
     </v-row>
-    <v-row justify="center">
-      <v-btn v-if="!isRunningAudio" color="green" @click="startAudioRecording">
-        <v-icon> mdi-microphone </v-icon>
-      </v-btn>
-      <v-btn v-if="isRunningAudio" color="blue" @click="stopAudioRecording">
-        <v-icon> mdi-microphone-off </v-icon>
-      </v-btn>
-      <v-btn color="red">
-        <v-icon> mdi-replay </v-icon>
-      </v-btn>
+    <v-row align="center">
+      <v-col justify="center">
+        <v-btn v-if="!isRunningAudio" color="green" @click="startRecording()">
+          <v-icon> mdi-microphone </v-icon>
+        </v-btn>
+        <v-btn v-if="isRunningAudio" color="blue" @click="stop()">
+          <v-icon> mdi-microphone-off </v-icon>
+        </v-btn>
+      </v-col>
+      <v-col justify="center">
+        <audio controls :src="recordedAudio" />
+      </v-col>
     </v-row>
-    <br>
-    <v-col>
-      <audio controls />
-    </v-col>
   </v-card>
 </template>
 
@@ -41,109 +39,35 @@ export default {
   },
   data: () => ({
     dialog: false,
-    audioRecorder: {
-      start: false,
-      stop: false,
-      cancel: false
-    },
     isRunningAudio: false,
-    audioBlobs: [],
-    mediaRecorder: null,
-    streamBeingCaptured: null,
+    recorder: null,
+    recordedChunks: [],
+    recordedAudio: null,
   }),
+  created() {
+    this.device = navigator.mediaDevices.getUserMedia({ audio: true })
+  },
   methods: {
-    async start() {
-      //Feature Detection
-      if (!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
-        //Feature is not supported in browser
-        //return a custom error
-        return Promise.reject(new Error('mediaDevices API or getUserMedia method is not supported in this browser.'))
-      }
-      else {
-        this.isRunningAudio = true
-        //Feature is supported in browser
-
-        //create an audio stream
-        return navigator.mediaDevices.getUserMedia({ audio: true }/*of type MediaStreamConstraints*/)
-          //returns a promise that resolves to the audio stream
-          .then((stream) /*of type MediaStream*/ => {
-            streamBeingCaptured = stream
-            mediaRecorder = new MediaRecorder(stream)
-            audioBlobs = []  //save
-            mediaRecorder.addEventListener("dataavailable", event => {
-              //store audio Blob object
-              audioBlobs.push(event.data)
-            })
-            mediaRecorder.start()
-
-          })
-      }
+    startRecording() {
+      this.isRunningAudio = true
+      this.recordedChunks = []
+      navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+        this.recorder = new MediaRecorder(stream)
+        this.recorder.addEventListener("dataavailable", (event) => {
+          this.recordedChunks.push(event.data)
+        })
+        this.recorder.addEventListener("stop", () => {
+          const audioBlob = new Blob(this.recordedChunks, { type: "audio/webm" })
+          this.recordedAudio = URL.createObjectURL(audioBlob)
+        })
+        this.recorder.start()
+      })
     },
     stop() {
-      return new Promise(resolve => {
-        //save audio type to pass to set the Blob type
-        const mimeType = mediaRecorder.mimeType
-
-        //listen to the stop event in order to create & return a single Blob object
-        mediaRecorder.addEventListener("stop", () => {
-          //create a single blob object, as we might have gathered a few Blob objects that needs to be joined as one
-          const audioBlob = new Blob(audioBlobs, { type: mimeType })
-
-          //resolve promise with the single audio blob representing the recorded audio
-          resolve(audioBlob)
-        })
-
-        //stop the recording feature
-        mediaRecorder.stop()
-
-        //stop all the tracks on the active stream in order to stop the stream
-        stopStream()
-
-        //reset API properties for next recording
-        resetRecordingProperties()
-
-        this.isRunningAudio = false
-
-      })
-
+      this.isRunningAudio = false
+      this.recorder.stop()
+      this.recorder = null
     },
-    stopStream() {
-      streamBeingCaptured.getTracks()
-      .forEach(track /*of type MediaStreamTrack*/ => track.stop())
-    },
-    resetRecordingProperties() {
-      mediaRecorder = null
-      streamBeingCaptured = null
-    },
-    cancel() {
-
-    },
-
-    //-------------------------------
-    startAudioRecording() {
-      this.start()
-      //start recording using the audio recording API
-      if (this.isRunningAudio) {
-        alert("Recording Audio")
-        console.log("Recording Audio...")
-      }
-      else {
-        alert("mediaDevices API or getUserMedia method is not supported in this browser.")
-        console.log("To record audio, use browsers like Chrome and Firefox.")
-      }
-    },
-    stopAudioRecording() {
-      alert("Stopping Audio Recording")
-      this.stop()
-      if (!this.isRunningAudio) {
-
-      }
-      else {
-        console.log("Error to stop audio")
-      }
-
-    },
-
   }
 }
 </script>
