fc114b8e180ad1bc240b75ad2d9239bea3e917e9
tales furlan <tales.furlan@gmail.com>
2023-05-14 19:26:43 -0300

    working pdf generator

--- src/components/atoms/PdfGenerateBtn.vue
+++ src/components/atoms/PdfGenerateBtn.vue
@@ -81,15 +81,60 @@ export default {
 import axios from 'axios'
 
 export default {
+  data: () => ({
+    formattedDate: '',
+  }),
   methods: {
     submitPdf() {
+      const date = new Date() // Get current date
+      const dayOfMonth = date.getDate() // Get day of the month
+      const monthNames = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ]
+      const monthName = monthNames[date.getMonth()] // Get month name
+      const year = date.getFullYear() // Get year
+
+      // Add ordinal suffix to day of month (e.g. 1st, 2nd, 3rd)
+      let dayOfMonthStr
+      switch (dayOfMonth) {
+        case 1:
+        case 21:
+        case 31:
+          dayOfMonthStr = dayOfMonth + 'st'
+          break
+        case 2:
+        case 22:
+          dayOfMonthStr = dayOfMonth + 'nd'
+          break
+        case 3:
+        case 23:
+          dayOfMonthStr = dayOfMonth + 'rd'
+          break
+        default:
+          dayOfMonthStr = dayOfMonth + 'th'
+      }
+
+      this.formattedDate = `${dayOfMonthStr} ${monthName}, ${year}`
+      console.log(this.formattedDate) // Output: "11th May, 2023"
+
       axios
         .post(
           'http://localhost:8000/api/endpoint',
+          // { item: ['olá', 'tudo bem?'] },
           {
             items: [
-              { name: 'Product 1', quantity: 2, price: 10.0 },
-              { name: 'Product 2', quantity: 1, price: 20.0 },
+              { name: 'Product 1', date: this.formattedDate, price: 10.0 },
             ],
             total: 40.0,
           },
@@ -99,9 +144,10 @@ export default {
           const url = window.URL.createObjectURL(new Blob([response.data]))
           const link = document.createElement('a')
           link.href = url
-          link.setAttribute('download', 'invoice.pdf')
+          link.setAttribute('download', 'heuristic-test.pdf')
           document.body.appendChild(link)
           link.click()
+          console.log(response)
         })
         .catch((error) => {
           console.error(error)
