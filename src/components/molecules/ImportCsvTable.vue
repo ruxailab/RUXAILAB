ab7dd3130b0e40fa9a5f8134df5e6b6f3d4a4467
tales furlan <tales.furlan@gmail.com>
2023-08-10 09:24:06 -0300

    now the accents are correct as the json is been read as text and not binary while converting the csv to a test

--- src/components/molecules/ImportCsvTable.vue
+++ src/components/molecules/ImportCsvTable.vue
@@ -57,84 +57,53 @@ export default {
       this.loadingUpdate = true
       try {
         let confirmationText =
-          'If you accept, all your present heuristics will be replaced for the ones on .csv file'
-        if (confirm(confirmationText) == true) {
-          const testId = this.$store.state.Tests.Test.id
-          console.log(testId)
-          console.log(this.$store.state.Tests.Test)
-          let lines = ''
-          let currentline = ''
-          let csv = ''
-          let headers = ''
-          let result = []
-          let result2 = []
-          let reader = new FileReader()
-          reader.readAsBinaryString(this.csvFile)
+          'If you accept, all your present heuristics will be replaced by the ones in the .csv file'
+        if (confirm(confirmationText)) {
+          const reader = new FileReader()
+          reader.readAsText(this.csvFile, 'UTF-8') // Use readAsText with UTF-8 encoding
           reader.onload = async () => {
-            csv = reader.result
-            lines = csv.split('\r' + '\n')
-            headers = lines[0].split(';')
-            headers[0] = headers[0].slice(3)
-            for (var i = 1; i < lines.length; i++) {
-              if (!lines[i]) continue
-              let obj = {}
-              currentline = lines[i]
-              var re = /"/g
-              currentline = re[Symbol.replace](currentline, '')
-              currentline = currentline.split(';')
-              for (var j = 0; j < headers.length; j++) {
-                if (j == 0 || j == 1 || j == 2 || j == 3) {
-                  let head = headers[j]
-                  let value = currentline[j]
-                  obj[head] = value
-                }
-              }
-              result.push(obj)
-            }
-            result = JSON.stringify(result)
-            result2 = JSON.parse(result.toString())
-            function getQuestionsFromHeuristic(heuristicArray, heuristicId) {
-              return heuristicArray.filter(
-                (element) => element.HID == heuristicId,
-              )
-            }
-            let heuristicTest = []
-            let heuristicOcurrencies = 0
-            for (i = 0; i < result2.length; i++) {
-              let auxHeuristic = getQuestionsFromHeuristic(result2, i + 1)
-              if (auxHeuristic.length > 0) {
-                heuristicOcurrencies = heuristicOcurrencies + 1
-                let auxQuestions = []
-                for (j = 0; j < auxHeuristic.length; ++j) {
-                  let auxQuestion = {
-                    id: auxHeuristic[j].QID,
-                    title: auxHeuristic[j].QUESTION,
-                    descriptions: auxHeuristic[j].QUESTION,
-                    text: auxHeuristic[j].QUESTION,
-                  }
-                  auxQuestions.push(auxQuestion)
-                }
-                let setHeuristics = {
-                  id: heuristicOcurrencies,
-                  questions: auxQuestions,
-                  title: auxHeuristic[0].HEURISTIC,
+            const csv = reader.result
+            const lines = csv.split('\r\n') // Split lines using '\r\n' for cross-platform compatibility
+            const headers = lines[0].split(';').map((header) => header.trim()) // Trim headers
 
-                  total: auxQuestions.length,
-                }
+            const heuristicMap = new Map()
 
-                heuristicTest.push(setHeuristics)
-                console.log(setHeuristics)
+            for (let i = 1; i < lines.length; i++) {
+              const currentline = lines[i]
+              if (!currentline) continue
+
+              const currentFields = currentline.split(';')
+              const heuristicId = currentFields[0]
+              const heuristicTitle = currentFields[1]
+              const questionId = currentFields[2]
+              const questionText = currentFields[3]
+
+              if (!heuristicMap.has(heuristicId)) {
+                heuristicMap.set(heuristicId, {
+                  id: heuristicId,
+                  title: heuristicTitle,
+                  questions: [],
+                })
               }
+
+              const heuristicEntry = heuristicMap.get(heuristicId)
+              heuristicEntry.questions.push({
+                id: questionId,
+                title: questionText,
+                descriptions: questionText,
+                text: questionText,
+              })
             }
+
+            const heuristicTest = Array.from(heuristicMap.values())
             this.$store.state.Tests.Test.testStructure = heuristicTest
-            console.log(this.$store.state.Tests.Test)
             this.$store.dispatch('updateTest', this.test)
           }
         }
-        this.loadingUpdate = false
       } catch (error) {
         // Handle errors here if necessary
         console.error('Update action failed:', error)
+      } finally {
         this.loadingUpdate = false
       }
     },
