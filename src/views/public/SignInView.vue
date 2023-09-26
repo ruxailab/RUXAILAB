611a8826ccf6fa9d391a238f529654b193075dd2
jvJUCA <jucafranzoni1@gmail.com>
2023-09-26 13:53:47 -0300

    fix:SignIn bug

--- src/views/public/SignInView.vue
+++ src/views/public/SignInView.vue
@@ -1,142 +1,128 @@
 <template>
-    <div class="background-grey">
-        <Snackbar />
-        <v-row justify="center" style="height: 90%" align="center">
-            <v-col cols="12" md="8">
-                <v-card color="#f5f7ff" class="mx-2">
-                    <v-row>
-                        <v-col cols="12" md="5" align-self="center">
-                            <div class="card-title">
-                                {{ $t("SIGNIN.sign-in") }}
-                            </div>
+  <div class="background-grey">
+    <Snackbar />
+    <v-row justify="center" style="height: 90%" align="center">
+      <v-col cols="12" md="8">
+        <v-card color="#f5f7ff" class="mx-2">
+          <v-row>
+            <v-col cols="12" md="5" align-self="center">
+              <div class="card-title">
+                {{ $t('SIGNIN.sign-in') }}
+              </div>
 
-                            <div class="divider"></div>
+              <div class="divider"></div>
 
-                            <v-form
-                                class="mx-3"
-                                @keyup.native.enter="onSignIn()"
-                            >
-                                <v-text-field
-                                    :label="$t('SIGNIN.email')"
-                                    outlined
-                                    prepend-inner-icon="mdi-account-circle"
-                                    v-model="email"
-                                    dense
-                                ></v-text-field>
+              <v-form class="mx-3" @keyup.native.enter="onSignIn()">
+                <v-text-field
+                  :label="$t('SIGNIN.email')"
+                  outlined
+                  prepend-inner-icon="mdi-account-circle"
+                  v-model="email"
+                  dense
+                ></v-text-field>
 
-                                <v-text-field
-                                    :label="$t('SIGNIN.password')"
-                                    prepend-inner-icon="mdi-lock"
-                                    outlined
-                                    :append-icon="
-                                        showPassword ? 'mdi-eye' : 'mdi-eye-off'
-                                    "
-                                    @click:append="showPassword = !showPassword"
-                                    :type="showPassword ? 'text' : 'password'"
-                                    v-model="password"
-                                    dense
-                                ></v-text-field>
-                            </v-form>
-                            <v-card-actions class="justify-center mt-4">
-                                <v-btn
-                                    color="#F9A826"
-                                    rounded
-                                    class="white--text"
-                                    @click="onSignIn()"
-                                    :loading="loading"
-                                    >{{ $t("SIGNIN.sign-in") }}</v-btn
-                                >
-                            </v-card-actions>
-                            <v-card-actions class="justify-center mt-1">
-                                <p>
-                                    <a style="color: #F9A826" href="/signup">{{
-                                        $t("SIGNIN.dont-have-account")
-                                    }}</a>
-                                </p>
-                            </v-card-actions>
-                        </v-col>
-
-                        <v-col
-                            cols="7"
-                            class="hidden-sm-and-down"
-                            align-self="center"
-                        >
-                            <v-img src="@/assets/signIn.svg"></v-img>
-                        </v-col>
-                    </v-row>
-                </v-card>
+                <v-text-field
+                  :label="$t('SIGNIN.password')"
+                  prepend-inner-icon="mdi-lock"
+                  outlined
+                  :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                  @click:append="showPassword = !showPassword"
+                  :type="showPassword ? 'text' : 'password'"
+                  v-model="password"
+                  dense
+                ></v-text-field>
+              </v-form>
+              <v-card-actions class="justify-center mt-4">
+                <v-btn
+                  color="#F9A826"
+                  rounded
+                  class="white--text"
+                  @click="onSignIn()"
+                  :loading="loading"
+                  >{{ $t('SIGNIN.sign-in') }}</v-btn
+                >
+              </v-card-actions>
+              <v-card-actions class="justify-center mt-1">
+                <p>
+                  <a style="color: #F9A826" href="/signup">{{
+                    $t('SIGNIN.dont-have-account')
+                  }}</a>
+                </p>
+              </v-card-actions>
             </v-col>
-        </v-row>
-    </div>
+
+            <v-col cols="7" class="hidden-sm-and-down" align-self="center">
+              <v-img src="@/assets/signIn.svg"></v-img>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-import Snackbar from "@/components/atoms/Snackbar";
+import Snackbar from '@/components/atoms/Snackbar'
 //import AuthController from "@/controllers/AuthController";
 
 export default {
-    data: () => ({
-        showPassword: false,
-        email: "",
-        password: "",
-    }),
-    computed: {
-        loading() {
-            return this.$store.getters.loading;
-        },
-        user() {
-            return this.$store.getters.user;
-        },
+  data: () => ({
+    showPassword: false,
+    email: '',
+    password: '',
+  }),
+  computed: {
+    loading() {
+      return this.$store.getters.loading
     },
-    methods: {
-         async onSignIn() {
-            try{
-            await this.$store.dispatch("signin", {
-                email: this.email,
-                password: this.password,
-            })
-            .then(() => {
-                this.$router.push('/testslist').catch(() => {});
-                });
-            if (this.user) {
-                this.$router.push("/").catch(() => {}).catch(() => {});
-            }
-            }
-            catch (err) {
-                console.log("Error. " + err.message)
-            }
-        },
+    user() {
+      return this.$store.getters.user
     },
+  },
+  methods: {
+    async onSignIn() {
+      try {
+        await this.$store.dispatch('signin', {
+          email: this.email,
+          password: this.password,
+        })
 
-    components: {
-        Snackbar,
+        // Autenticação bem-sucedida, você pode redirecionar ou realizar outras ações necessárias.
+      } catch (error) {
+        console.error('Erro de autenticação:', error)
+      }
     },
-};
+  },
+
+  components: {
+    Snackbar,
+  },
+}
 </script>
 
 <style scoped>
 .background-grey {
-    background-color: #e8eaf2;
-    height: 100vh;
-    align-content: center;
+  background-color: #e8eaf2;
+  height: 100vh;
+  align-content: center;
 }
 .card-title {
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: 300;
-    font-size: 48px;
-    line-height: 56px;
-    margin-left: 12px;
-    margin-bottom: 20px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 48px;
+  line-height: 56px;
+  margin-left: 12px;
+  margin-bottom: 20px;
 }
 .divider {
-    margin-bottom: 40px;
-    margin-left: 8px;
-    background: linear-gradient(
-        90deg,
-        #c4c4c4,
-        rgba(196, 196, 196, 0)
-    ) !important;
-    height: 0.5px;
+  margin-bottom: 40px;
+  margin-left: 8px;
+  background: linear-gradient(
+    90deg,
+    #c4c4c4,
+    rgba(196, 196, 196, 0)
+  ) !important;
+  height: 0.5px;
 }
 </style>
