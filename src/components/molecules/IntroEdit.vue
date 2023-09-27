d0690182deaeca69a688ece0440170cc253e3999
Na1au <analaura-osilva@hotmail.com>
2023-09-27 10:38:14 -0300

    feat: add translations on intros

--- src/components/molecules/IntroEdit.vue
+++ src/components/molecules/IntroEdit.vue
@@ -1,63 +1,61 @@
-<template
-  ><div>
+<template>
+  <div>
     <IntroComp
       :colors="['#d128c9', '#9a1aab']"
       :title="'Edit Test'"
       :image="'IntroEdit.svg'"
-      :main="
-        'Create and customize your tasks and heuristics for your research needs.'
-      "
-      :link="'Get started!'"
-      @linkClicked="closeIntro()"
+      :main="$t('descriptions.intro.edit')"
+      :link="$t('descriptions.intro.start')"
       :items="items"
+      @linkClicked="closeIntro()"
       @callFunc="callFunc"
-    ></IntroComp>
+    />
   </div>
 </template>
 
 <script>
-import IntroComp from "@/components/atoms/IntrosComponent";
+import IntroComp from '@/components/atoms/IntrosComponent'
+import i18n from '@/i18n'
 
 export default {
-  data: () => ({}),
   components: {
     IntroComp,
   },
-  methods: {
-    goToDoc() {
-      this.$router.push("/edit/documentation").catch(() => {});
-    },
-    goToDisc() {
-      window.open("https://discord.gg/XHhnXBjgRh");
-    },
-    closeIntro() {
-      this.$emit("closeIntro");
-    },
-    callFunc(func) {
-      this[func](); //call item function
-    },
-  },
+  data: () => ({}),
   computed: {
     items() {
       return [
         {
-          iconColor: "#D128C9",
-          icon: "mdi-file-document",
-          title: "Read documentation",
-          subtitle:
-            "Click to access the documentation on how to use the edit page.",
-          func: "goToDoc",
+          iconColor: '#D128C9',
+          icon: 'mdi-file-document',
+          title: i18n.t('pages.intros.docTitle'),
+          subtitle: i18n.t('pages.intros.docSubtitle') + i18n.t('titles.test'),
+          func: 'goToDoc',
         },
 
         {
-          iconColor: "#D128C9",
-          icon: "mdi-emoticon-happy",
-          title: "Join the comunity!",
-          subtitle: "You will find support on our discord server.",
-          func: "goToDisc",
+          iconColor: '#D128C9',
+          icon: 'mdi-emoticon-happy',
+          title: i18n.t('pages.intros.discTitle'),
+          subtitle: i18n.t('pages.intros.discSubtitle'),
+          func: 'goToDisc',
         },
-      ];
+      ]
     },
   },
-};
+  methods: {
+    goToDoc() {
+      this.$router.push('/edit/documentation').catch(() => {})
+    },
+    goToDisc() {
+      window.open('https://discord.gg/XHhnXBjgRh')
+    },
+    closeIntro() {
+      this.$emit('closeIntro')
+    },
+    callFunc(func) {
+      this[func]() //call item function
+    },
+  },
+}
 </script>
