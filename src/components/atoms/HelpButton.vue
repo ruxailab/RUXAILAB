cf6129920d25e075da3c673e5f663bbb8e3e4d99
jvJUCA <jucafranzoni1@gmail.com>
2023-09-29 17:38:45 -0300

    Feat: Succesfully created a FAQ page

--- src/components/atoms/HelpButton.vue
+++ src/components/atoms/HelpButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <template>
-        <v-btn small icon v-bind="attrs" v-on="on" class="mr-1" @click="$router.push('/help')">
+        <v-btn small icon v-bind="attrs" v-on="on" class="mr-1" @click="$router.push('/help')" >
           <v-icon size="20">mdi-help-circle-outline</v-icon>
         </v-btn>
     </template>
