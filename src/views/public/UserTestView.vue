6a23136d96742e1956a001643e16083fd505360a
jvJUCA <jucafranzoni1@gmail.com>
2023-10-09 17:56:33 -0300

    feat: revived user testing

--- src/views/public/UserTestView.vue
+++ src/views/public/UserTestView.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        <h1>User Test</h1>
-    </div>
+  <v-col cols="12">
+      <span class="text-h3">ID do teste: {{ test.id }}</span>
+      <br><br>
+      <span class="text-h3">Titulo do teste: {{ test.testTitle }}</span>
+      <br><br>
+      <span class="text-h3">Descrição do teste: {{ test.testDescription }}</span>
+      <br><br>
+      <span class="text-h3">ID do admin do teste: {{ test.testAdmin.userDocId}}</span>
+  </v-col>
 </template>
+
+<script>
+export default {
+  computed: {
+    test() {
+      return this.$store.getters.test
+    },
+  },
+}
+</script>
+
+<style></style>
