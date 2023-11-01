06a08b6f08afd5801e4a3767b945509a4b1b4ad0
jvJUCA <jucafranzoni1@gmail.com>
2023-11-01 18:26:35 -0300

    feat(UserAnswer):progress made

--- src/components/organisms/UserAnalytics.vue
+++ src/components/organisms/UserAnalytics.vue
@@ -1,46 +1,57 @@
 <template>
   <div v-if="answers">
-    <IntroAnalytics v-if="answers != null && intro == true" @goToCoops="goToCoops()" />
+    <IntroAnalytics v-if="answers != null && intro" @goToCoops="goToCoops" />
     <ShowInfo v-if="answers != null && !intro" title="Analytics">
-      <div slot="content" class="ma-0 pa-0">
-        <v-card style="background: #f5f7ff">
-          <v-row v-if="taskAnswers" class="ma-0 pa-0">
-            <!--Tasks List-->
-            <v-col class="ma-0 pa-0" cols="2">
-              <v-list dense height="560px" outlined>
+      <div slot="content">
+        <v-card outlined class="task-container">
+          <v-row class="ma-0 pa-0">
+            <!-- Tasks List -->
+            <v-col class="ma-0 pa-0 task-list" cols="3">
+              <v-list dense class="list-scroll">
                 <v-subheader>Tasks</v-subheader>
                 <v-divider />
-                <v-list dense height="470px" outlined class="list-scroll">
-                  <v-list-item-group v-model="taskSelect" color="#fca326">
-                    <v-list-item v-for="(item, i) in testTasks" :key="i">
-                      <v-list-item-content>
-                        <v-list-item-title>{{ item }}</v-list-item-title>
-                      </v-list-item-content>
-                      <v-list-item-icon v-if="i == taskSelect">
-                        <v-icon>mdi-chevron-right</v-icon>
-                      </v-list-item-icon>
-                    </v-list-item>
-                  </v-list-item-group>
-                </v-list>
+                <v-list-item-group v-model="taskSelect" color="#fca326">
+                  <v-list-item v-for="(item, i) in testTasks" :key="i">
+                    <v-list-item-content>
+                      <v-list-item-title>{{ item }}</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
               </v-list>
             </v-col>
             <v-divider vertical inset />
-            <!--Answer List-->
-            <v-col class="ma-0 pa-0" cols="10">
-              <v-list dense height="560px" outlined>
-                <v-subheader>Task {{ testTasks[taskSelect] }} Answers</v-subheader>
-                <v-divider />
-                <div v-for="(item, i) in taskAnswers" :key="i">
-                  <v-list-item v-if="containsTask(item.tasks, testTasks[taskSelect]) !== null">
-                    <v-list-item-content>
-                      <v-list-item-title>
-                        {{ item.userDocId }}:
-                        {{ containsTask(item.tasks, testTasks[taskSelect]).taskAnswer }}
-                      </v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                </div>
-              </v-list>
+
+            <!-- Answer List -->
+            <v-col class="ma-0 pa-1 answer-list" cols="9">
+              <v-data-table
+                :headers="dataHeaders"
+                :items="taskAnswers"
+                item-key="userDocId"
+              >
+                <template>
+                  <v-btn text @click="showDialog = true">Show Answers</v-btn>
+                </template>
+                <!-- <template v-slot:expanded-item="{ item }">
+                  <v-row>
+                    <v-col cols="6">
+                      <v-card-title>
+                        <span class="font-weight-bold">Answers</span>
+                      </v-card-title>
+                      <v-card-text>{{
+                        item.tasks[taskSelect].taskAnswer
+                      }}</v-card-text>
+                    </v-col>
+                    <v-col cols="6">
+                      <v-card-title>
+                        <span class="font-weight-bold">Observations</span>
+                      </v-card-title>
+                      <v-card-text>{{
+                        item.tasks[taskSelect].taskObservations
+                      }}</v-card-text>
+                    </v-col>
+                  </v-row>
+                </template> -->
+              </v-data-table>
             </v-col>
           </v-row>
         </v-card>
@@ -57,95 +68,34 @@ export default {
     ShowInfo,
   },
   data: () => ({
+    showDialog: false,
     search: '',
-    ind: 0,
+    taskSelect: 0, // Define o valor inicial de seleção de tarefas
     testTasks: [],
     taskAnswers: [],
-    // testTasks: [
-    //   'task1',
-    //   'task2',
-    //   'task3',
-    //   'task4',
-    // ],
-    // taskAnswers: [
-    //   {
-    //     preTestUrl: 'https://example.com/pretest_A_1',
-    //     consentUrl: 'https://example.com/consent_A_1',
-    //     postTestUrl: 'https://example.com/posttest_A_1',
-    //     tasks: [{
-    //       taskAnswer: 'pretty cool',
-    //       taskObservations: 'Observations for Task A1',
-    //       taskTime: 'Task A1 Time',
-    //       audioRecordURL: 'https://example.com/audio_A_1',
-    //       screenRecordURL: 'https://example.com/screen_A_1',
-    //       webcamRecordURL: 'https://example.com/webcam_A_1',
-    //       taskId: 'task1',
-    //     }, {
-    //       taskAnswer: 'cool task',
-    //       taskObservations: 'Observations for Task A2',
-    //       taskTime: 'Task A2 Time',
-    //       audioRecordURL: 'https://example.com/audio_A_2',
-    //       screenRecordURL: 'https://example.com/screen_A_2',
-    //       webcamRecordURL: 'https://example.com/webcam_A_2',
-    //       taskId: 'task2',
-    //     }],
-    //     progress: 100,
-    //     total: 10,
-    //     submitted: false,
-    //     userDocId: 'Carlos',
-    //     lastUpdate: new Date(), // Current timestamp
-    //   },
-    //   {
-    //     preTestUrl: 'https://example.com/pretest_B_1',
-    //     consentUrl: 'https://example.com/consent_B_1',
-    //     postTestUrl: 'https://example.com/posttest_B_1',
-    //     tasks: [{
-    //       taskAnswer: 'awful task',
-    //       taskObservations: 'Observations for Task B1',
-    //       taskTime: 'Task B1 Time',
-    //       audioRecordURL: 'https://example.com/audio_B_1',
-    //       screenRecordURL: 'https://example.com/screen_B_1',
-    //       webcamRecordURL: 'https://example.com/webcam_B_1',
-    //       taskId: 'task4',
-    //     }],
-    //     progress: 30,
-    //     total: 5,
-    //     submitted: true,
-    //     userDocId: 'Maria',
-    //     lastUpdate: new Date(), // Current timestamp
-    //   },
-    //   {
-    //     preTestUrl: 'https://example.com/pretest_C_1',
-    //     consentUrl: 'https://example.com/consent_C_1',
-    //     postTestUrl: 'https://example.com/posttest_C_1',
-    //     tasks: [{
-    //       taskAnswer: 'mid',
-    //       taskObservations: 'Observations for Task C1',
-    //       taskTime: 'Task C1 Time',
-    //       audioRecordURL: 'https://example.com/audio_C_1',
-    //       screenRecordURL: 'https://example.com/screen_C_1',
-    //       webcamRecordURL: 'https://example.com/webcam_C_1',
-    //       taskId: 'task3',
-    //     }, {
-    //       taskAnswer: 'kinda fun',
-    //       taskObservations: 'Observations for Task C2',
-    //       taskTime: 'Task C2 Time',
-    //       audioRecordURL: 'https://example.com/audio_C_2',
-    //       screenRecordURL: 'https://example.com/screen_C_2',
-    //       webcamRecordURL: 'https://example.com/webcam_C_2',
-    //       taskId: 'task2',
-    //     }],
-    //     progress: 70,
-    //     total: 12,
-    //     submitted: false,
-    //     userDocId: 'Joao',
-    //     lastUpdate: new Date(), // Current timestamp
-    //   }],
-    taskSelect: null,
-    answerSelect: null,
     intro: null,
+    dataHeaders: [
+      {
+        text: 'Email',
+        value: 'userDocId',
+      },
+      {
+        text: 'Actions',
+        sortable: false,
+        value: 'actions',
+      },
+    ],
   }),
   computed: {
+    test() {
+      return this.$store.getters.test
+    },
+    testStructure() {
+      return this.$store.state.Tests.Test.testStructure
+    },
+    tasksAnswer() {
+      return this.$store.getters.testAnswerDocument
+    },
     answers() {
       if (!this.$store.getters.testAnswerDocument) {
         return []
@@ -157,19 +107,33 @@ export default {
       return !Object.values(this.answers).length
     },
   },
-  // created() {
-  //   console.log(this.$store.getters.testAnswerDocument)
-  // },
+  created() {
+    let i = 0
+    this.testStructure.userTasks.forEach((task) => {
+      this.testTasks[i] = task.taskName
+      i++
+    })
+    let c = 0
+    for (const key in this.answers) {
+      this.taskAnswers[c] = this.answers[key]
+      c++
+    }
+    console.log(this.taskAnswers)
+  },
   methods: {
-    containsTask(tasks, selectedTask) {
-      const index = tasks.findIndex((task) => task.taskId === selectedTask)
-      if (index !== -1) {
-        return tasks[index]
-      }
-      return null
-    },
     goToCoops() {
       this.$emit('goToCoops')
+    },
+    getCooperatorEmail(userDocId) {
+      let cooperatorEmail = null
+      if (this.test.cooperators && Array.isArray(this.test.cooperators)) {
+        for (const element of this.test.cooperators) {
+          if (element && element.email && element.userDocId === userDocId) {
+            cooperatorEmail = element.email
+          }
+        }
+      }
+      return cooperatorEmail
     },
   },
 }
