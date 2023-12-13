d9906f6ad183b7b98e405d7e953cd123a1453cd1
jvJUCA <jucafranzoni1@gmail.com>
2023-12-13 17:36:08 -0300

    feat(userTest): componentized the camera record feature

---
+++ src/components/atoms/VideoRecorder.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <video
+      class="web-cam mb-2 ml-3"
+      ref="video"
+      height="100"
+      autoplay
+      v-if="recording"
+    ></video>
+    <v-btn
+      v-if="!recording && recordedVideo == ''"
+      @click="startRecording"
+      class="ml-4 mb-2 xl"
+      color="grey lighten-2"
+      elevation="0"
+    >
+      <v-icon class="mr-2">mdi-camera</v-icon>Start Recording
+    </v-btn>
+    <v-btn color="red" icon v-if="recording" @click="stopRecording">
+      <v-icon dark>mdi-stop</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+export default {
+  props: {
+    testId: String,
+    currentUserTestAnswer: Object,
+    taskIndex: Number,
+  },
+  data() {
+    return {
+      recording: false,
+      videoStream: null,
+      recordedChunks: [],
+      mediaRecorder: null,
+      recordedVideo: '',
+    }
+  },
+  methods: {
+    async startRecording() {
+      this.recording = true
+      this.videoStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      })
+      this.$refs.video.srcObject = this.videoStream
+
+      this.recordedChunks = []
+      this.mediaRecorder = new MediaRecorder(this.videoStream)
+
+      this.mediaRecorder.ondataavailable = (event) => {
+        if (event.data.size > 0) {
+          this.recordedChunks.push(event.data)
+        }
+      }
+
+      this.mediaRecorder.onstop = async () => {
+        const videoBlob = new Blob(this.recordedChunks, {
+          type: 'video/webm',
+        })
+        const storage = getStorage()
+        const storageRef = ref(
+          storage,
+          `tests/${this.testId}/${this.currentUserTestAnswer.userDocId}/task_${this.taskIndex}/video/${this.recordedVideo}`,
+        )
+        await uploadBytes(storageRef, videoBlob)
+
+        this.recordedVideo = await getDownloadURL(storageRef)
+
+        this.currentUserTestAnswer.tasks[
+          this.taskIndex
+        ].webcamRecordURL = this.recordedVideo
+
+        console.log(
+          this.currentUserTestAnswer.tasks[this.taskIndex].webcamRecordURL,
+        )
+      }
+
+      this.mediaRecorder.start()
+    },
+    stopRecording() {
+      if (this.mediaRecorder) {
+        this.mediaRecorder.stop()
+        this.videoStream.getTracks().forEach((track) => track.stop())
+        this.recording = false
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.web-cam {
+  position: relative;
+  text-align: center;
+  height: 125px;
+  width: 125px;
+  border-radius: 50%;
+  overflow: hidden;
+  mask-image: radial-gradient(circle, white 100%, black 100%);
+}
+video {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
