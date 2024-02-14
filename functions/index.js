e862c6a38c262b70411559458bf82ff7d557eb12
FernandaCasali <fernanda.t.casali@gmail.com>
2024-02-14 10:44:51 -0300

    function being called successfully
    Signed-off-by: FernandaCasali <fernanda.t.casali@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -1,47 +1,59 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const nodemailer = require("nodemailer");
-require("dotenv").config();
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const nodemailer = require('nodemailer')
+require('dotenv').config()
 
-admin.initializeApp();
+admin.initializeApp()
 
-exports.onTestCreate = functions.firestore.document('tests/{docId}').onCreate(async (snap, context) => {
-  const userId = snap.data().testAdmin.userDocId
-  const test = snap.data()
+exports.onTestCreate = functions.firestore
+  .document('tests/{docId}')
+  .onCreate(async (snap, context) => {
+    const userId = snap.data().testAdmin.userDocId
+    const test = snap.data()
 
-  return await admin.firestore().collection('users').doc(userId).update({
-    [`myTests.${snap.id}`]: {
-      testDocId: snap.id,
-      testTitle: test.testTitle,
-      testType: test.testType,
-      numberColaborators: 0,
-      creationDate: test.creationDate ?? null,
-      updateDate: Date.now(),
-    }
+    return await admin
+      .firestore()
+      .collection('users')
+      .doc(userId)
+      .update({
+        [`myTests.${snap.id}`]: {
+          testDocId: snap.id,
+          testTitle: test.testTitle,
+          testType: test.testType,
+          numberColaborators: 0,
+          creationDate: test.creationDate ?? null,
+          updateDate: Date.now(),
+        },
+      })
   })
-})
 
-exports.onTestUpdate = functions.firestore.document('tests/{docId}').onUpdate(async (snap, context) => {
-  const userId = snap.after.data().testAdmin.userDocId
-  const test = snap.after.data()
+exports.onTestUpdate = functions.firestore
+  .document('tests/{docId}')
+  .onUpdate(async (snap, context) => {
+    const userId = snap.after.data().testAdmin.userDocId
+    const test = snap.after.data()
 
-  return await admin.firestore().collection('users').doc(userId).update({
-    [`myTests.${snap.after.id}`]: {
-      testDocId: snap.after.id,
-      testTitle: test.testTitle,
-      testType: test.testType,
-      numberColaborators: test.numberColaborators ?? 0,
-      creationDate: test.creationDate,
-      updateDate: Date.now(),
-    }
+    return await admin
+      .firestore()
+      .collection('users')
+      .doc(userId)
+      .update({
+        [`myTests.${snap.after.id}`]: {
+          testDocId: snap.after.id,
+          testTitle: test.testTitle,
+          testType: test.testType,
+          numberColaborators: test.numberColaborators ?? 0,
+          creationDate: test.creationDate,
+          updateDate: Date.now(),
+        },
+      })
   })
-})
 
 exports.processSignUp = functions.auth.user().onCreate(async (user) => {
   try {
     await admin
       .firestore()
-      .collection("users")
+      .collection('users')
       .doc(user.uid)
       .set({
         email: user.email,
@@ -49,59 +61,57 @@ exports.processSignUp = functions.auth.user().onCreate(async (user) => {
         myTests: {},
         myAnswers: {},
         notifications: [],
-      });
+      })
   } catch (err) {
-    console.error("Error to create user in database ", err);
+    console.error('Error to create user in database ', err)
   }
-});
+})
 
 exports.setUserRole = functions.https.onCall(async (data) => {
   try {
     return await admin
       .firestore()
-      .collection("users")
+      .collection('users')
       .doc(data.uid)
       .update({
         accessLevel: data.customClaims.accessLevel,
-      });
+      })
   } catch (err) {
-    return err;
+    return err
   }
-});
+})
 
 exports.deleteAuth = functions.https.onCall(async (data, context) => {
   try {
-    return await admin
-      .auth()
-      .deleteUser(data.id)
+    return await admin.auth().deleteUser(data.id)
   } catch (err) {
-    return err;
+    return err
   }
-});
+})
 
 exports.sendEmail = functions.https.onCall(async (data, context) => {
   let transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: 'gmail',
     auth: {
       user: process.env.VUE_APP_FIREBASE_EMAIL,
       pass: process.env.VUE_APP_FIREBASE_PASSWORD,
     },
-  });
+  })
 
   let mail = {
-    from: "Uramaki Lab",
+    from: 'Uramaki Lab',
     to: data.guest.email,
-    subject: "You have been invited to evaluate a test!",
+    subject: 'You have been invited to evaluate a test!',
     html: data.template,
-  };
+  }
 
   return await transporter
     .sendMail(mail)
     .then((info) => {
-      return `Message sent: ${info.messageId} `;
+      return `Message sent: ${info.messageId} `
     })
     .catch((e) => {
-      console.log("Error in transporter ", e);
-      return `Error on sending msg: ${e}`;
-    });
-});
+      console.log('Error in transporter ', e)
+      return `Error on sending msg: ${e}`
+    })
+})
--- weight_function/main.py
+++ weight_function/main.py
@@ -1,11 +1,46 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
-
-from firebase_functions import https_fn
-from firebase_admin import initialize_app
+import json
+from typing import Any
+from firebase_functions import https_fn, options
+from firebase_admin import db, initialize_app
+import google.cloud.firestore
+import functions_framework
 
 initialize_app()
+
+@functions_framework.http
+def cors_enabled(request):
+    # Set CORS headers for the preflight request
+    if request.method == "OPTIONS":
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Max-Age": "3600",
+        }
+        return ("", 204, headers)
+    # Set CORS headers for the main request
+    headers = {"Access-Control-Allow-Origin": "*"}
+    return ("Hello World!", 200, headers)
+
+
 @https_fn.on_request()
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+def HWexample(req:  https_fn.Request) -> https_fn.Response:
+ return https_fn.Response("Hello world!")
+
+@https_fn.on_request(
+    cors=options.CorsOptions(
+        cors_origins=[r"firebase\.com$",  r"http://localhost:8080"],
+        cors_methods=["get", "post"],
+    )
+)
+def say_hello(req: https_fn.Request) -> https_fn.Response:
+    response_data = {
+        "message": "Hello world!"
+    }
+    return https_fn.Response(json.dumps(response_data), content_type="application/json")
+
+@https_fn.on_call()
+def addmessage(req: https_fn.CallableRequest) -> Any:
+    return("Deu certo!")
