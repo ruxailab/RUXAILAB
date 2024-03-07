1e6a375b19d1f0147fb9d92f74e9360109575fe9
Gaurav-Verma07 <cosmosian07@gmail.com>
2024-03-08 01:24:07 +0530

    feat: a global error handling model

---
+++ src/components/atoms/GlobalErrorHandler.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <Toast
+      position="top-right"
+      auto-close="{3000}"
+      :render="error()"
+    />
+  </div>
+</template>
+
+<script>
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+import Vue from 'vue'
+
+const options = {
+  timeout: 3000,
+}
+Vue.use(Toast, options)
+export default {
+  components: { Toast },
+  computed: {
+    isError() {
+      return (
+        this.$store.state.error.message || this.$store.state.error.errorCode
+      )
+    },
+  },
+  methods: {
+    error() {
+      const mainError = this.$store.state.error
+    if (mainError && (mainError.message || mainError.errorCode)) {
+      if (this.previousErrorMessage !== mainError.message || this.previousErrorCode !== mainError.errorCode) {
+        this.$toast.error(`${mainError.errorCode}: ${mainError.message}`)
+        this.previousErrorMessage = ''
+        this.previousErrorCode = ''
+      }
+      this.$store.state.error.errorCode= ''
+      this.$store.state.error.message= ''
+    }
+   },
+
+  },
+}
+</script>
