505f4c65a55c7486f052be68c9654e7165ab6e69
jvJUCA <jucafranzoni1@gmail.com>
2024-03-13 17:30:16 -0300

    feat(ScreenRec): loading on save, quick recording function, and toast success on save

--- src/components/atoms/ScreenRecorder.vue
+++ src/components/atoms/ScreenRecorder.vue
@@ -1,41 +1,47 @@
 <template>
   <div>
     <v-col>
-    <v-row>
-      <v-btn
-        @click="captureScreen"
-        class="ml-4 mb-2 xl"
-        v-if="!isCapture"
-        color="grey lighten-2"
-        elevation="0"
-      >
-        <v-icon left dark>mdi-monitor-screenshot</v-icon>
-        Capture
-      </v-btn>
-      <v-btn
-        class="ml-4 mb-2 xl"
-        v-if="isCapture && videoUrl == ''"
-        :color="!isRecording ? 'grey lighten-2' : 'red lighten-1'"
-        :dark="isRecording"
-        prepend-icon="mdi-monitor-screenshot"
-        elevation="0"
-        @click="recordScreen"
-      >
-        <v-icon left dark v-if="!isRecording">mdi-monitor-screenshot</v-icon>
-        <v-icon left dark v-else>mdi-stop</v-icon>
-        {{ isRecording ? 'Stop recording' : 'Start recording' }}
-      </v-btn>
-    </v-row>
+      <v-row>
+        <v-btn
+          @click="captureScreen"
+          class="ml-4 mb-2 xl"
+          v-if="
+            !isCapture &&
+              currentUserTestAnswer.tasks[taskIndex].screenRecordURL == ''
+          "
+          color="grey lighten-2"
+          elevation="0"
+        >
+          <v-icon left dark>mdi-monitor-screenshot</v-icon>
+          Capture
+        </v-btn>
+        <v-btn
+          class="ml-4 mb-2 xl"
+          v-if="
+            isCapture &&
+              currentUserTestAnswer.tasks[taskIndex].screenRecordURL == ''
+          "
+          :color="!isRecording ? 'grey lighten-2' : 'red lighten-1'"
+          :dark="isRecording"
+          prepend-icon="mdi-monitor-screenshot"
+          elevation="0"
+          @click="recordScreen"
+        >
+          <v-icon left dark v-if="!isRecording">mdi-monitor-screenshot</v-icon>
+          <v-icon left dark v-else>mdi-stop</v-icon>
+          {{ isRecording ? 'Stop recording' : 'Start recording' }}
+        </v-btn>
+      </v-row>
     </v-col>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 export default {
   props: {
     testId: String,
-    currentUserTestAnswer: Object,
     taskIndex: Number,
   },
   data() {
@@ -43,55 +49,64 @@ export default {
       isCapture: false,
       isRecording: false,
       videoUrl: '',
+      videoStream: null,
       mediaRecorder: null,
       chunks: [],
-    };
+    }
+  },
+  computed: {
+    currentUserTestAnswer() {
+      return this.$store.getters.currentUserTestAnswer
+    },
   },
   methods: {
     async captureScreen() {
-      const videoElem = document.getElementById('vpreview');
       try {
-        videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(
-          this.displayMediaOptions
-        );
-        this.isCapture = true;
+        this.videoStream = await navigator.mediaDevices.getDisplayMedia({
+          cursor: true,
+        })
+        this.isCapture = true
+        this.recordScreen()
       } catch (err) {
-        console.error(err);
+        console.error(err)
       }
     },
     recordScreen() {
       if (!this.isRecording) {
-        const videoElem = document.getElementById('vpreview');
-        this.mediaRecorder = new MediaRecorder(videoElem.srcObject);
-        this.mediaRecorder.start();
+        this.mediaRecorder = new MediaRecorder(this.videoStream)
+        this.mediaRecorder.start()
         this.mediaRecorder.ondataavailable = (e) => {
-          this.chunks.push(e.data);
-        };
+          this.chunks.push(e.data)
+        }
 
         this.mediaRecorder.onstop = async () => {
-          const videoBlob = new Blob(this.chunks, { type: 'video/webm' });
-          const storage = getStorage();
+          this.$emit('showLoading')
+          const videoBlob = new Blob(this.chunks, { type: 'video/webm' })
+          const storage = getStorage()
           const storageRef = ref(
             storage,
-            `tests/${this.testId}/${this.currentUserTestAnswer.userDocId}/task_${this.taskIndex}/screen_record/${this.videoUrl}`
-          );
-          await uploadBytes(storageRef, videoBlob);
+            `tests/${this.testId}/${this.currentUserTestAnswer.userDocId}/task_${this.taskIndex}/screen_record/${this.videoUrl}`,
+          )
+          await uploadBytes(storageRef, videoBlob)
 
-          this.videoUrl = await getDownloadURL(storageRef);
+          this.videoUrl = await getDownloadURL(storageRef)
 
           this.currentUserTestAnswer.tasks[
             this.taskIndex
-          ].screenRecordURL = this.videoUrl;
-        };
-        this.isRecording = true;
+          ].screenRecordURL = this.videoUrl
+          this.videoStream = null
+          this.$emit('stopShowLoading')
+          Vue.$toast.success('Screen record saved!')
+        }
+        this.isRecording = true
       } else {
-        this.mediaRecorder.stop();
-        this.isRecording = false;
+        this.mediaRecorder.stop()
+        this.videoStream.getTracks().forEach((track) => track.stop())
+        this.isRecording = false
       }
     },
   },
-};
+}
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
