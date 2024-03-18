4afed3b6b5dc37c93e790ea1563f5688bd502d7e
FernandaCasali <fernanda.t.casali@gmail.com>
2024-03-18 17:26:09 -0300

    feat: import apexchart
    Signed-off-by: FernandaCasali <fernanda.t.casali@gmail.com>

--- src/components/organisms/HeuristicsTestAnswer.vue
+++ src/components/organisms/HeuristicsTestAnswer.vue
@@ -323,7 +323,18 @@
                       </v-col>
                       <v-col cols="12" sm="6" md="8" class="my-6 py-6">
                         <v-card class="pa-2 weightsStatisticsRadar elevation-4">
-                          2
+                          <div class="example">
+                            <ColumnExample
+                              width="500"
+                              height="350"
+                              type="bar"
+                              :options="chartOptions"
+                              :series="series"
+                            />
+                            <button @click="updateChart">
+                              Update!
+                            </button>
+                          </div>
                         </v-card>
                       </v-col>
                     </v-row>
@@ -357,6 +368,7 @@ import RadarChart from '@/components/atoms/RadarChart.vue'
 import ShowInfo from '@/components/organisms/ShowInfo'
 import IntroAnswer from '@/components/molecules/IntroAnswer'
 import AnalyticsView from '@/views/admin/AnalyticsView.vue'
+import ColumnExample from '@/components/atoms/ColumnBarChart.vue'
 
 import { standardDeviation, finalResult, statistics } from '@/utils/statistics'
 
@@ -367,6 +379,7 @@ export default {
     ShowInfo,
     IntroAnswer,
     AnalyticsView,
+    ColumnExample,
   },
   props: { id: { type: String, default: '' } },
   data: () => ({
