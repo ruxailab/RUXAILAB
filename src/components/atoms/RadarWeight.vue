ca073a3b777d0def50e14b58b3c2979358e10290
FernandaCasali <fernanda.t.casali@gmail.com>
2024-03-22 16:40:14 -0300

    Feat(Answer): radar charts working
    Signed-off-by: FernandaCasali <fernanda.t.casali@gmail.com>

---
+++ src/components/atoms/RadarWeight.vue
@@ -0,0 +1,71 @@
+<script>
+import { Radar } from 'vue-chartjs/legacy'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+} from 'chart.js'
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  RadialLinearScale,
+  LineElement,
+)
+
+export default {
+  extends: Radar,
+  props: {
+    labels: {
+      type: Array,
+      required: true,
+      default: function() {
+        return []
+      },
+    },
+    data: {
+      type: Array,
+      required: true,
+      default: function() {
+        return []
+      },
+    },
+  },
+  watch: {
+    data() {
+      this.$data._chart.update()
+    },
+  },
+  mounted() {
+    this.renderChart(
+      {
+        labels: this.labels,
+        datasets: [
+          {
+            label: 'Importance',
+            backgroundColor: 'rgba(249, 152, 38,0.24)',
+            borderColor: 'rgba(255, 81, 47,1)',
+            pointBackgroundColor: 'rgba(255, 81, 47,1)',
+            data: this.data,
+          },
+        ],
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+        scale: {
+          ticks: {
+            suggestedMin: 0,
+            suggestedMax: 100,
+          },
+        },
+      },
+    )
+  },
+}
+</script>
