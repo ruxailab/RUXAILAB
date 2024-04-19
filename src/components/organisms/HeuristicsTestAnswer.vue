15e7823116e11bb8627dd948c6909ab93387653d
jvJUCA <jucafranzoni1@gmail.com>
2024-04-19 16:57:48 -0300

    fix: Evaluators order random

--- src/components/organisms/HeuristicsTestAnswer.vue
+++ src/components/organisms/HeuristicsTestAnswer.vue
@@ -180,7 +180,13 @@
                   "
                   :data="evaluatorStatistics.items.map((item) => item.result)"
                 />
-                <v-card v-else flat class="mx-auto mt-10 mb-10 py-6 px-3 if-card" align="center" width="970px">
+                <v-card
+                  v-else
+                  flat
+                  class="mx-auto mt-10 mb-10 py-6 px-3 if-card"
+                  align="center"
+                  width="970px"
+                >
                   The graphic can only be generated with 3 or more evaluators,
                   please colect more data from your research to procede.
                 </v-card>
@@ -189,7 +195,13 @@
           </v-card>
 
           <!-- Tab 3 - Heuristics-->
-          <v-card v-if="tab == 2" rounded="xl" flat class="mb-6 py-2" style="background: #f5f7ff">
+          <v-card
+            v-if="tab == 2"
+            rounded="xl"
+            flat
+            class="mb-6 py-2"
+            style="background: #f5f7ff"
+          >
             <v-card
               v-if="evaluatorStatistics.items.length <= 1"
               class="mx-auto mt-10 mb-10 py-6 if-card"
@@ -294,7 +306,9 @@
                           <div style="padding-top: 2px; padding-bottom: 2px">
                             <v-chip
                               style="width: 35%"
-                              :color="getColor(item.average, item.max, item.min)"
+                              :color="
+                                getColor(item.average, item.max, item.min)
+                              "
                               dark
                             >
                               {{ item.percentage }}
@@ -326,25 +340,29 @@
                         align="center"
                         width="970px"
                       >
-                        This page needs weight function (python) to be running, can be in
-                        emulators or in deploy mode, and the weights to be full marked on your creating test page.
+                        This page needs weight function (python) to be running,
+                        can be in emulators or in deploy mode, and the weights
+                        to be full marked on your creating test page.
                       </v-card>
                       <div v-else>
                         <v-row align="center" justify="space-around">
-                          <v-col cols="6" md="4">
+                          <v-col md="4" sm="8">
                             <v-card
                               align="center"
-                              class=" elevation-4 weightsStatisticsStyle mt-6 py-4 mb-6 mx-auto"
+                              class="elevation-4 weightsStatisticsStyle mt-6 py-4 mb-6 mx-auto"
                               width="950px"
                             >
                               <v-card-title class="mt-4 mb-4 font-weight-bold">
                                 <v-row align="center" justify="center">
-                                  Usability Percentage <br>
+                                  Usability Percentage <br />
                                   With Weights
                                 </v-row>
                               </v-card-title>
                               <v-card-text>
-                                <v-row align="center" justify="center mt-2 mb-2">
+                                <v-row
+                                  align="center"
+                                  justify="center mt-2 mb-2"
+                                >
                                   <p class="display-3">
                                     {{ usabilityTotalFix }}
                                   </p>
@@ -366,9 +384,7 @@
                                   )
                                 "
                                 :data="
-                                  weightsStatistics.items.map(
-                                    (item) => item.rw,
-                                  )
+                                  weightsStatistics.items.map((item) => item.rw)
                                 "
                                 :maxValue="maxValue"
                               />
@@ -461,7 +477,9 @@ export default {
         value: 'heuristic',
       })
       if (this.resultEvaluator) {
+        let evaluatorIndex = 1
         this.resultEvaluator.forEach((evaluator) => {
+          evaluator.id = `Ev${evaluatorIndex}`
           const header = table.header.find((h) => h.text == evaluator.id)
           if (!header) {
             table.header.push({
@@ -485,6 +503,7 @@ export default {
               })
             }
           })
+          evaluatorIndex++
         })
       }
       return table
@@ -603,7 +622,6 @@ export default {
 
     usabilityTotalFix() {
       const usabilityTotalFix = parseFloat(this.usability_total).toFixed(2)
-      console.log(this.maxValue)
       return usabilityTotalFix
     },
 
@@ -612,11 +630,10 @@ export default {
       let maxValue = relative[0]
       for (let i = 1; i < relative.length; i++) {
         if (relative[i] > maxValue) {
-            maxValue = relative[i]
+          maxValue = relative[i]
         }
       }
       const maxplus = parseFloat(maxValue).toFixed(1)
-      console.log(maxplus)
       return maxplus
     },
 
@@ -733,7 +750,6 @@ export default {
         console.error('Erro ao chamar Cloud Function:', erro)
       }
     },
-
   },
 }
 </script>
