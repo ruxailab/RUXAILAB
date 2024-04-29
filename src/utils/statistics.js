a5347875bb4c3b720bb1da4cb4779795d310f17c
jvJUCA <jucafranzoni1@gmail.com>
2024-04-29 16:06:32 -0300

    fix: not showing statistics if not answered

--- src/utils/statistics.js
+++ src/utils/statistics.js
@@ -100,7 +100,11 @@ function statistics() {
             if (question.heuristicAnswer.value === null) {
               noAplication++
             }
-            if (question.heuristicAnswer.value === '') noReply++
+            if (
+              question.heuristicAnswer.value === '' ||
+              Object.values(question.heuristicAnswer).length < 3
+            )
+              noReply++
             return totalQuestions + Number(question.heuristicAnswer.value)
           },
           0,
@@ -135,21 +139,29 @@ function finalResult() {
   const evaluatorStatistics = store.state.Answer.evaluatorStatistics
   if (evaluatorStatistics.items.length) {
     const res = evaluatorStatistics.items.reduce((total, value) => {
-      return !isNaN(parseInt(value.result)) ? total + value.result / evaluatorStatistics.items.length : 0
+      return !isNaN(parseInt(value.result))
+        ? total + value.result / evaluatorStatistics.items.length
+        : 0
     }, 0)
 
     testData.average = `${Math.fround(res).toFixed(2)}%`
 
     testData.max = `${Math.max(
-      ...evaluatorStatistics.items.map((item) => !isNaN(parseInt(item.result)) ? item.result : 0),
+      ...evaluatorStatistics.items.map((item) =>
+        !isNaN(parseInt(item.result)) ? item.result : 0,
+      ),
     ).toFixed(2)}%`
 
     testData.min = `${Math.min(
-      ...evaluatorStatistics.items.map((item) => !isNaN(parseInt(item.result)) ? item.result : 0),
+      ...evaluatorStatistics.items.map((item) =>
+        !isNaN(parseInt(item.result)) ? item.result : 0,
+      ),
     ).toFixed(2)}%`
 
     testData.sd = `${standardDeviation(
-      evaluatorStatistics.items.map((item) => !isNaN(parseInt(item.result)) ? item.result : 0),
+      evaluatorStatistics.items.map((item) =>
+        !isNaN(parseInt(item.result)) ? item.result : 0,
+      ),
     ).toFixed(2)}%`
   }
   return testData
