e660fa293f2ddeec1a90c72aa83ad3e0ab578e91
Sergio Beltrán <sbeltran@eciyclic.com>
2024-05-04 16:10:58 +0200

    e2e auth tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 require('dotenv').config()
 
 module.exports = defineConfig({
+  projectId: 'xmf2jf',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -10,5 +11,6 @@ module.exports = defineConfig({
   env: {
     // add environment variables here
     ...process.env,
+    url: 'http://localhost:8080',
   },
 })
