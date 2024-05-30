40cbc68e9c19331cd32d66f68a8db3b9548182e8
zlatanjakic <zlatan.jakic@klika.ba>
2024-05-30 15:53:13 +0200

    Romved the repo owener variable

--- ishikawa_tools/ishikawa_tools_script.py
+++ ishikawa_tools/ishikawa_tools_script.py
@@ -160,7 +160,6 @@ def generate_scatter_diagram_report(repo_owner, repo_name, github_token):
     plt.savefig('./ishikawa_tools/output/scatter.pdf')
 
 def main():
-    repo_owner = "marcgc21"
     github_token = os.getenv('TOKEN')
     username = os.getenv('USER')
     repository_name = os.getenv('PROJECT')
@@ -169,7 +168,7 @@ def main():
         print("One or more environment variables are missing.")
         return
 
-    issues = fetch_issues(repo_owner, repository_name, github_token)
+    issues = fetch_issues(username, repository_name, github_token)
     if issues is None:
         print("Exiting script due to fetch issues failure.")
         return
