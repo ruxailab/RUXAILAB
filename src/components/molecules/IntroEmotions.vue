af4c300dff75c84eeffe7c070ed386d5b376c3b2
SebastianJitaru <sjitaru29@gmail.com>
2024-06-11 12:57:13 +0200

    add: added svg introemotions

---
+++ src/components/molecules/IntroEmotions.vue
@@ -0,0 +1,63 @@
+<template>
+    <IntroComp
+      :colors="['#9ac94f', '#7eb543']"
+      :title="'Emotions'"
+      :image="'IntroAnswer.svg'"
+      :main="
+        $t('descriptions.intro.emotions')
+      "
+      :link="$t('descriptions.intro.invite')"
+      :items="items"
+      @linkClicked="goToCoops()"
+      @callFunc="callFunc"
+    />
+  </template>
+  
+  <script>
+  import IntroComp from '@/components/atoms/IntrosComponent'
+  import i18n from '@/i18n'
+  
+  export default {
+    components: {
+      IntroComp,
+    },
+    data: () => ({}),
+    computed: {
+      items() {
+        return [
+          {
+            iconColor: '#4bbdaf',
+            icon: 'mdi-file-document',
+            title: i18n.t('pages.intros.docTitle'),
+            subtitle:
+            i18n.t('pages.intros.docSubtitle')  + i18n.t('titles.answers'),
+            func: 'goToDoc',
+          },
+  
+          {
+            iconColor: '#4bbdaf',
+            icon: 'mdi-emoticon-happy',
+            title: i18n.t('pages.intros.discTitle'),
+            subtitle: i18n.t('pages.intros.discSubtitle'),
+            func: 'goToDisc',
+          },
+        ]
+      },
+    },
+    methods: {
+      goToCoops() {
+        this.$emit('goToCoops')
+      },
+      goToDoc() {
+        this.$router.push('/emotions/documentation').catch(() => {})
+      },
+      goToDisc() {
+        window.open('https://discord.gg/MFWNpwTq9q')
+      },
+      callFunc(func) {
+        this[func]()
+      },
+    },
+  }
+  </script>
+  
