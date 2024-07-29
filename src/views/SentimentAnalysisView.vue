b50741840832310a6e3f97abdd55cfb8dbcf752d
BasmaElhoseny <basmaelhoseny6@gmail.com>
2024-07-29 22:35:36 +0300

    feat: Added view for sentiment analysis tab in user test

---
+++ src/views/SentimentAnalysisView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- checking whether to show heuristics answer sheet or user answer sheet-->
+    <!-- <div v-if="testAnswerDocument.type === 'HEURISTICS'">
+      <HeuristicsTestAnswer />
+    </div>
+    <div v-else>
+      <UserTestAnswer />
+    </div> -->
+
+    <!-- Audio Wave -->
+    <AudioWave />
+
+    SentimentAnalysisView :D
+  </div>
+</template>
+
+<script>
+// Components
+import AudioWave from '@/components/molecules/AudioWave.vue'
+// import HeuristicsTestAnswer from '@/components/organisms/HeuristicsTestAnswer.vue'
+// import UserTestAnswer from '@/components/organisms/UserTestAnswer.vue'
+export default {
+  components: {
+    AudioWave,
+  },
+}
+</script>
+
+<style></style>
