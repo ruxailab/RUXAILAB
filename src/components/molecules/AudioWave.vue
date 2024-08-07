adab09f71ade063d5277d9d39f6d01b1b5b64582
BasmaElhoseny <basmaelhoseny6@gmail.com>
2024-08-07 23:34:56 +0300

    feat: Added Region slider + buttin

--- src/components/molecules/AudioWave.vue
+++ src/components/molecules/AudioWave.vue
@@ -10,6 +10,12 @@
         {{ playing ? 'mdi-pause' : 'mdi-play' }}
       </v-icon>
     </v-btn>
+
+    <!--  -->
+    {{ regionStart }} - {{ regionEnd }}
+    <v-btn color="#F9A826" class="white--text" @click="analyzeTimeStamp()">
+      Analyze
+    </v-btn>
   </div>
 </template>
 
@@ -17,6 +23,7 @@
 import { getStorage, ref, getDownloadURL } from 'firebase/storage'
 
 import WaveSurfer from 'wavesurfer.js'
+import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.esm.js'
 
 // Components
 
@@ -33,6 +40,9 @@ export default {
     return {
       wave_surfer: null,
       playing: false,
+      regions: null,
+      regionStart: 0,
+      regionEnd: 8,
     }
   },
 
@@ -43,18 +53,46 @@ export default {
     },
   },
   mounted() {
+    // Regions
+    this.regions = RegionsPlugin.create()
+
     // Initialize WaveSurfer
     this.initWaveSurfer()
 
     // Update play/pause state when WaveSurfer's playback state changes
     this.wave_surfer.on('play', () => (this.playing = true))
     this.wave_surfer.on('pause', () => (this.playing = false))
+
+    this.wave_surfer.on('decode', () => {
+      if (!this.wave_surfer) return
+      if (!this.regions) return
+      // Regions
+      const init_region = this.regions.addRegion({
+        start: 0,
+        end: 8,
+        content: '',
+        color: 'rgba(252, 162, 42, 0.24)',
+        drag: true,
+        resize: true,
+      })
+
+      init_region.on('update-end', () => {
+        this.regionStart = init_region.start
+        this.regionEnd = init_region.end
+      })
+    })
   },
   methods: {
     async loadAudioFile() {
       if (!this.wave_surfer) return
 
       if (!this.file) return
+
+      // Clear regions
+      if (this.regions) {
+        this.regions.clearRegions()
+      }
+
       // Get Audio File URL
       const storage = getStorage()
       const fileRef = ref(storage, this.file)
@@ -91,6 +129,7 @@ export default {
         cursorColor: '#666',
         barWidth: 2,
         barRadius: 3,
+        plugins: [this.regions],
         // cursorWidth: 1,
         // height: 500,
       })
@@ -105,6 +144,11 @@ export default {
       } else {
         this.wave_surfer.play()
       }
+    },
+
+    analyzeTimeStamp() {
+      console.log('Analyze')
+      console.log(this.regionStart, this.regionEnd)
     },
   },
 }
