0a8e035a3eeb47e2d549d37b49df69fcac17ab99
Basma Elhoseny <basmaelhoseny6@gmail.com>
2024-08-16 16:53:25 +0300

    Added LIst of Vuetify

---
+++ src/components/molecules/SentimentTranscript.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container>
+      SentimentTranscript  
+    </v-container>
+</template>
+
+
+<script>
+export default {
+  props: {
+
+    sentiment: {
+      type: "POS"| "NEG" | "NEU",
+      default: '',
+    },
+    confidence: {
+      type: Number,
+      default: 0,
+    },
+   
+  },
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
