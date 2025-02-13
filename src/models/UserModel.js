a8a575aedefa7f45c3d1f33aefcbdb6287bd7bbb
Sahitya Chandra <sahityajb@gmail.com>
2025-02-14 01:32:57 +0530

    updated profileview to use usercontroller and retrieve user data from firestore DB

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -6,25 +6,34 @@
  * @param {Object[]} myAnswers - An array of UserAnswer value.
  * @param {Object[]} myTests - An array of UserTest value.
  * @param {Object[]} inbox - An array of past Notification values.
+ * @param {string} username - The username of the user.
+ * @param {string} contactNo - The contact number of the user.
+ * @param {string} country - The country of the user.
+ * @param {string} profileImage - The profile image URL of the user.
  */
 
 export default class User {
     constructor({
         id, accessLevel, email, notifications = [], myAnswers = [], myTests = [], inbox = [],
+        username = null, contactNo = null, country = null, profileImage = '',
     } = {}) {
-        this.id = id
-        this.accessLevel = accessLevel
-        this.email = email
-        this.notifications = notifications
-        this.myAnswers = myAnswers
-        this.myTests = myTests
-        this.inbox = inbox // Add inbox field
+        this.id = id;
+        this.accessLevel = accessLevel;
+        this.email = email;
+        this.notifications = notifications;
+        this.myAnswers = myAnswers;
+        this.myTests = myTests;
+        this.inbox = inbox; // Add inbox field
+        this.username = username; // Add username field
+        this.contactNo = contactNo; // Add contactNo field
+        this.country = country; // Add country field
+        this.profileImage = profileImage; // Add profileImage field
     }
 
     static toUser(data) {
         return new User({
             ...data,
-        })
+        });
     }
 
     toFirestore() {
@@ -35,14 +44,18 @@ export default class User {
             myAnswers: this.myAnswers,
             myTests: this.myTests,
             inbox: this.inbox, // Include inbox in Firestore representation
-        }
+            username: this.username, // Include username in Firestore representation
+            contactNo: this.contactNo, // Include contactNo in Firestore representation
+            country: this.country, // Include country in Firestore representation
+            profileImage: this.profileImage, // Include profileImage in Firestore representation
+        };
     }
 
     /**
      * Move all current notifications to the inbox.
      */
     archiveNotifications() {
-        this.inbox = [...this.inbox, ...this.notifications] // Add notifications to inbox
-        this.notifications = [] // Clear current notifications
+        this.inbox = [...this.inbox, ...this.notifications]; // Add notifications to inbox
+        this.notifications = []; // Clear current notifications
     }
 }
