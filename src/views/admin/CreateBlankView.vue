f9483119d5a00fc152fd2568451a4f156eb37e8f
Sahitya Chandra <sahityajb@gmail.com>
2025-02-15 23:45:49 +0530

    Added all 4 language support to CreateBlankView

--- src/views/admin/CreateBlankView.vue
+++ src/views/admin/CreateBlankView.vue
@@ -3,7 +3,7 @@
     <v-col cols="12">
       <v-row justify="center">
         <p class="titles ma-16">
-          What kind of test are you looking to start?
+          {{ $t('Createblank.title') }}
         </p>
       </v-row>
     </v-col>
@@ -13,10 +13,10 @@
         <v-col cols="10" md="5" sm="10" class="card">
           <CardTypeTest
             :img="require('../../../public/specialist.png')"
-            title="Usability Heuristic"
-            type="Test"
+            :title="$t('Createblank.testType_1.testTitle')"
+            :type="$t('Createblank.test')"
             segund-type="HEURISTICS"
-            :texts="['Usability Percentage', 'Final Report PDF', 'Invite specialists to evaluate your application']"
+            :texts="$t('Createblank.testType_1.text')"
             @click="setTestType"
           />
         </v-col>
@@ -24,10 +24,10 @@
         <v-col cols="12" md="5" sm="10" class="card">
           <CardTypeTest
             :img="require('../../../public/user.png')"
-            title="Usability User"
-            type="Test"
+            :title="$t('Createblank.testType_2.testTitle')"
+            :type="$t('Createblank.test')"
             segund-type="User"
-            :texts="['Webcam, audio & screen record', 'Enhanced answer analysis', 'Moderated or non moderated tests']"
+            :texts="$t('Createblank.testType_2.text')"
             @click="setTestType"
           />
         </v-col>
