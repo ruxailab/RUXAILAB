816a37768ee268a6d617b80e43b12f0d30978e81
Sahitya Chandra <sahityajb@gmail.com>
2025-03-25 18:20:44 +0530

    downgraded vue-i18n version

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack')
+
 module.exports = {
   transpileDependencies: [
     'vuetify',
@@ -14,10 +16,18 @@ module.exports = {
   },
 
   configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+        process: 'process/browser',
+      }),
+    ],
     resolve: {
       fallback: {
         crypto: require.resolve('crypto-browserify'),
         stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer/'),
+        process: require.resolve('process'),
       },
     },
   },
