a12b4b990e70e8208280d5e2f4e706ce13b2359a
Peter Huang <peterhxk@gmail.com>
2025-04-08 10:09:05 -0400

    removed changes to i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,17 +1,16 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import en from './locales/en.json'
 
 Vue.use(VueI18n)
 
-function loadLocaleMessages () {
+function loadLocaleMessages() {
   const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
   const messages = {}
   locales.keys().forEach((key) => {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i)
     if (matched && matched.length > 1) {
       const locale = matched[1]
-      messages[locale] = locales(key).default || locales(key)
+      messages[locale] = locales(key).default
     }
   })
   return messages
