dbad49c33e814c906e901db6ab4c456e42128cfe
Yuri Peruzzo <yuriperuzzo9@gmail.com>
2025-04-29 10:59:09 -0300

    fix responsive pages

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -24,11 +24,11 @@ const storage = getStorage(firebaseApp, `gs://${firebaseConfig.storageBucket}`)
 
 // Emulators if running locally
 
-/*if (process.env.NODE_ENV === 'development') {
-  connectFirestoreEmulator(db, 'localhost', 8081)
-  connectAuthEmulator(auth, 'http://localhost:9099')
-  connectFunctionsEmulator(fbFunctions, 'localhost', 5001)
-  connectStorageEmulator(storage, '127.0.0.1', 9199)
-}*/
+// if (process.env.NODE_ENV === 'development') {
+//   connectFirestoreEmulator(db, 'localhost', 8081)
+//   connectAuthEmulator(auth, 'http://localhost:9099')
+//   connectFunctionsEmulator(fbFunctions, 'localhost', 5001)
+//   connectStorageEmulator(storage, '127.0.0.1', 9199)
+// }
 
 export { auth, db, analytics, fbFunctions, storage }
