c7199449a8c6ba86f6dadfaa5054f49f0eba5d5e
Sahitya Chandra <sahityajb@gmail.com>
2025-06-02 15:30:30 +0530

    tip button and some lint fix

--- src/components/atoms/FormTask.vue
+++ src/components/atoms/FormTask.vue
@@ -1,37 +1,94 @@
 <template>
   <v-form ref="form">
     <v-row justify="space-around">
-      <v-col class="mt-4" cols="5">
-        <v-text-field v-model="localTask.taskName" :label="$t('common.name')" :rules="requiredRule" variant="outlined"
-          density="compact" />
-        <quill-editor v-model:value="localTask.taskDescription" class="mb-5" style="height: 40%;" />
-        <v-text-field v-model="localTask.taskTip" :label="$t('buttons.tip')" variant="outlined" density="compact" />
-        <v-text-field v-model="localTask.taskLink" label="Link" variant="outlined" density="compact" />
+      <v-col
+        class="mt-4"
+        cols="5"
+      >
+        <v-text-field
+          v-model="localTask.taskName"
+          :label="$t('common.name')"
+          :rules="requiredRule"
+          variant="outlined"
+          density="compact"
+        />
+        <quill-editor
+          v-model:value="localTask.taskDescription"
+          class="mb-5"
+          style="height: 40%;"
+        />
+        <v-text-field
+          v-model="localTask.taskTip"
+          :label="$t('buttons.tip')"
+          variant="outlined"
+          density="compact"
+        />
+        <v-text-field
+          v-model="localTask.taskLink"
+          label="Link"
+          variant="outlined"
+          density="compact"
+        />
       </v-col>
       <v-col cols="5">
-        <v-radio-group v-model="localTask.taskType" :label="$t('titles.answerType')" :mandatory="false"
-          :rules="requiredRule">
-          <v-radio :label="$t('switches.noAnswer')" value="null" />
-          <v-radio :label="$t('switches.textArea')" value="textArea" />
-          <v-radio :label="$t('switches.postTest')" value="form" />
-          <v-radio :label="$t('switches.postForm')" value="postForm" />
+        <v-radio-group
+          v-model="localTask.taskType"
+          :label="$t('titles.answerType')"
+          :mandatory="false"
+          :rules="requiredRule"
+        >
+          <v-radio
+            :label="$t('switches.noAnswer')"
+            value="null"
+          />
+          <v-radio
+            :label="$t('switches.textArea')"
+            value="textArea"
+          />
+          <v-radio
+            :label="$t('switches.postTest')"
+            value="form"
+          />
+          <v-radio
+            :label="$t('switches.postForm')"
+            value="postForm"
+          />
         </v-radio-group>
-        <v-text-field v-if="localTask.taskType === 'form'" v-model="localTask.postQuestion"
-          :label="$t('switches.postTest')" variant="outlined" density="compact" />
-        <v-text-field v-if="task.taskType === 'postForm'" v-model="localTask.postForm" :label="$t('switches.postForm')"
-          variant="outlined" density="compact"
-          :rules="[(v) => !!v && v.startsWith('http') || 'Field must be a valid URL']" />
+        <v-text-field
+          v-if="localTask.taskType === 'form'"
+          v-model="localTask.postQuestion"
+          :label="$t('switches.postTest')"
+          variant="outlined"
+          density="compact"
+        />
+        <v-text-field
+          v-if="task.taskType === 'postForm'"
+          v-model="localTask.postForm"
+          :label="$t('switches.postForm')"
+          variant="outlined"
+          density="compact"
+          :rules="[(v) => !!v && v.startsWith('http') || 'Field must be a valid URL']"
+        />
         <v-row align="center">
           {{ $t('switches.screenRecord') }}
-          <v-switch v-model="localTask.hasScreenRecord" class="ml-2" />
+          <v-switch
+            v-model="localTask.hasScreenRecord"
+            class="ml-2"
+          />
         </v-row>
         <v-row align="center">
           {{ $t('switches.camera') }}
-          <v-switch v-model="localTask.hasCamRecord" class="ml-2" />
+          <v-switch
+            v-model="localTask.hasCamRecord"
+            class="ml-2"
+          />
         </v-row>
         <v-row align="center">
           {{ $t('switches.audioRecord') }}
-          <v-switch v-model="localTask.hasAudioRecord" class="ml-2" />
+          <v-switch
+            v-model="localTask.hasAudioRecord"
+            class="ml-2"
+          />
         </v-row>
       </v-col>
     </v-row>
