24cd26efeade748b28ac589c93951be290f7ffa0
saltykheera <vishal.kumar-ug23@nsut.ac.in>
2025-08-01 13:17:40 +0530

    (codedupli) attempt fix

--- src/router/modules/manualAccessibility.js
+++ src/router/modules/manualAccessibility.js
@@ -1,70 +1,50 @@
-const authorizeMeta = { meta: { authorize: [1] } };
+import { createAccessibilityRoutes } from './accessibilityUtils';
 
-const manualAccessibilityRoutes = [
-    {
-        path: '/accessibility/manual/:testId',
-        name: 'ManualAccessibility',
-        component: () => import('@/views/admin/AccessibilityManagerView.vue'),
-        ...authorizeMeta,
-        props: true,
-        children: [
-            {
-                path: '',
-                name: 'AccessibilityHome',
-                component: () => import('@/views/admin/ManualAccessibility/AccessibilityHome.vue'),
-                ...authorizeMeta
-            },
-            {
-                path: '/edit/:testId',
-                name: 'EditAccessibilityTest',
-                component: () => import('@/views/admin/ManualAccessibility/AccessibilityEditTest.vue'),
-                props: true,
-                ...authorizeMeta
-            },
-            {
-                path: '/preview/:testId',
-                name: 'AccessibilityPreviewTest',
-                component: () => import('@/views/admin/ManualAccessibility/AccessibilityPreviewTest.vue'),
-                props: true,
-                ...authorizeMeta
-            },
-            {
-                path: '/result/:testId',
-                name: 'AccessibilityTestAnswers',
-                component: () => import('@/views/admin/ManualAccessibility/AccessibilityAnswer.vue'),
-                props: true,
-                ...authorizeMeta
-            },
-            {
-                path: '/report/:testId',
-                name: 'AccessibilityTestReport',
-                component: () => import('@/views/admin/AccessibilityReport.vue'),
-                props: true,
-                ...authorizeMeta
-            },
-            {
-                path: '/cooperative/:testId',
-                name: 'AccessibilityTestCooperative',
-                component: () => import('@/views/admin/ManualAccessibility/AccessibilityCooperative.vue'),
-                props: true,
-                ...authorizeMeta
-            },
-            {
-                path: '/setting/:testId',
-                name: 'AccessibilityTestSettings',
-                component: () => import('@/views/admin/AccessibilitySettings.vue'),
-                props: true,
-                ...authorizeMeta
-            },
-            {
-                path: '/config/:testId',
-                name: 'AccessibilityConfig',
-                component: () => import('@/views/admin/ManualAccessibility/AccessibilityConfig.vue'),
-                props: true,
-                ...authorizeMeta
-            }
-        ]
-    }
-];
+const manualAccessibilityRoutes = createAccessibilityRoutes(
+    '/accessibility/manual',
+    () => import('@/views/admin/AccessibilityManagerView.vue'),
+    [
+        {
+            path: '',
+            name: 'AccessibilityHome',
+            component: () => import('@/views/admin/ManualAccessibility/AccessibilityHome.vue')
+        },
+        {
+            path: '/edit/:testId',
+            name: 'EditAccessibilityTest',
+            component: () => import('@/views/admin/ManualAccessibility/AccessibilityEditTest.vue')
+        },
+        {
+            path: '/preview/:testId',
+            name: 'AccessibilityPreviewTest',
+            component: () => import('@/views/admin/ManualAccessibility/AccessibilityPreviewTest.vue')
+        },
+        {
+            path: '/result/:testId',
+            name: 'AccessibilityTestAnswers',
+            component: () => import('@/views/admin/ManualAccessibility/AccessibilityAnswer.vue')
+        },
+        {
+            path: '/report/:testId',
+            name: 'AccessibilityTestReport',
+            component: () => import('@/views/admin/AccessibilityReport.vue')
+        },
+        {
+            path: '/cooperative/:testId',
+            name: 'AccessibilityTestCooperative',
+            component: () => import('@/views/admin/ManualAccessibility/AccessibilityCooperative.vue')
+        },
+        {
+            path: '/setting/:testId',
+            name: 'AccessibilityTestSettings',
+            component: () => import('@/views/admin/AccessibilitySettings.vue')
+        },
+        {
+            path: '/config/:testId',
+            name: 'AccessibilityConfig',
+            component: () => import('@/views/admin/ManualAccessibility/AccessibilityConfig.vue')
+        }
+    ]
+);
 
 export default manualAccessibilityRoutes;
