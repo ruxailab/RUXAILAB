6765fc46dcb7f03b88d4a0e91ee7fdc7c312853e
saltykheera <vishal.kumar-ug23@nsut.ac.in>
2025-08-01 13:04:40 +0530

    (qa): remove redundant code

--- src/router/modules/manualAccessibility.js
+++ src/router/modules/manualAccessibility.js
@@ -1,67 +1,70 @@
+const authorizeMeta = { meta: { authorize: [1] } };
 
-export default [
+const manualAccessibilityRoutes = [
     {
         path: '/accessibility/manual/:testId',
         name: 'ManualAccessibility',
-        meta: { authorize: [1] },
         component: () => import('@/views/admin/AccessibilityManagerView.vue'),
+        ...authorizeMeta,
         props: true,
         children: [
             {
                 path: '',
                 name: 'AccessibilityHome',
                 component: () => import('@/views/admin/ManualAccessibility/AccessibilityHome.vue'),
-                meta: { authorize: [1] }
+                ...authorizeMeta
             },
             {
                 path: '/edit/:testId',
                 name: 'EditAccessibilityTest',
                 component: () => import('@/views/admin/ManualAccessibility/AccessibilityEditTest.vue'),
                 props: true,
-                meta: { authorize: [1] }
+                ...authorizeMeta
             },
             {
                 path: '/preview/:testId',
                 name: 'AccessibilityPreviewTest',
                 component: () => import('@/views/admin/ManualAccessibility/AccessibilityPreviewTest.vue'),
                 props: true,
-                meta: { authorize: [1] }
+                ...authorizeMeta
             },
             {
                 path: '/result/:testId',
                 name: 'AccessibilityTestAnswers',
                 component: () => import('@/views/admin/ManualAccessibility/AccessibilityAnswer.vue'),
                 props: true,
-                meta: { authorize: [1] }
+                ...authorizeMeta
             },
             {
                 path: '/report/:testId',
                 name: 'AccessibilityTestReport',
                 component: () => import('@/views/admin/AccessibilityReport.vue'),
                 props: true,
-                meta: { authorize: [1] }
+                ...authorizeMeta
             },
             {
                 path: '/cooperative/:testId',
                 name: 'AccessibilityTestCooperative',
                 component: () => import('@/views/admin/ManualAccessibility/AccessibilityCooperative.vue'),
                 props: true,
-                meta: { authorize: [1] }
+                ...authorizeMeta
             },
             {
                 path: '/setting/:testId',
                 name: 'AccessibilityTestSettings',
                 component: () => import('@/views/admin/AccessibilitySettings.vue'),
                 props: true,
-                meta: { authorize: [1] }
+                ...authorizeMeta
             },
             {
                 path: '/config/:testId',
                 name: 'AccessibilityConfig',
                 component: () => import('@/views/admin/ManualAccessibility/AccessibilityConfig.vue'),
                 props: true,
-                meta: { authorize: [1] }
-            },
+                ...authorizeMeta
+            }
         ]
-    },
+    }
 ];
+
+export default manualAccessibilityRoutes;
