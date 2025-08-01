1536ed0d9e2e6b453f47d69f6e0dd56d28c31161
saltykheera <vishal.kumar-ug23@nsut.ac.in>
2025-08-01 13:04:40 +0530

    (qa): remove redundant code

--- src/router/modules/automaticAccessibility.js
+++ src/router/modules/automaticAccessibility.js
@@ -1,47 +1,51 @@
-export default [
+const authorizeMeta = { meta: { authorize: [1] } };
+
+const automatedAccessibilityRoutes = [
     {
         path: '/accessibility/automatic/:testId',
         name: 'AutomatedAccessibility',
         component: () => import('@/views/admin/AutomatedAccessibilityManager.vue'),
-        meta: { authorize: [1] },
+        ...authorizeMeta,
         props: true,
         children: [
             {
                 path: '',
                 name: 'AutomatedAccessibilityHome',
                 component: () => import('@/views/admin/AutomatedAccessibility/AutomatedAccessibilityHome.vue'),
-                meta: { authorize: [1] }
+                ...authorizeMeta
             },
             {
                 path: '/analyse/:testId',
                 name: 'Analyse',
                 component: () => import('@/views/admin/AutomatedAccessibility/EditTest.vue'),
-                meta: { authorize: [1] }
+                ...authorizeMeta
             },
             {
                 path: '/answers/:testId',
                 name: 'AccessibilityAnswers',
                 component: () => import('@/views/admin/AutomatedAccessibility/Answers.vue'),
-                meta: { authorize: [1] }
+                ...authorizeMeta
             },
             {
                 path: '/report/:testId',
                 name: 'AccessibilityReport',
                 component: () => import('@/views/admin/AutomatedAccessibility/Report.vue'),
-                meta: { authorize: [1] }
+                ...authorizeMeta
             },
             {
                 path: '/cooperation/:testId',
                 name: 'AccessibilityCooperation',
                 component: () => import('@/views/admin/AutomatedAccessibility/Cooperation.vue'),
-                meta: { authorize: [1] }
+                ...authorizeMeta
             },
             {
                 path: '/settings/:testId',
                 name: 'AccessibilitySettings',
                 component: () => import('@/views/admin/AutomatedAccessibility/Settings.vue'),
-                meta: { authorize: [1] }
+                ...authorizeMeta
             }
         ]
-    },
+    }
 ];
+
+export default automatedAccessibilityRoutes;
