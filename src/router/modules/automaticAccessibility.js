c239d21d232fa1ae62a0077e74c6bb0bedfb02c3
saltykheera <vishal.kumar-ug23@nsut.ac.in>
2025-08-01 13:17:40 +0530

    (codedupli) attempt fix

--- src/router/modules/automaticAccessibility.js
+++ src/router/modules/automaticAccessibility.js
@@ -1,51 +1,40 @@
-const authorizeMeta = { meta: { authorize: [1] } };
+import { createAccessibilityRoutes } from './accessibilityUtils';
 
-const automatedAccessibilityRoutes = [
-    {
-        path: '/accessibility/automatic/:testId',
-        name: 'AutomatedAccessibility',
-        component: () => import('@/views/admin/AutomatedAccessibilityManager.vue'),
-        ...authorizeMeta,
-        props: true,
-        children: [
-            {
-                path: '',
-                name: 'AutomatedAccessibilityHome',
-                component: () => import('@/views/admin/AutomatedAccessibility/AutomatedAccessibilityHome.vue'),
-                ...authorizeMeta
-            },
-            {
-                path: '/analyse/:testId',
-                name: 'Analyse',
-                component: () => import('@/views/admin/AutomatedAccessibility/EditTest.vue'),
-                ...authorizeMeta
-            },
-            {
-                path: '/answers/:testId',
-                name: 'AccessibilityAnswers',
-                component: () => import('@/views/admin/AutomatedAccessibility/Answers.vue'),
-                ...authorizeMeta
-            },
-            {
-                path: '/report/:testId',
-                name: 'AccessibilityReport',
-                component: () => import('@/views/admin/AutomatedAccessibility/Report.vue'),
-                ...authorizeMeta
-            },
-            {
-                path: '/cooperation/:testId',
-                name: 'AccessibilityCooperation',
-                component: () => import('@/views/admin/AutomatedAccessibility/Cooperation.vue'),
-                ...authorizeMeta
-            },
-            {
-                path: '/settings/:testId',
-                name: 'AccessibilitySettings',
-                component: () => import('@/views/admin/AutomatedAccessibility/Settings.vue'),
-                ...authorizeMeta
-            }
-        ]
-    }
-];
+const automatedAccessibilityRoutes = createAccessibilityRoutes(
+    '/accessibility/automatic',
+    () => import('@/views/admin/AutomatedAccessibilityManager.vue'),
+    [
+        {
+            path: '',
+            name: 'AutomatedAccessibilityHome',
+            component: () => import('@/views/admin/AutomatedAccessibility/AutomatedAccessibilityHome.vue')
+        },
+        {
+            path: '/analyse/:testId',
+            name: 'Analyse',
+            component: () => import('@/views/admin/AutomatedAccessibility/EditTest.vue')
+        },
+        {
+            path: '/answers/:testId',
+            name: 'AccessibilityAnswers',
+            component: () => import('@/views/admin/AutomatedAccessibility/Answers.vue')
+        },
+        {
+            path: '/report/:testId',
+            name: 'AccessibilityReport',
+            component: () => import('@/views/admin/AutomatedAccessibility/Report.vue')
+        },
+        {
+            path: '/cooperation/:testId',
+            name: 'AccessibilityCooperation',
+            component: () => import('@/views/admin/AutomatedAccessibility/Cooperation.vue')
+        },
+        {
+            path: '/settings/:testId',
+            name: 'AccessibilitySettings',
+            component: () => import('@/views/admin/AutomatedAccessibility/Settings.vue')
+        }
+    ]
+);
 
 export default automatedAccessibilityRoutes;
