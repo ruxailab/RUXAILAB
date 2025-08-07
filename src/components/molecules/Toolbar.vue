cdd99109f7d06b9f8c00e3f35c5e9f0f9a623722
Marc Gonzalez Capdevila <marcgc21@gmail.com>
2025-08-07 11:32:19 +0200

    style: added logo and colours

--- src/components/molecules/Toolbar.vue
+++ src/components/molecules/Toolbar.vue
@@ -1,130 +1,64 @@
 <template>
   <div>
-    <v-app-bar
-      density="comfortable"
-      color="#272727"
-      class="px-4"
-    >
-      <v-btn
-        v-if="user"
-        icon
-        class="d-flex d-lg-none"
-        @click.stop="drawer = !drawer"
-      >
+    <v-app-bar density="comfortable" color="#00213F" class="px-4">
+      <v-btn v-if="user" icon class="d-flex d-lg-none" @click.stop="drawer = !drawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
 
-      <v-toolbar-title
-        v-if="$route.path !== '/help'"
-        style="cursor: pointer"
-        @click="goTo('/testslist')"
-      >
-        <v-icon @click="goTo('/testslist')">
-          mdi-alpha-r-circle
-        </v-icon>
-        RUXAILAB
+      <v-toolbar-title v-if="$route.path !== '/help'" style="cursor: pointer" class="d-flex align-center"
+        @click="goTo('/testslist')">
+        <img src="@/assets/ruxailab-long-crop-white.png" alt="RUXAILAB Logo" height="25" class="mr-3 align-self-center"
+          style="vertical-align: middle;" @click="goTo('/testslist')" />
       </v-toolbar-title>
-      <v-toolbar-title
-        v-else
-        style="cursor: pointer"
-        @click="goTo('/testslist')"
-      >
-        Help Center
+      <v-toolbar-title v-else style="cursor: pointer" class="d-flex align-center" @click="goTo('/testslist')">
+        <img src="@/assets/ruxailab-long-crop-white.png" alt="RUXAILAB Logo" height="25" class="mr-3 align-self-center"
+          style="vertical-align: middle;" @click="goTo('/testslist')" />
+        <span class="text-white font-weight-bold">Help Center</span>
       </v-toolbar-title>
 
       <v-spacer />
 
       <locale-changer />
 
-      <v-btn
-        v-if="$route.path === '/' && user"
-        variant="text"
-        color="#f9a826"
-        class="console-button mx-1 d-none d-lg-flex"
-        @click="goTo('/testslist')"
-      >
+      <v-btn v-if="$route.path === '/' && user" variant="text" color="#f9a826"
+        class="console-button mx-1 d-none d-lg-flex" @click="goTo('/testslist')">
         {{ $t('buttons.goToConsole') }}
       </v-btn>
 
-      <v-btn
-        v-if="['/testslist', '/signin', '/signup'].includes($route.path)"
-        variant="text"
-        color="#f9a826"
-        class="console-button mx-1 d-none d-lg-flex"
-        @click="goTo('/')"
-      >
+      <v-btn v-if="['/testslist', '/signin', '/signup'].includes($route.path)" variant="text" color="#f9a826"
+        class="console-button mx-1 d-none d-lg-flex" @click="goTo('/')">
         {{ $t('AccessNotAllowed.goHome') }}
       </v-btn>
 
-      <v-btn
-        v-if="!['/', '/testslist', '/signin', '/signup'].includes($route.path)"
-        variant="text"
-        color="#f9a826"
-        class="console-button mx-1 d-none d-lg-flex"
-        @click="goTo('/testslist')"
-      >
+      <v-btn v-if="!['/', '/testslist', '/signin', '/signup'].includes($route.path)" variant="text" color="#f9a826"
+        class="console-button mx-1 d-none d-lg-flex" @click="goTo('/testslist')">
         {{ $t('buttons.returnToConsole') }}
       </v-btn>
 
       <HelpButton :class="smAndDown ? 'mx-1' : 'mx-2'" />
-      <NotificationBtn
-        v-if="user"
-        :class="smAndDown ? 'mx-1' : 'mx-2'"
-      />
+      <NotificationBtn v-if="user" :class="smAndDown ? 'mx-1' : 'mx-2'" />
 
-      <v-btn
-        v-if="!user"
-        variant="text"
-        class="d-none d-lg-flex"
-        @click="goTo('/signin')"
-      >
+      <v-btn v-if="!user" variant="text" class="d-none d-lg-flex" @click="goTo('/signin')">
         <v-icon start>
           mdi-lock
         </v-icon>
         {{ $t('SIGNIN.sign-in') }}
       </v-btn>
 
-      <v-btn
-        v-if="!user"
-        icon
-        class="d-flex d-lg-none"
-        @click="goTo('/signin')"
-      >
+      <v-btn v-if="!user" icon class="d-flex d-lg-none" @click="goTo('/signin')">
         <v-icon :size="iconSize">
           mdi-lock
         </v-icon>
       </v-btn>
 
       <div class="d-none d-lg-flex">
-        <v-menu
-          v-if="user"
-          v-model="menu"
-          :offset="[0, 8]"
-          min-width="300"
-          transition="slide-y-transition"
-          :close-on-content-click="false"
-          class="rounded-lg"
-        >
+        <v-menu v-if="user" v-model="menu" :offset="[0, 8]" min-width="300" transition="slide-y-transition"
+          :close-on-content-click="false" class="rounded-lg">
           <template #activator="{ props }">
-            <v-btn
-              variant="text"
-              class="pa-0 btn-fix"
-              v-bind="props"
-            >
-              <v-avatar
-                size="24"
-                class="mr-1"
-              >
-                <v-img
-                  v-if="profileImage"
-                  :src="profileImage"
-                  alt="User Profile"
-                />
-                <v-icon
-                  v-else
-                  color="white"
-                  size="24"
-                >
+            <v-btn variant="text" class="pa-0 btn-fix" v-bind="props">
+              <v-avatar size="24" class="mr-1">
+                <v-img v-if="profileImage" :src="profileImage" alt="User Profile" />
+                <v-icon v-else color="white" size="24">
                   mdi-account-circle
                 </v-icon>
               </v-avatar>
@@ -137,22 +71,14 @@
           <template #default>
             <div class="custom-dropdown bg-white rounded-lg">
               <div class="pa-6 d-flex align-center">
-                <v-avatar
-                  size="48"
-                  color="primary"
-                  class="elevation-2"
-                >
+                <v-avatar size="48" color="primary" class="elevation-2">
                   <span class="text-h5 font-weight-medium text-white">{{ userInitial }}</span>
                 </v-avatar>
                 <div class="ml-4 flex-grow-1">
                   <div class="d-flex align-center">
                     <span class="text-h6 font-weight-bold text-grey-darken-4">{{ username || $t('buttons.username')
-                    }}</span>
-                    <v-icon
-                      color="primary"
-                      size="20"
-                      class="ml-2"
-                    >
+                      }}</span>
+                    <v-icon color="primary" size="20" class="ml-2">
                       mdi-check-decagram
                     </v-icon>
                   </div>
@@ -164,21 +90,12 @@
 
               <div class="pa-2">
                 <v-hover v-slot="{ isHovering }">
-                  <div
-                    class="d-flex align-center px-4 py-3 rounded-lg cursor-pointer"
-                    :class="{ 'primary lighten-5': isHovering }"
-                    @click="goToProfile(); menu = false;"
-                  >
-                    <v-icon
-                      color="primary"
-                      size="20"
-                    >
+                  <div class="d-flex align-center px-4 py-3 rounded-lg cursor-pointer"
+                    :class="{ 'primary lighten-5': isHovering }" @click="goToProfile(); menu = false;">
+                    <v-icon color="primary" size="20">
                       mdi-account
                     </v-icon>
-                    <span
-                      class="ml-3 text-subtitle-1 font-weight-medium"
-                      :class="{ 'primary--text': isHovering }"
-                    >
+                    <span class="ml-3 text-subtitle-1 font-weight-medium" :class="{ 'primary--text': isHovering }">
                       {{ $t('buttons.profile') }}
                     </span>
                   </div>
@@ -187,21 +104,12 @@
                 <v-divider class="my-2" />
 
                 <v-hover v-slot="{ isHovering }">
-                  <div
-                    class="d-flex align-center px-4 py-3 rounded-lg cursor-pointer"
-                    :class="{ 'error lighten-5': isHovering }"
-                    @click="signOut(); menu = false;"
-                  >
-                    <v-icon
-                      color="error"
-                      size="20"
-                    >
+                  <div class="d-flex align-center px-4 py-3 rounded-lg cursor-pointer"
+                    :class="{ 'error lighten-5': isHovering }" @click="signOut(); menu = false;">
+                    <v-icon color="error" size="20">
                       mdi-logout
                     </v-icon>
-                    <span
-                      class="ml-3 text-subtitle-1 font-weight-medium"
-                      :class="{ 'error--text': isHovering }"
-                    >
+                    <span class="ml-3 text-subtitle-1 font-weight-medium" :class="{ 'error--text': isHovering }">
                       {{ $t('buttons.signout') }}
                     </span>
                   </div>
@@ -213,62 +121,34 @@
       </div>
     </v-app-bar>
 
-    <v-navigation-drawer
-      v-if="user"
-      v-model="drawer"
-      temporary
-      class="d-flex d-lg-none"
-      style="height: 100vh;"
-    >
+    <v-navigation-drawer v-if="user" v-model="drawer" temporary class="d-flex d-lg-none" style="height: 100vh;">
       <div class="mobile-dropdown dropdown">
-        <div
-          class="pa-4 d-flex align-center"
-          style="cursor: pointer"
-          @click="goToProfile(); drawer = false;"
-        >
-          <v-avatar
-            size="48"
-            color="primary"
-            class="user-avatar text-white"
-          >
+        <div class="pa-4 d-flex align-center" style="cursor: pointer" @click="goToProfile(); drawer = false;">
+          <v-avatar size="48" color="primary" class="user-avatar text-white">
             <span class="text-h5 font-weight-medium">{{ userInitial }}</span>
           </v-avatar>
           <div class="ml-4 flex-grow-1">
             <div class="d-flex align-center">
-              <span class="mobile-user-name text-white">{{ username || $t('buttons.username') }}</span>
-              <v-icon
-                color="primary"
-                size="20"
-                class="ml-2 verified-icon"
-              >
+              <span class="mobile-user-name" style="color: #00213F;">{{ username || $t('buttons.username') }}</span>
+              <v-icon color="primary" size="20" class="ml-2 verified-icon">
                 mdi-check-decagram
               </v-icon>
             </div>
-            <span class="mobile-user-email text-grey-lighten-1">{{ user.email }}</span>
+            <span class="mobile-user-email text-grey-darken-1">{{ user.email }}</span>
           </div>
         </div>
 
         <v-divider />
 
-        <v-list
-          v-if="isManager && test"
-          density="compact"
-          nav
-        >
-          <v-list-item
-            v-for="(menuItem, i) in items"
-            :key="i"
-            :value="menuItem.path"
-            link
-            class="mobile-menu-item rounded-lg mx-2"
-            @click="goTo(menuItem.path); drawer = false;"
-          >
+        <v-list v-if="isManager && test" density="compact" nav>
+          <v-list-item v-for="(menuItem, i) in items" :key="i" :value="menuItem.path" link
+            class="mobile-menu-item rounded-lg mx-2" @click="goTo(menuItem.path); drawer = false;">
             <template #prepend>
-              <v-icon color="white">
+              <v-icon class="menu-icon">
                 {{ menuItem.icon }}
               </v-icon>
             </template>
-            <v-list-item-title class="text-white">
+            <v-list-item-title class="menu-title">
               {{ menuItem.title }}
             </v-list-item-title>
           </v-list-item>
@@ -279,17 +159,11 @@
         <div class="mobile-signout">
           <v-divider />
           <div class="pa-2">
-            <div
-              class="mobile-menu-item d-flex align-center px-4 py-3 rounded-lg"
-              @click="signOut(); drawer = false;"
-            >
-              <v-icon
-                color="error"
-                size="20"
-              >
+            <div class="mobile-menu-item d-flex align-center px-4 py-3 rounded-lg" @click="signOut(); drawer = false;">
+              <v-icon color="error" size="20">
                 mdi-logout
               </v-icon>
-              <span class="ml-3 text-white">{{ $t('buttons.signout') }}</span>
+              <span class="ml-3" style="color: #00213F;">{{ $t('buttons.signout') }}</span>
             </div>
           </div>
         </div>
@@ -548,5 +422,39 @@ onMounted(() => {
   flex: 1;
   display: flex;
   flex-direction: column;
+}
+
+/* Mobile menu specific styling */
+.mobile-menu-item .v-list-item-title {
+  color: #00213F !important;
+}
+
+.mobile-menu-item .v-icon {
+  color: #00213F !important;
+}
+
+.mobile-menu-item .menu-icon {
+  color: #00213F !important;
+}
+
+.mobile-menu-item .menu-title {
+  color: #00213F !important;
+}
+
+.mobile-user-name {
+  color: #00213F !important;
+}
+
+.mobile-signout span {
+  color: #00213F !important;
+}
+
+/* Force blue color for all mobile menu text */
+.v-navigation-drawer .mobile-dropdown .v-list-item-title {
+  color: #00213F !important;
+}
+
+.v-navigation-drawer .mobile-dropdown .v-icon:not(.verified-icon) {
+  color: #00213F !important;
 }
 </style>
