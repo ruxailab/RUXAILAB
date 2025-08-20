ff9fd85de584d4146afec65b3bc7c339f8f9e029
Sahitya Chandra <sahityajb@gmail.com>
2025-08-20 23:35:44 +0530

    major bugs fixes

--- src/components/organisms/EditUserTest.vue
+++ src/components/organisms/EditUserTest.vue
@@ -18,6 +18,7 @@
     <v-card v-if="index === 0" rounded="xxl">
       <TextareaForm :title="$t('ModeratedTest.consentForm')"
         subtitle="Edit the consent text for the test. Changes are saved when you click the Save button."
+        @update:value="saveState($event)"
         @input="updateData" />
     </v-card>
 
@@ -25,7 +26,7 @@
       <UserVariables @input="updateData" />
     </v-card>
 
-    <ListTasks v-if="index === 2" :tasks="object.itemsTasks" @input="updateData" />
+    <ListTasks v-if="index === 2" @input="updateData" />
 
     <v-card v-if="index === 3" rounded="xxl">
       <FormPostTest @input="updateData" />
@@ -39,7 +40,7 @@ import { useStore } from 'vuex';
 import ListTasks from '@/components/molecules/ListTasks.vue';
 import FormPostTest from '@/components/atoms/FormPostTest.vue';
 import UserVariables from '@/components/atoms/UserVariables.vue';
-import TextareaForm from '../atoms/TextareaForm.vue';
+import TextareaForm from '@/components/atoms/TextareaForm.vue';
 
 const props = defineProps({
   type: {
@@ -67,6 +68,10 @@ const formData = ref({
 
 const testStructure = computed(() => store.state.Tests.Test.testStructure);
 
+const saveState = async (value) => {
+  await store.dispatch('setConsent', value)
+}
+
 onMounted(() => {
   if (props.type !== 'content' && props.type !== 'tabs') {
     console.error(`${props.type} type in EditUserTest.vue is not valid.`);
@@ -88,9 +93,12 @@ const tabClicked = (index) => {
 
 const updateData = (data) => {
   if (props.index === 0) {
+    store.dispatch('setConsent', data);
+  }
+  if (props.index === 1) {
     store.dispatch('setPreTest', data);
   }
-  if (props.index === 2) {
+  if (props.index === 3) {
     store.dispatch('setPostTest', data);
   }
 };
