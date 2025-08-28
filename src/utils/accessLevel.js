db9e323165a2b335bc6271bf149da5432904c412
JulioManoel <juliobonow@gmail.com>
2025-08-28 15:23:34 -0300

    Update Route Tests

--- src/utils/accessLevel.js
+++ src/utils/accessLevel.js
@@ -1,19 +1,26 @@
 export const systemAccessLevels =
 {
-    0: 'superAdmin',
-    1: 'admin',
-    2: 'user',
+  0: 'superAdmin',
+  1: 'admin',
+  2: 'user',
 }
 
 export const testAccessLevels =
 {
-    0: 'administrator',
-    1: 'guest',
-    2: 'evaluator',
+  0: 'administrator',
+  1: 'guest',
+  2: 'evaluator',
+}
+
+export const ACCESS_LEVEL =
+{
+  ADMIN: 0,
+  GUEST: 1,
+  EVALUETOR: 2,
 }
 
 export const testTypes =
 {
-    0: 'heuristics',
-    1: 'user',
+  0: 'heuristics',
+  1: 'user',
 }
