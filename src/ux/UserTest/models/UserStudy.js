6cc49f8de9c392dc4ee6f0b91062a1aaab0de361
Karine Pistili <karine.pistili@gmail.com>
2025-09-01 08:56:20 -0300

    feat(models): updated study type enums (wip)

--- src/ux/UserTest/models/UserStudy.js
+++ src/ux/UserTest/models/UserStudy.js
@@ -1,9 +1,10 @@
+import { STUDY_TYPES } from '@/shared/constants/methodDefinitions'
 import Study from '../../../shared/models/Study'
 
 export default class UserStudy extends Study {
     constructor(params = {}) {
         super(params)
-        this.testType = 'User'
+        this.testType = STUDY_TYPES.USER
     }
 
     toFirestore() {
