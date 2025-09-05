cf7db6d5f7524136d011a0e0c02eef121b0f2cbe
saltykheera <vishal.kumar-ug23@nsut.ac.in>
2025-09-05 15:25:10 +0530

    chores: moved controller and store compatible with new project structure

--- src/ux/accessibility/controllers/ManualAccessibilityController.js
+++ src/ux/accessibility/controllers/ManualAccessibilityController.js
@@ -1,4 +1,4 @@
-import BaseAccessibilityController from '@/controllers/BaseAccessibilityController';
+import BaseAccessibilityController from './BaseAccessibilityController';
 import ManualAccessibilityTest from '@/ux/accessibility/models/ManualAccessibilityTest';
 
 export default new BaseAccessibilityController(ManualAccessibilityTest, 'MANUAL');
