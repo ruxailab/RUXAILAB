3350c0c4a929513efd536bedfe428065cd718fe7
Karine Pistili <karine.pistili@gmail.com>
2025-09-08 15:20:24 -0300

    fix(templates): fixed community list of templates

--- src/shared/composables/useItemFormatting.js
+++ src/shared/composables/useItemFormatting.js
@@ -5,12 +5,13 @@ export function useItemFormatting(type) {
     const { t } = useI18n()
 
     const getItemTitle = (item) => {
-        if (type.value === 'myTemplates') return item.header?.templateTitle
+        if (type.value === 'myTemplates' || type.value === 'publicTemplates') return item.header?.templateTitle
         return item.testTitle ?? item.email ?? 'Untitled'
     }
 
     const getOwnerName = (item) => {
         if (type.value === 'myTemplates') return t('pages.listTests.me')
+        if (type.value === 'publicTemplates') return item.header?.templateAuthor?.userEmail || t('pages.listTests.unknown')
         return (
             item.testAdmin?.email ??
             item.testAuthorEmail ??
@@ -19,16 +20,16 @@ export function useItemFormatting(type) {
     }
 
     const getOwnerImage = (item) => {
-        if (type.value === 'myTemplates') return item.header?.templateAuthor?.imageUrl || null
+        if (type.value === 'myTemplates' || type.value === 'publicTemplates') return item.header?.templateAuthor?.imageUrl || null
         return item.testAdmin?.imageUrl || null
     }
 
     const getParticipantCount = (item) => {
-        return item.numberColaborators ?? '-'
+        return item.numberColaborators ?? 0
     }
 
     const formatItemDate = (item) => {
-        if (type.value === 'myTemplates') return formatDateLong(item.header.creationDate, 'es')
+        if (type.value === 'myTemplates' || type.value === 'publicTemplates') return formatDateLong(item.header.creationDate, 'es')
         return formatDateLong(item.creationDate || item.updateDate, 'es')
     }
 
