7a9047fd920ce39a6afde46b774d8b81b2ccaeb4
saltykheera <vishal.kumar-ug23@nsut.ac.in>
2025-09-11 15:06:23 +0530

    use shared cooperation view

--- src/ux/accessibility/router.js
+++ src/ux/accessibility/router.js
@@ -1,13 +1,14 @@
 import { createAccessibilityRoutes } from './accessibilityUtils';
 // Shared Settings View
 import SettingsView from '@/shared/views/SettingsView.vue';
+// Shared Cooperators View
+import CooperatorsView from '@/shared/views/CooperatorsView.vue';
 
 // Manual-Accessibility-Pages(x7)
 import AccessibilityManagerView from '@/ux/accessibility/view/manual/AccessibilityManagerView.vue';
 import AccessibilityHome from '@/ux/accessibility/view/manual/AccessibilityHome.vue';
 import AccessibilityPreviewTest from '@/ux/accessibility/view/manual/AccessibilityPreviewTest.vue';
 import AccessibilityTestAnswers from '@/ux/accessibility/view/manual/AccessibilityAnswer.vue';
-import AccessibilityTestCooperative from '@/ux/accessibility/view/manual/AccessibilityCooperative.vue';
 import AccessibilityConfig from '@/ux/accessibility/view/manual/AccessibilityConfig.vue';
 
 // Automated-Accessibility-Pages (x7)
@@ -15,8 +16,6 @@ import AutomatedAccessibilityManager from '@/ux/accessibility/view/automatic/Aut
 import AutomatedAccessibilityHome from '@/ux/accessibility/view/automatic/AutomatedAccessibilityHome.vue';
 import AccessibilityAnswers from '@/ux/accessibility/view/automatic/Answers.vue';
 import AccessibilityReport from '@/ux/accessibility/view/automatic/Report.vue';
-import AccessibilityCooperation from '@/ux/accessibility/view/automatic/Cooperation.vue';
-import AccessibilitySettings from '@/ux/accessibility/view/automatic/Settings.vue';
 import AccessibilityAnalyse from '@/ux/accessibility/view/automatic/EditTest.vue';
 
 const accessibilityRoutes = [
@@ -28,7 +27,7 @@ const accessibilityRoutes = [
             { path: 'edit/:id', name: 'EditAccessibilityTest', component: SettingsView, props: true },
             { path: 'preview/:id', name: 'AccessibilityPreviewTest', component: AccessibilityPreviewTest },
             { path: 'result/:id', name: 'AccessibilityTestAnswers', component: AccessibilityTestAnswers },
-            { path: 'cooperative/:id', name: 'AccessibilityTestCooperative', component: AccessibilityTestCooperative },
+            { path: 'cooperative/:id', name: 'AccessibilityTestCooperative', component: CooperatorsView, props: true },
             { path: 'config/:id', name: 'AccessibilityConfig', component: AccessibilityConfig }
         ]
     ),
@@ -40,7 +39,7 @@ const accessibilityRoutes = [
             { path: 'analyse/:id', name: 'Analyse', component: AccessibilityAnalyse, props: true },
             { path: 'answers/:id', name: 'AccessibilityAnswers', component: AccessibilityAnswers },
             { path: 'reports/:id', name: 'AccessibilityReport', component: AccessibilityReport },
-            { path: 'cooperation/:id', name: 'AccessibilityCooperation', component: AccessibilityCooperation },
+            { path: 'cooperation/:id', name: 'AccessibilityCooperation', component: CooperatorsView, props: true },
             { path: 'settings/:id', name: 'AccessibilitySettings', component: SettingsView, props: true }
         ]
     )
