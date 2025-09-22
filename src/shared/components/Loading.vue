7e0861deb2d0d18a8f61382b935c5595226677dc
Sahitya Chandra <sahityajb@gmail.com>
2025-09-23 00:46:11 +0530

    created a loader using new RUXAILAB logo and fixed its alignment

--- src/shared/components/Loading.vue
+++ src/shared/components/Loading.vue
@@ -1,6 +1,17 @@
 <template>
-  <v-overlay v-if="$route.path.includes('manager')" v-model="loading" class="text-center">
-    <v-progress-circular indeterminate color="#fca326" size="50" />
+  <v-overlay
+    v-if="$route.path.includes('manager')"
+    v-model="loading"
+    class="d-flex flex-column justify-center align-center text-center"
+  >
+    <!-- Spinning logo -->
+    <v-img
+      :src="logo"
+      class="spin-loader"
+      width="100"
+      height="100"
+      contain
+    />
     <div class="white-text mt-3">
       {{ $t('common.loading') }}
     </div>
@@ -10,7 +21,23 @@
 <script setup>
 import { computed } from 'vue'
 import { useStore } from 'vuex'
+import logo from '@/assets/ruxailab_logotip_Mesa_de_trabajo_1_copia_11.png' 
 
 const store = useStore()
 const loading = computed(() => store.getters.loading)
 </script>
+
+<style scoped>
+.spin-loader {
+  animation: spin 1.5s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
