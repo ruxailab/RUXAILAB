ded19d6cc60d71b29506303a435ba5a94ed016f4
Rakshit Yadav <yadavrakshit60@gmail.com>
2025-12-19 19:19:07 +0530

    fix the spelling of months in english
    Signed-off-by: Rakshit Yadav <yadavrakshit60@gmail.com>

--- src/shared/utils/dateUtils.js
+++ src/shared/utils/dateUtils.js
@@ -4,18 +4,18 @@
 
 // Mapeo de meses en español
 const MONTHS_ES = {
-    0: 'Enero',
-    1: 'Febrero',
-    2: 'Marzo',
-    3: 'Abril',
-    4: 'Mayo',
-    5: 'Junio',
-    6: 'Julio',
-    7: 'Agosto',
-    8: 'Septiembre',
-    9: 'Octubre',
-    10: 'Noviembre',
-    11: 'Diciembre'
+    0: 'January',
+    1: 'February',
+    2: 'March',
+    3: 'April',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'August',
+    8: 'September',
+    9: 'October',
+    10: 'November',
+    11: 'December'
 }
 
 /**
