afb31b50830a6df7da79144c254bb23e76199c9d
DivyanshuVorrtex <divyanshuchandra9027@gmail.com>
2026-03-06 18:38:55 +0000

    chores

--- src/features/dashboard/components/StatsCards.vue
+++ src/features/dashboard/components/StatsCards.vue
@@ -108,9 +108,7 @@ const formattedStorage = computed(() => {
     const gb = (props.usedStorage / 1000).toFixed(2)
     return `${gb}GB/0.5GB`
   }
-
   return `${parseFloat(props.usedStorage.toFixed(2))}MB/500MB`
-
 })
 </script>
 
