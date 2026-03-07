bf02c5592744a87f5e15094bb37c5a2954b2971a
Rakshit Yadav <yadavrakshit60@gmail.com>
2026-03-08 00:44:03 +0530

    feat add new weight option  toggle feature
    Signed-off-by: Rakshit Yadav <yadavrakshit60@gmail.com>

--- src/ux/Heuristic/models/HeuristicStudy.js
+++ src/ux/Heuristic/models/HeuristicStudy.js
@@ -11,11 +11,13 @@ export default class HeuristicStudy extends Study {
 
     this.testType = STUDY_TYPES.HEURISTIC
     this.testWeights = params.testWeights ?? {}
+    this.useWeights = params.useWeights ?? false
   }
 
   toFirestore() {
     return Object.assign(super.toFirestore(), {
       testWeights: this.testWeights,
+      useWeights: this.useWeights,
     })
   }
 }
