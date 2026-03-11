38d02d6ca7b37e3b6a0a9fad4df4f3c5eb67cbd1
Anushree Bondia <anushreebondia@gmail.com>
2026-03-11 19:41:57 +0530

    Issue #1851: fixed firebase emutaor path

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,11 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   transform: {
     '^.+\\.vue$': '@vue/vue3-jest',
-    '^.+\\.js$': 'babel-jest'
+    '^.+\\.js$': 'babel-jest',
   },
   testMatch: ['**/*.spec.js'],
   testPathIgnorePatterns: ['/e2e/'],
+  transformIgnorePatterns: ['/node_modules/(?!(axios)/)'],
   setupFilesAfterEnv: ['./tests/mocks/firebase.js'],
   resetMocks: true,
   clearMocks: true,
