e208163d3dea4095f1696400fe55f0a7994eeddf
Harshit2405-2004 <virmaniharshit27@gmail.com>
2026-03-11 20:31:50 +0530

    fix(test): add transformIgnorePatterns for axios ESM compatibility
    axios uses ESM import syntax which Jest cannot parse without
    transforming. Add transformIgnorePatterns to exclude axios from the
    default node_modules ignore list so babel-jest can convert it to CJS.
    
    Fixes CI test failure on Node 24 LTS.

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,9 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   transform: {
     '^.+\\.vue$': '@vue/vue3-jest',
-    '^.+\\.js$': 'babel-jest'
+    '^.+\\.js$': 'babel-jest',
   },
+  transformIgnorePatterns: ['/node_modules/(?!axios)'],
   testMatch: ['**/*.spec.js'],
   testPathIgnorePatterns: ['/e2e/'],
   setupFilesAfterEnv: ['./tests/mocks/firebase.js'],
