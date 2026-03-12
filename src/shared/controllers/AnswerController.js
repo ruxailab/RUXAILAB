8e9760234def7096353655d6af3403ed7453e8ae
Karthik Gundu <karrrthik7@gmail.com>
2026-03-12 13:00:22 +0530

    fix: use answersDocId instead of testDocId when deleting answer document

--- src/shared/controllers/AnswerController.js
+++ src/shared/controllers/AnswerController.js
@@ -40,7 +40,7 @@ export default class AnswerController extends Controller {
 
     // Delete answers document
     const answerDocumentId =
-      userToUpdate.myAnswers[`${payload.testDocId}`].testDocId
+      userToUpdate.myAnswers[`${payload.testDocId}`].answersDocId
     await super.delete(COLLECTION, answerDocumentId)
 
     // Remove it from user
