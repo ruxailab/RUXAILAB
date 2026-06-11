d86a8e9143be540632c5904737b24bf421fe575f
Montse Ponsa <montserrat.poncap.dam@insalfonscostafreda.cat>
2026-06-11 18:04:35 +0200

    fix: replace GlobalErrorHandler with GlobalToastHandler

--- src/app/layouts/DefaultLayout.vue
+++ src/app/layouts/DefaultLayout.vue
@@ -1,8 +1,6 @@
 <template>
   <v-app>
     <GlobalToolbar />
-    <GlobalErrorHandler />
-
     <GlobalToastHandler />
     <Loading />
     <v-main>
@@ -18,14 +16,12 @@
 
 <script>
 import { GlobalToolbar } from '@/features/navigation/utils'
-import GlobalErrorHandler from '@/app/layouts/components/GlobalErrorHandler.vue'
 import GlobalToastHandler from '@/app/layouts/components/GlobalToastHandler.vue'
 import Loading from '@/shared/components/Loading.vue'
 
 export default {
   components: {
     GlobalToolbar,
-    GlobalErrorHandler,
     GlobalToastHandler,
     Loading,
   },
--- src/app/layouts/NoToolbarLayout.vue
+++ src/app/layouts/NoToolbarLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <GlobalErrorHandler />
+    <GlobalToastHandler />
     <v-main>
       <v-container
         fluid
@@ -13,11 +13,11 @@
 </template>
 
 <script>
-import GlobalErrorHandler from '@/app/layouts/components/GlobalErrorHandler.vue'
+import GlobalToastHandler from '@/app/layouts/components/GlobalToastHandler.vue'
 
 export default {
     components: {
-        GlobalErrorHandler,
+        GlobalToastHandler,
     },
 }
 </script>
