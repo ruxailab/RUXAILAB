94cfdf02e2070b68796e0ef977caed5768a4bad6
Marc Gonzalez Capdevila <marcgc21@gmail.com>
2026-08-29 13:19:33 +0200

    fix: general improvements on heuristic method

--- src/ux/Heuristic/views/EditTest.vue
+++ src/ux/Heuristic/views/EditTest.vue
@@ -19,7 +19,7 @@
       @action="save({ showToast: true })"
     />
 
-    <v-container>
+    <v-container v-if="isCurrentStudy">
       <div>
         <v-tabs
           v-if="!isMobile"
@@ -86,7 +86,7 @@ import PageWrapper from '@/shared/views/template/PageWrapper.vue'
 import AutoSaveStatusBanner from '@/shared/components/AutoSaveStatusBanner.vue'
 import { ref, computed, onMounted, onBeforeUnmount, watch } from 'vue'
 import { useStore } from 'vuex'
-import { onBeforeRouteLeave } from 'vue-router'
+import { onBeforeRouteLeave, useRoute } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import { debounce } from 'lodash'
 import { instantiateStudyByType } from '@/shared/constants/methodDefinitions'
@@ -109,6 +109,7 @@ const props = defineProps({
 })
 
 const store = useStore()
+const route = useRoute()
 const { t } = useI18n()
 const studyController = new StudyController()
 
@@ -124,7 +125,8 @@ let currentSavePromise = null
 
 const showSettingsTab = computed(() => !props.isTemplate)
 const test = computed(() => store.getters.test)
-const showWeightsTab = computed(() => test.value.useWeights ?? false)
+const isCurrentStudy = computed(() => test.value?.id === route.params.id)
+const showWeightsTab = computed(() => test.value?.useWeights ?? false)
 const showOptionsTab = computed(
   () =>
     !showWeightsTab.value &&
