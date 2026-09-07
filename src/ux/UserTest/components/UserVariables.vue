06cc6135453a1264a2c70c9304d195fe0f0ad9d9
pyarchana <93970069+pyarchana@users.noreply.github.com>
2026-09-07 08:13:48 +0530

    fix(pre-test):unsquash delete

--- src/ux/UserTest/components/UserVariables.vue
+++ src/ux/UserTest/components/UserVariables.vue
@@ -92,30 +92,25 @@
                         density="comfortable"
                         :rules="[(v) => !!v || $t('errors.fieldRequired')]"
                         color="primary"
-                        class="mr-md-2"
+                        class="mr-md-2 flex-grow-1"
                         :disabled="isTemplate"
                         @update:model-value="markDirty"
+                      />
+                      <v-icon
+                        v-if="!isTemplate"
+                        color="accent"
+                        class="mr-2"
+                        @click="newSelection(i)"
                       >
-                        <template #append>
-                          <v-icon
-                            v-if="!isTemplate"
-                            color="accent"
-                            class="mr-2"
-                            @click="newSelection(i)"
-                          >
-                            mdi-plus-circle
-                          </v-icon>
-                          <v-icon
-                            v-if="
-                              !isTemplate && item.selectionFields.length > 1
-                            "
-                            color="error"
-                            @click="showDeleteOption(i, index)"
-                          >
-                            mdi-trash-can-outline
-                          </v-icon>
-                        </template>
-                      </v-text-field>
+                        mdi-plus-circle
+                      </v-icon>
+                      <v-icon
+                        v-if="!isTemplate && item.selectionFields.length > 2"
+                        color="error"
+                        @click="showDeleteOption(i, index)"
+                      >
+                        mdi-trash-can-outline
+                      </v-icon>
                     </div>
                     <div
                       v-if="item.selectionFields.length === 0"
@@ -135,7 +130,7 @@
                     </div>
                   </div>
                   <v-row align="center" class="mt-2">
-                    <v-col cols="12" sm="6">
+                    <v-col cols="12" sm="5">
                       <v-checkbox
                         v-model="item.selectionField"
                         :label="$t('UserTestTable.checkboxes.selectionField')"
@@ -153,7 +148,7 @@
                         @update:model-value="(selectText(i), markDirty())"
                       />
                     </v-col>
-                    <v-col cols="12" sm="1" class="text-right">
+                    <v-col cols="12" sm="auto" class="text-right">
                       <v-btn
                         icon
                         color="error"
@@ -377,7 +372,7 @@ const selectField = (i) => {
     items.value[i].selectionField &&
     items.value[i].selectionFields.length === 0
   ) {
-    items.value[i].selectionFields.push('')
+    items.value[i].selectionFields.push('', '')
   }
   if (!items.value[i].selectionField) {
     items.value[i].selectionFields = []
